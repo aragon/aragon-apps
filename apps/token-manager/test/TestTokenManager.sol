be9ba7f2c7ad8f6a5eb783b998a7f6aab1fe3af0
Jorge Izquierdo <jorge@izqui.me>
2017-10-22 13:27:02 +0200

    Add tokenmanager

---
+++ apps/token-manager/test/TestTokenManager.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.4.15;
+
+import "@aragon/core/contracts/common/MiniMeToken.sol";
+
+// You might think this file is a bit odd, but let me explain.
+// We only use the MiniMeToken contract in our tests, which
+// means Truffle will not compile it for us, because it is
+// from an external dependency.
+//
+// We are now left with three options:
+// - Copy/paste the MiniMeToken contract
+// - Run the tests with `truffle compile --all` on
+// - Or trick Truffle by claiming we use it in a Solidity test
+//
+// You know which one I went for.
+
+contract TestTokenManager {
+  // ...
+}
