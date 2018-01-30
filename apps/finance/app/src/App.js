1b70f6e65c526e355150c0c3019caf1b725bd0eb
Pierre Bertet <hello@pierre.world>
2018-01-30 20:08:20 +0100

    Demo data + transfers table

--- apps/finance/app/src/App.js
+++ apps/finance/app/src/App.js
@@ -1,29 +1,53 @@
 import React from 'react'
 import styled from 'styled-components'
-import {
-  AragonApp,
-  AppBar,
-  Button,
-  SidePanel,
-  SidePanelSeparator,
-} from '@aragon/ui'
+import { AragonApp, AppBar, Button } from '@aragon/ui'
+import { transfers } from './demo-state'
+import Transfers from './components/Transfers'
 
 class App extends React.Component {
   render() {
     return (
       <AragonApp publicUrl="/aragon-ui/">
-        <AppBar
-          title="Finance"
-          endContent={<Button mode="strong">New Payment</Button>}
-        />
-        <Main>Finance</Main>
+        <Layout>
+          <Layout.FixedHeader>
+            <AppBar
+              title="Finance"
+              endContent={<Button mode="strong">New Payment</Button>}
+            />
+          </Layout.FixedHeader>
+          <Layout.ScrollWrapper>
+            <Content>
+              <Transfers transfers={transfers} />
+            </Content>
+          </Layout.ScrollWrapper>
+        </Layout>
       </AragonApp>
     )
   }
 }
 
-const Main = styled.div`
+const Content = styled.div`
   padding: 30px;
+`
+
+const Layout = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: stretch;
+`
+
+Layout.FixedHeader = styled.div`
+  flex-shrink: 0;
+`
+
+Layout.ScrollWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  overflow: auto;
+  flex-grow: 1;
 `
 
 export default App
---
+++ apps/finance/app/src/demo-state.js
@@ -0,0 +1,69 @@
+import seedRandom from 'seed-random'
+import { subDays } from 'date-fns'
+import { randomInt, randomEntry } from './lib/utils'
+
+const TODAY = new Date()
+const RANDOM = seedRandom('seed')
+
+const CURRENCIES = ['ANT', 'ETH']
+
+const HASH_BASE = '0998B61aE8eD80f9370B579ee8085e4e05ff7451'
+
+const ARAGON_IDS = [
+  'mark',
+  'stephan',
+  'john',
+  'adrian',
+  'billie',
+  'clara',
+  'jane',
+  'michele',
+]
+
+const REFS_IN = [
+  'Invoice IN23987',
+  'Invoice IN32322',
+  'Invoice IN19733',
+  'Invoice IN23898',
+]
+const REFS_OUT = [
+  'Payment to service providers',
+  'Payment for PR services',
+  'Buy hardware',
+  'Payroll',
+]
+
+const transfer = (date, ref, amount, currency, approvedBy, transaction) => ({
+  date,
+  ref,
+  amount,
+  currency,
+  approvedBy,
+  transaction,
+})
+
+const createApprovedBy = () =>
+  randomInt(0, 2, RANDOM)
+    ? randomHash()
+    : `${randomEntry(ARAGON_IDS, RANDOM)}.aragonid.eth`
+
+const randomHash = () =>
+  `0x${[...HASH_BASE].sort(() => RANDOM() - 0.5).join('')}`
+
+export const transfers = [...new Array(43)]
+  .map((_, i) => {
+    const amount =
+      (randomInt(1, 15000, RANDOM) +
+        (randomInt(0, 2, RANDOM) ? 0 : randomInt(0, 1000, RANDOM) / 1000)) *
+      (randomInt(0, 2, RANDOM) || -1)
+    const refs = amount > 0 ? REFS_IN : REFS_OUT
+    return transfer(
+      subDays(TODAY, randomInt(0, 30, RANDOM)),
+      randomEntry(refs, RANDOM),
+      amount,
+      randomEntry(CURRENCIES, RANDOM),
+      createApprovedBy(),
+      randomHash(),
+    )
+  })
+  .sort(({ dateA, dateB }) => dateA - dateB)
