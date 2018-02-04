724890c52fd918028ec95a6f102334992c2c5271
Jorge Izquierdo <jorge@izqui.me>
2018-02-04 09:46:55 +0100

    Deploy vault and voting app with ipfs content uri

--- templates/dev/migrations/2_deploy.js
+++ templates/dev/migrations/2_deploy.js
@@ -5,17 +5,18 @@ const daoFactoryMigration = require('@aragon/os/migrations/3_factory')
 
 const DevTemplate = artifacts.require('DevTemplate')
 const Voting = artifacts.require('@aragon/apps-voting/contracts/Voting')
+const Vault = artifacts.require('@aragon/apps-voting/contracts/Vault')
 
-const appURI = appName => 'file:' + path.resolve('../../', 'apps', appName)
+const votingIpfs = 'ipfs:QmV5sEjshcZ6mu6uFUhJkWM5nTa53wbHfRFDD4Qy2Yx88m'
+const vaultIpfs = 'ipfs:QmPhd9aQGGoHJNcVVAmbzudShvhpYmyKxLWgqLoqSGD81G'
 
 module.exports = async (deployer, network, accounts) => {
   const { apm, ensAddr } = await apmMigration(deployer, network, accounts, artifacts)
   const { daoFact } = await daoFactoryMigration(deployer, network, accounts, artifacts)
 
   const votingBase = await Voting.new()
-
-  const template = await DevTemplate.new(daoFact.address, apm.address, votingBase.address, appURI('voting'))
-
+  const vaultBase = await Vault.new()
+  const template = await DevTemplate.new(daoFact.address, apm.address, votingBase.address, votingIpfs, vaultBase.address, vaultIpfs)
   const receipt = await template.createInstance()
   const daoAddr = receipt.logs.filter(l => l.event == 'DeployInstance')[0].args.dao
 
--- templates/dev/truffle.js
+++ templates/dev/truffle.js
@@ -1,1 +1,6 @@
-module.exports = require("@aragon/os/truffle-config")
+let x = require("@aragon/os/truffle-config")
+
+x.networks.rinkeby.gasPrice = 15000000001
+x.networks.rinkeby.gasLimit = 6.99e6
+
+module.exports = x
