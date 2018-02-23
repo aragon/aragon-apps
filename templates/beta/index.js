52b1e5324c0db97f3db8a566f3a9fead0762892a
Jorge Izquierdo <izqui97@gmail.com>
2018-02-22 19:38:53 -0800

    Implement and deploy Aragon 0.5 Beta templates (#91)
    * Start work on BetaTemplate, create base
    
    * BetaTemplate: Implement derived Democracy and Multisig templates
    
    * Implement and deploy beta templates
    
    * Add beta templates readme
    
    * Fix package.json
    
    * Fix readme

---
+++ templates/beta/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "rinkeby": {
+    "ens": "0xaa0ccb537289d226941745c4dd7a819a750897d0",
+    "templates": [
+      {
+        "name": "DemocracyTemplate",
+        "address": "0xfcc089230e47d9376fcbd7177164c095ce8e9f23"
+      },
+      {
+        "name": "MultisigTemplate",
+        "address": "0xce16339814b0c2e825a077ada6f08e4b4fc4e21f"
+      }
+    ]
+  },
+}
---
+++ templates/beta/migrations/2_deploy.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const fs = require('fs')
+
+const namehash = require('eth-ens-namehash').hash
+
+const daoFactoryMigration = require('@aragon/os/migrations/3_factory')
+const MiniMeTokenFactory = artifacts.require('@aragon/os/contracts/lib/minime/MiniMeTokenFactory')
+const ENS = artifacts.require('@aragon/os/contracts/lib/ens/ENS.sol')
+const EtherToken = artifacts.require('@aragon/os/contracts/common/EtherToken.sol')
+
+const templates = ['DemocracyTemplate', 'MultisigTemplate']
+
+// ensure alphabetic order
+const apps = ['finance', 'token-manager', 'vault', 'voting']
+
+const appIds = apps.map(app => namehash(require(`@aragon/apps-${app}/arapp`).appName))
+
+const deployMany = async (cs, params) => {
+  const x = await Promise.all(cs.map(c => artifacts.require(c).new(...params)))
+
+  return x.map(c => c.address)
+}
+
+const newRepo = async (apm, name, acc, contract) => {
+  const c = await artifacts.require(contract).new()
+  return await apm.newRepoWithVersion(name, acc, [1, 0, 0], c.address, '0x1245')
+}
+
+module.exports = async (deployer, network, accounts) => {
+  let indexObj = require('../index.js')
+  const ens = ENS.at(indexObj[network].ens)
+
+  const apmAddr = await artifacts.require('PublicResolver').at(await ens.resolver(namehash('aragonpm.eth'))).addr(namehash('aragonpm.eth'))
+
+  if (network == 'rpc') { // Useful for testing to avoid manual deploys with aragon-dev-cli
+    if (await ens.owner(appIds[0]) == '0x0000000000000000000000000000000000000000') {
+      const apm = artifacts.require('APMRegistry').at(apmAddr)
+
+      await newRepo(apm, 'voting', accounts[0], 'Voting')
+      await newRepo(apm, 'finance', accounts[0], 'Finance')
+      await newRepo(apm, 'token-manager', accounts[0], 'TokenManager')
+      await newRepo(apm, 'vault', accounts[0], 'Vault')
+    }
+  }
+
+  const { daoFact } = await daoFactoryMigration(deployer, network, accounts, artifacts)
+
+  const minimeFac = await MiniMeTokenFactory.new()
+  const etherToken = await EtherToken.new()
+
+  const aragonid = await ens.owner(namehash('aragonid.eth'))
+  const tmpls = await deployMany(templates, [daoFact.address, minimeFac.address, apmAddr, etherToken.address, aragonid, appIds])
+
+  const ts = tmpls.map((address, i) => ({ name: templates[i], address }) )
+
+  indexObj[network].templates = ts
+
+  const indexFile = 'module.exports = ' + JSON.stringify(indexObj, null, 2)
+  // could also use https://github.com/yeoman/stringify-object if you wanted single quotes
+  fs.writeFileSync('index.js', indexFile)
+
+  console.log('Template addresses saved to index.js')
+}
