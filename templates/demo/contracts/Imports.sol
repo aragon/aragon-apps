b2fc01f78d857bf0bde0f62a1b4c6fa4992dae2b
Pierre Bertet <hello@pierre.world>
2018-02-26 10:50:08 +0000

    Demo template tweaks (#106)
    * Create votes from different accounts
    * Import the dev contracts to make updates easier

--- templates/demo/contracts/Imports.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.18;
-
-// HACK to workaround truffle artifact loading on dependencies
-
-import "@aragon/os/contracts/factory/ENSFactory.sol";
-import "@aragon/os/contracts/factory/APMRegistryFactory.sol";
-import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
-
-contract Imports {}
