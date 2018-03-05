f90ac581f1a181a3cbc488f430f36d95f3903f69
ßingen <bingen@aragon.one>
2018-03-05 22:44:32 +0100

    templates: restore Imports workaround

---
+++ templates/beta/contracts/Imports.sol
@@ -0,0 +1,11 @@
+/* solium-disable */
+pragma solidity 0.4.18;
+
+// HACK to workaround truffle artifact loading on dependencies
+
+import "@aragon/os/contracts/factory/ENSFactory.sol";
+import "@aragon/os/contracts/factory/APMRegistryFactory.sol";
+import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
+import "@aragon/os/contracts/common/EtherToken.sol";
+
+contract Imports {}
