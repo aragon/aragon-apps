e414ead8ad960d6b1b8cdc36d80b5d08ada65f8b
ßingen <bingen@aragon.one>
2018-03-05 10:08:07 +0100

    Revert "teplates: Remove unnecessary return statement"
    This reverts commit cc767826e7e2b887addc6a7df0a37fd713d57ccc.

--- templates/beta/contracts/BetaTemplateBase.sol
+++ templates/beta/contracts/BetaTemplateBase.sol
@@ -55,6 +55,8 @@ contract BetaTemplateBase {
     function latestVersionAppBase(bytes32 appId) public view returns (address base) {
         Repo repo = Repo(PublicResolver(ens().resolver(appId)).addr(appId));
         (,base,) = repo.getLatest();
+
+        return base;
     }
 
     function createDAO(
