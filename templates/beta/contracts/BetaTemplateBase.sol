44531eb17ad667cf34526a0661748e24d4ab1434
ßingen <bingen@aragon.one>
2018-03-05 09:37:09 +0100

    teplates: Remove unnecessary return statement

--- templates/beta/contracts/BetaTemplateBase.sol
+++ templates/beta/contracts/BetaTemplateBase.sol
@@ -55,8 +55,6 @@ contract BetaTemplateBase {
     function latestVersionAppBase(bytes32 appId) public view returns (address base) {
         Repo repo = Repo(PublicResolver(ens().resolver(appId)).addr(appId));
         (,base,) = repo.getLatest();
-
-        return base;
     }
 
     function createDAO(
