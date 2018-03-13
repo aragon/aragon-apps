67071c638d5792d8694bb02273132333615a7055
Jorge Izquierdo <jorge@izqui.me>
2018-03-13 17:17:39 +0100

    Start work on new vault

--- apps/vault/contracts/Vault.sol
+++ apps/vault/contracts/Vault.sol
@@ -1,44 +1,27 @@
 pragma solidity 0.4.18;
 
+import "@aragon/os/contracts/common/DelegateProxy.sol";
 import "@aragon/os/contracts/apps/AragonApp.sol";
 
 import "@aragon/os/contracts/lib/zeppelin/token/ERC20.sol";
 import "@aragon/os/contracts/lib/misc/Migrations.sol";
 
-
 contract Vault is AragonApp {
-    event SetAllowance(address indexed token, address indexed spender, uint256 amount);
-    event TokenTransfer(address indexed token, address indexed receiver, uint256 amount);
+    event Transfer(address indexed token, address indexed receiver, uint256 amount);
+    event Deposit(address indexed token, address indexed sender, uint256 amount);
+
+    mapping (address => address) connectors;
 
     bytes32 constant public REQUEST_ALLOWANCES_ROLE = keccak256("REQUEST_ALLOWANCES_ROLE");
     bytes32 constant public TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
 
-    /**
-    * @notice Request for `_spender` to spend up to `_amounts` in `_tokens`
-    * @dev This action creates an ERC20 and will allow the spender to transferFrom the Vault address until the allowance is completely spent or it is overiden
-    * @param _tokens Array of token addresses
-    * @param _amounts Array of token amounts
-    */
-    function requestAllowances(ERC20[] _tokens, uint256[] _amounts) auth(REQUEST_ALLOWANCES_ROLE) external {
-        require(_tokens.length == _amounts.length);
+    address constant ETH = 0x0;
+    bytes32 constant erc777Identifier = keccak256('erc777');
+    bytes32 constant erc20Identifier = keccak256('erc20');
 
-        for (uint i = 0; i < _tokens.length; i++) {
-            requestAllowance(_tokens[i], _amounts[i]);
-        }
-    }
+    function initialize(address defaultConnector, address erc777connector, address ethConnector) onlyInit {
+      initialized();
 
-    function requestAllowance(ERC20 _token, uint256 _amount) authP(REQUEST_ALLOWANCES_ROLE, arr(address(_token), _amount)) public {
-        address spender = msg.sender;
-
-        // Some token implementations will throw when changing an allowance from non-zero to non-zero
-        // https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        if (_token.allowance(address(this), spender) > 0)
-            assert(_token.approve(spender, 0));
-
-        if (_amount > 0)
-            assert(_token.approve(spender, _amount));
-
-        SetAllowance(_token, spender, _amount);
     }
 
     /**
@@ -48,8 +31,23 @@ contract Vault is AragonApp {
     * @param _receiver Address of the recipient of tokens
     * @param _amount Amount of tokens being transferred
     */
-    function transferTokens(ERC20 _token, address _receiver, uint256 _amount) authP(TRANSFER_ROLE, arr(address(_token), _receiver, _amount)) external {
+    function transfer(ERC20 _token, address _receiver, uint256 _amount) authP(TRANSFER_ROLE, arr(address(_token), _receiver, _amount)) external {
         assert(_token.transfer(_receiver, _amount));
         TokenTransfer(_token, _receiver, _amount);
     }
+
+    function () payable {
+        require(msg.value > 0);
+        Deposit(ETH, msg.sender, msg.value);
+    }
+      /*
+        if (msg.value > 0) {
+
+        } else {
+            address connector = connectors[msg.sender];
+            require(connector != address(0) && msg.data.length > 0);
+
+            delegatedFwd(msg.data);
+        }
+    */
 }
