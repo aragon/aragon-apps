3162540fc2aa93d78a3c4fe933d7a1499741cb53
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-15 02:48:12 +0100

    TokenManager: add assign tokens side panel

--- apps/token-manager/app/src/App.js
+++ apps/token-manager/app/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { AragonApp, AppBar, Button, Badge, observe } from '@aragon/ui'
+import {
+  AragonApp,
+  AppBar,
+  Button,
+  Badge,
+  SidePanel,
+  observe,
+} from '@aragon/ui'
 import EmptyState from './screens/EmptyState'
 import Holders from './screens/Holders'
 import AppLayout from './components/AppLayout'
+import AssignVotePanelContent from './components/Panels/AssignVotePanelContent'
 import { hasLoadedTokenSettings } from './token-settings'
 
 class App extends React.Component {
@@ -17,6 +25,8 @@ class App extends React.Component {
     holders: [],
   }
   state = {
+    assignTokensConfig: {},
+    sidepanelOpened: false,
     tokenSettingsLoaded: false,
   }
   componentWillReceiveProps(nextProps) {
@@ -28,9 +38,29 @@ class App extends React.Component {
       })
     }
   }
+  handleAssignTokens = ({ amount, recipient }) => {
+    this.props.app.assign(recipient, amount)
+  }
+  handleAppBarLaunchAssignTokens = () => this.handleLaunchAssignTokens()
+  handleLaunchAssignTokens = recipient => {
+    this.setState({
+      assignTokensConfig: { recipient },
+      sidepanelOpened: true,
+    })
+  }
+  handleSidepanelClose = () => {
+    this.setState({
+      assignTokensConfig: {},
+      sidepanelOpened: false,
+    })
+  }
   render() {
     const { tokenSymbol, tokenSupply, holders } = this.props
-    const { tokenSettingsLoaded } = this.state
+    const {
+      assignTokensConfig,
+      sidepanelOpened,
+      tokenSettingsLoaded,
+    } = this.state
     return (
       <AragonApp publicUrl="/aragon-ui/">
         <AppLayout>
@@ -42,7 +72,14 @@ class App extends React.Component {
                   {tokenSymbol && <Badge.App>{tokenSymbol}</Badge.App>}
                 </Title>
               }
-              endContent={<Button mode="strong">Issue Tokens</Button>}
+              endContent={
+                <Button
+                  mode="strong"
+                  onClick={this.handleAppBarLaunchAssignTokens}
+                >
+                  Assign Tokens
+                </Button>
+              }
             />
           </AppLayout.Header>
           <AppLayout.ScrollWrapper>
@@ -55,6 +92,17 @@ class App extends React.Component {
             </AppLayout.Content>
           </AppLayout.ScrollWrapper>
         </AppLayout>
+        <SidePanel
+          title="Assign Tokens"
+          opened={sidepanelOpened}
+          onClose={this.handleSidepanelClose}
+        >
+          <AssignVotePanelContent
+            onAssignTokens={this.handleAssignTokens}
+            opened={sidepanelOpened}
+            {...assignTokensConfig}
+          />
+        </SidePanel>
       </AragonApp>
     )
   }
