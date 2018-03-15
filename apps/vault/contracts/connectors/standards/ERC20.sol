464ea98969f133ac7ff8fcb38fdc74512e7c486d
Jorge Izquierdo <jorge@izqui.me>
2018-03-15 08:53:04 +0100

    Scaffold tests

--- apps/vault/contracts/connectors/standards/ERC20.sol
+++ apps/vault/contracts/connectors/standards/ERC20.sol
@@ -1,11 +1,3 @@
 pragma solidity ^0.4.18;
 
-
-interface ERC20 {
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address who) public constant returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    function allowance(address owner, address spender) public constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-}
+import "@aragon/os/contracts/lib/zeppelin/token/ERC20.sol";
--- apps/vault/contracts/detectors/ERC780Detector.sol
+++ apps/vault/contracts/detectors/ERC780Detector.sol
@@ -1,1 +1,8 @@
-// TODO: wait for replies to https://github.com/ethereum/EIPs/issues/777#issuecomment-373022672
+pragma solidity 0.4.18;
+
+import "./standards/ERC780.sol";
+
+
+contract ERC780Detector {
+    // TODO: wait for replies to https://github.com/ethereum/EIPs/issues/777#issuecomment-373022672
+}
