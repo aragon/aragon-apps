d9a81562b84ebfa30c82e3d4acd0a0810eb2aa11
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-16 17:51:23 +0100

    Dev and Demo template: add Finance (#138)
    * Dev template: install Finance
    
    * Demo template: add deposits and payments to finance

--- templates/dev/migrations/2_deploy.js
+++ templates/dev/migrations/2_deploy.js
@@ -2,6 +2,7 @@ const apmMigration = require('@aragon/os/migrations/2_apm')
 const daoFactoryMigration = require('@aragon/os/migrations/3_factory')
 
 const ipfsHashes = require('../ipfs.js')
+const financeIpfs = ipfsHashes.finance
 const tokenManagerIpfs = ipfsHashes.tokenManager
 const vaultIpfs = ipfsHashes.vault
 const votingIpfs = ipfsHashes.voting
@@ -10,43 +11,59 @@ module.exports = async (deployer, network, accounts, arts = null) => {
   if (arts != null) artifacts = arts // allow running outside
 
   const DevTemplate = artifacts.require('DevTemplate')
-  const Voting = artifacts.require('@aragon/apps-voting/contracts/Voting')
-  const Vault = artifacts.require('@aragon/apps-vault/contracts/Vault')
+  const Finance = artifacts.require('@aragon/apps-finance/contracts/Finance')
   const TokenManager = artifacts.require('@aragon/apps-token-manager/contracts/TokenManager')
+  const Vault = artifacts.require('@aragon/apps-vault/contracts/Vault')
+  const Voting = artifacts.require('@aragon/apps-voting/contracts/Voting')
 
   const MiniMeTokenFactory = artifacts.require('@aragon/os/lib/minime/MiniMeTokenFactory')
 
   const { apm, ensAddr } = await apmMigration(deployer, network, accounts, artifacts)
   const { daoFact } = await daoFactoryMigration(deployer, network, accounts, artifacts)
-  const votingBase = await Voting.new()
-  const vaultBase = await Vault.new()
+  const financeBase = await Finance.new()
   const tokenManagerBase = await TokenManager.new()
+  const vaultBase = await Vault.new()
+  const votingBase = await Voting.new()
 
   const minimeFac = await MiniMeTokenFactory.new()
   const template = await DevTemplate.new(daoFact.address, minimeFac.address, apm.address)
-  await template.apmInit(votingBase.address, votingIpfs, vaultBase.address, vaultIpfs, tokenManagerBase.address, tokenManagerIpfs)
+  await template.apmInit(
+    financeBase.address,
+    financeIpfs,
+    tokenManagerBase.address,
+    tokenManagerIpfs,
+    vaultBase.address,
+    vaultIpfs,
+    votingBase.address,
+    votingIpfs
+  )
 
   const receipt = await template.createInstance()
 
   const daoAddr = receipt.logs.filter(l => l.event == 'DeployInstance')[0].args.dao
 
-  const votingId = await template.votingAppId()
-  const vaultId = await template.vaultAppId()
+  const financeId = await template.financeAppId()
   const tokenManagerId = await template.tokenManagerAppId()
+  const vaultId = await template.vaultAppId()
+  const votingId = await template.votingAppId()
+
   const installedApps = receipt.logs.filter(l => l.event == 'InstalledApp')
-  const votingAddr = installedApps.filter(e => e.args.appId == votingId)[0].args.appProxy
-  const vaultAddr = installedApps.filter(e => e.args.appId == vaultId)[0].args.appProxy
+  const financeAddr = installedApps.filter(e => e.args.appId == financeId)[0].args.appProxy
   const tokenManagerAddr = installedApps.filter(e => e.args.appId == tokenManagerId)[0].args.appProxy
+  const vaultAddr = installedApps.filter(e => e.args.appId == vaultId)[0].args.appProxy
+  const votingAddr = installedApps.filter(e => e.args.appId == votingId)[0].args.appProxy
 
   console.log('DAO:', daoAddr)
-  console.log("DAO's voting app:", votingAddr)
-  console.log("DAO's vault app:", vaultAddr)
+  console.log("DAO's finance app:", financeAddr)
   console.log("DAO's token manager app:", tokenManagerAddr)
+  console.log("DAO's vault app:", vaultAddr)
+  console.log("DAO's voting app:", votingAddr)
   console.log('ENS:', ensAddr)
 
   return {
     daoAddr,
     ensAddr,
+    financeAddr,
     tokenManagerAddr,
     vaultAddr,
     votingAddr,
