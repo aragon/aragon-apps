ccbb609d0f120e10390137dbde2f6e92cfd5a623
ßingen <bingen@aragon.one>
2018-03-18 08:45:06 +0100

    [wip] vault: Try to increase coverage

--- apps/vault/contracts/connectors/standards/ERC777.sol
+++ apps/vault/contracts/connectors/standards/ERC777.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.18;
 
+import "@aragon/os/contracts/lib/minime/MiniMeToken.sol";
+import "../../detectors/standards/ERC165.sol";
 
 interface ERC777 {
     function name() public constant returns (string);
@@ -34,8 +36,47 @@ interface ERC777TokensRecipient {
 }
 
 
-contract ERC777Fake {
-    function ERC777Fake() public {
-        // work around coverage weird error
+contract ERC777Token is MiniMeToken, ERC165 {
+    function granularity() public constant returns (uint256) {
+        // TODO
+    }
+
+    function send(address to, uint256 amount) public {
+        // TODO
+    }
+
+    function send(address to, uint256 amount, bytes userData) public {
+        // TODO
+    }
+
+    function authorizeOperator(address operator) public {
+        // TODO
+    }
+
+    function revokeOperator(address operator) public {
+        // TODO
+    }
+
+    function isOperatorFor(address operator, address tokenHolder) public constant returns (bool) {
+        // TODO
+    }
+
+    function operatorSend(address from, address to, uint256 amount, bytes userData, bytes operatorData) public {
+        // TODO
+    }
+
+    function supportsInterface(bytes4 interfaceID) external view returns (bool) {
+        return
+          interfaceID == this.supportsInterface.selector || // ERC165
+          interfaceID == this.name.selector
+            ^ this.symbol.selector
+            ^ this.totalSupply.selector
+            ^ this.granularity.selector
+            ^ this.balanceOf.selector
+            // TODO! ^ this.send.selector
+            ^ this.authorizeOperator.selector
+            ^ this.revokeOperator.selector
+            ^ this.isOperatorFor.selector
+            ^ this.operatorSend.selector;
     }
 }
