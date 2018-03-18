4a0e5ee4fe6b1f1148e18e2647dcbc8802ef3506
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-18 09:46:20 +0100

    Finance: remove demo state

--- apps/finance/app/src/lib/utils.js
+++ apps/finance/app/src/lib/utils.js
@@ -1,22 +1,10 @@
-import { round } from './math-utils'
-
-// TODO: move these utilities to Aragon UI
-
-export const randomInt = (min, max, random = Math.random) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(random() * (max - min)) + min
-}
-
-export const randomEntry = (array, random = Math.random) =>
-  array[randomInt(0, array.length, random)]
-
 export const formatTokenAmount = (
   amount,
   decimals = 0,
   displaySign = false,
   { rounding = 2 } = {}
 ) =>
+export const formatTokenAmount = (amount, displaySign = false) =>
   (displaySign && amount > 0 ? '+' : '') +
   Number(round(amount / Math.pow(10, decimals), rounding)).toLocaleString(
     'latn',
