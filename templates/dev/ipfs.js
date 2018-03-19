cc5de6ca7bf32ebaadb7bcc96762c87210ae8515
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-19 15:50:52 +0100

    Dev template: update finance IPFS hash

--- templates/dev/ipfs.js
+++ templates/dev/ipfs.js
@@ -1,5 +1,5 @@
 module.exports = {
-  finance: 'ipfs:QmcvtcmYo2HxBCvDToir6VhuX1Pywe6vG9zk9H62Wpkyb3',
+  finance: 'ipfs:QmVx4gi6VCoxYHQjZBdgoVHT1oG1faYcbi8ERpMGVc7Ag6',
   tokenManager: 'ipfs:QmYeV2iNw6EVDmChWKS8dXmFf7PJev8y8rWGx9Zty4jUBy',
   vault: 'ipfs:QmPhd9aQGGoHJNcVVAmbzudShvhpYmyKxLWgqLoqSGD81G',
   voting: 'ipfs:QmV5sEjshcZ6mu6uFUhJkWM5nTa53wbHfRFDD4Qy2Yx88m',
