fdc7aa07a6f74b5783adb10bdbf123cbc77eb7d9
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-19 15:31:51 +0100

    Fix merge issue

--- apps/finance/app/src/lib/utils.js
+++ apps/finance/app/src/lib/utils.js
@@ -1,10 +1,11 @@
+import { round } from './math-utils'
+
 export const formatTokenAmount = (
   amount,
   decimals = 0,
   displaySign = false,
   { rounding = 2 } = {}
 ) =>
-export const formatTokenAmount = (amount, displaySign = false) =>
   (displaySign && amount > 0 ? '+' : '') +
   Number(round(amount / Math.pow(10, decimals), rounding)).toLocaleString(
     'latn',
