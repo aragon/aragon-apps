76f0240a71bf3f5567b4dda8d3b6ffc0ddb3e707
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-21 10:41:34 +0100

    Finance: use @aragon/templates-token

--- apps/finance/app/src/script.js
+++ apps/finance/app/src/script.js
@@ -1,5 +1,6 @@
 import Aragon from '@aragon/client'
 import financeSettings, { hasLoadedFinanceSettings } from './finance-settings'
+import { testTokenAddresses } from './testnet'
 import tokenBalanceOfAbi from './abi/token-balanceof.json'
 import tokenDecimalsAbi from './abi/token-decimals.json'
 import tokenSymbolAbi from './abi/token-symbol.json'
@@ -31,7 +32,7 @@ app.store(async (state, event) => {
   }
 
   // NOTE: this is only for rinkeby!
-  nextState = await loadTestState(nextState)
+  nextState = await loadTestnetState(nextState)
 
   return nextState
 })
@@ -223,28 +224,16 @@ function marshallTransactionDetails({
  * RINKEBY TEST STATE *
  *                    *
  **********************/
-const TEST_TOKEN_ADDRS = [
-  '0x0d5263b7969144a852d58505602f630f9b20239d',
-  '0x6142214d83670226872d51e935fb57bec8832a60',
-  '0x1e1cab55639f67e70973586527ec1dfdaf9bf764',
-  '0x5e381afb0104d374f1f3ccde5ba7fe8f5b8af0e6',
-  '0xa53899a7eb70b309f05f8fdb344cdc8c8f272abe',
-  '0x5b2fdbba47e8ae35b9d6f8e1480703334f48b96c',
-  '0x51e53b52555a4ab7227423a7761cc8e418b147c8',
-  '0xc42da14b1c0ae7d4dd3946633f1046c3d46f3101',
-  '0x4fc6e3b791560f25ed4c1bf5e2db9ab0d0e80747',
-]
-
-function loadTestState(nextState) {
+function loadTestnetState(nextState) {
   // Reload all the test tokens' balances for this DAO's vault
-  return loadTestTokenBalances(nextState)
+  return loadTestnetTokenBalances(nextState)
 }
 
-async function loadTestTokenBalances(nextState) {
+async function loadTestnetTokenBalances(nextState) {
   let reducedState = nextState
-  for (let tokenAddress of TEST_TOKEN_ADDRS) {
+  for (let tokenAddress of testTokenAddresses) {
     reducedState = {
-      ...nextState,
+      ...reducedState,
       balances: await updateBalances(reducedState, { token: tokenAddress }),
     }
   }
