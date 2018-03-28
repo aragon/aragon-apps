4b56c2170d1e9e0a2dafd3b1e32320576bf3c147
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-28 22:54:05 +0200

    Finance: correctly identity outgoing transfers (#218)

--- apps/finance/app/src/lib/utils.js
+++ apps/finance/app/src/lib/utils.js
@@ -2,11 +2,12 @@ import { round } from './math-utils'
 
 export const formatTokenAmount = (
   amount,
+  isIncoming,
   decimals = 0,
   displaySign = false,
   { rounding = 2 } = {}
 ) =>
-  (displaySign && amount > 0 ? '+' : '') +
+  (displaySign && isIncoming ? '+' : '') +
   Number(round(amount / Math.pow(10, decimals), rounding)).toLocaleString(
     'latn',
     {
