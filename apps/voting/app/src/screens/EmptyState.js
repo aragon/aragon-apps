c191a582769d027910dff3d8c982fe04174f46af
Pierre Bertet <hello@pierre.world>
2018-03-28 21:17:12 +0100

    Fix empty state vertical centering in apps (#217)

--- apps/voting/app/src/screens/EmptyState.js
+++ apps/voting/app/src/screens/EmptyState.js
@@ -19,9 +19,9 @@ const EmptyState = ({ onActivate }) => (
 
 const Main = styled.div`
   display: flex;
-  height: 100vh;
   align-items: center;
   justify-content: center;
+  flex-grow: 1;
 `
 
 export default EmptyState
