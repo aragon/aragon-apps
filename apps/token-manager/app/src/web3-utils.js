f4f436d744c7052f280d72c2bba00bdbcb041d3d
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-29 11:07:32 +0200

    TokenManager: add address input validation

--- apps/token-manager/app/src/web3-utils.js
+++ apps/token-manager/app/src/web3-utils.js
@@ -6,3 +6,8 @@ export function addressesEqual(first, second) {
   second = second && toChecksumAddress(second)
   return first === second
 }
+
+export const addressPattern = '(0x)?[0-9a-fA-F]{40}'
+
+// Re-export some web3-utils functions
+export { isAddress } from 'web3-utils'
