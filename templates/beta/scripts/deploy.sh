3bb2867bbe913808233405d94725cc1d00e06a54
Brett Sun <qisheng.brett.sun@gmail.com>
2018-04-26 01:52:42 +0800

    Include install into templates-beta deploy script

--- templates/beta/scripts/deploy.sh
+++ templates/beta/scripts/deploy.sh
@@ -3,21 +3,25 @@
 set -e
 
 cd node_modules/@aragon/os
+printf "\nInstalling aragonOS dependencies...\n"
+npm install
+printf "\nDeploying test ENS instance...\n"
 export ENS=$(npm run deploy:devnet:ens | tail -n 1) # get last line of output
-echo "Using ENS ${ENS}"
+printf "Using ENS ${ENS}"
+printf "\nDeploying test APM instance...\n"
 npm run deploy:devnet:apm
 # extract and set ENS
 cd -
 
 cd node_modules/@aragon/id
+printf "\nInstalling aragon-id dependencies...\n"
 npm install
+printf "\nDeploying test aragon-id instance...\n"
 npm run deploy:devnet
+
 cd -
-
-#cd ../../ # aragon-apps
-#env DEBUG=true npm run publish:devnet
-#cd -
-
+printf "\nDeploying beta templates...\n"
 npm run deploy:devnet
 
+printf "\nRunning tests...\n"
 ./node_modules/.bin/truffle test --network devnet test/docker.js
