7514d06efe5113a0fccf8cb1b498d24ebf6c1ea7
Jorge Izquierdo <jorge@aragon.one>
2018-04-28 14:17:33 +0800

    Begin work on bringing back old vault

--- apps/vault/contracts/Vault.sol
+++ apps/vault/contracts/Vault.sol
@@ -1,118 +1,65 @@
 pragma solidity 0.4.18;
 
-import "./VaultBase.sol"; // split made to avoid circular import
+import "@aragon/os/contracts/apps/AragonApp.sol";
 
-import "./connectors/ERC20Connector.sol";
-import "./connectors/ETHConnector.sol";
-
+import "@aragon/os/contracts/lib/zeppelin/token/ERC20.sol";
 import "@aragon/os/contracts/lib/misc/Migrations.sol";
 
 
-contract Vault is VaultBase {
-    struct TokenStandard {
-        uint32 erc;
-        uint32 interfaceDetectionERC;
-        bytes4 interfaceID;
-        address connector;
+contract Vault is AragonApp {
+    address constant ETH = address(0);
+
+    bytes32 constant public TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+
+    event Transfer(address indexed token, address indexed to, uint256 amount);
+    event Deposit(address indexed token, address indexed sender, uint256 amount);
+
+    /**
+    * @dev When used behind an AppProxy, this fallback is never executed
+    *      as it will be intercepted by the Proxy (see aragonOS#281)
+    */ 
+    function () payable {
+        require(msg.data.length == 0);
+        deposit(ETH, msg.sender, msg.value, msg.data);
     }
 
-    TokenStandard[] public standards;
-    mapping (address => address) public connectors;
-    mapping (uint32 => bool) public supportedInterfaceDetectionERCs;
-
-    // this is required to avoid having to redeploy on every instance.
-    // they can just be gotten from the base by templates
-    ERC20Connector public erc20ConnectorBase;
-    ETHConnector public ethConnectorBase;
-
-    event NewTokenStandard(uint32 indexed erc, uint32 indexed interfaceDetectionERC, bytes4 indexed interfaceID, address connector);
-
-    function Vault() public {
-        erc20ConnectorBase = new ERC20Connector();
-        ethConnectorBase = new ETHConnector();
-    }
-
-    function initialize(ERC20Connector erc20Connector, ETHConnector ethConnector) onlyInit public {
-        initialized();
-
-        supportedInterfaceDetectionERCs[NO_DETECTION] = true;
-        supportedInterfaceDetectionERCs[ERC165] = true;
-
-        _setConnectors(erc20Connector, ethConnector);
-    }
-
-    function initializeWithBase(Vault baseVault) onlyInit public {
-        initialize(baseVault.erc20ConnectorBase(), baseVault.ethConnectorBase());
-    }
-
-    function _setConnectors(ERC20Connector erc20Connector, ETHConnector ethConnector) internal {
-        require(erc20Connector != address(0) && ethConnector != address(0));
-
-        // register erc20 as the first standard
-        _registerStandard(20, NO_DETECTION, bytes4(0), erc20Connector);
-        // directly manage ETH with the ethConnector
-        connectors[ETH] = ethConnector;
-    }
-
-    function () payable public {
-        address token = ETH;
-
-        // 4 (sig) + 32 (at least the token address to locate connector)
-        if (msg.data.length < 36) {
-            require(msg.value > 0); // if no data, only call ETH connector when ETH
+    /**
+    * @notice Deposit `value` `token` to the vault
+    * @param token Address of the token being transferred
+    * @param from Entity that currently owns the tokens
+    * @param value Amount of tokens being transferred
+    * @param data (Currently unused for API comp) Extra data associated with the deposit
+    */
+    function deposit(address token, address from, uint256 value, bytes data) payable public {
+        if (token == ETH) {
+            require(msg.value > 0 && msg.value == value);
+            require(msg.sender == from);
         } else {
-            assembly { token := calldataload(4) } // token address MUST be the first argument to any Vault calls
+            require(ERC20(token).transferFrom(from, this, value));
         }
 
-        if (connectors[token] == address(0)) {
-            connectors[token] = standards[detectTokenStandard(token)].connector;
+        Deposit(token, from, value);
+    }
+
+    /**
+    * @notice Transfer `value` `token` from the Vault to `to`
+    * @param token Address of the token being transferred
+    * @param to Address of the recipient of tokens
+    * @param value Amount of tokens being transferred
+    * @param data Extra data associated with the transfer (only allowed for ETH)
+    */
+    function transfer(address token, address to, uint256 value, bytes data)
+        authP(TRANSFER_ROLE, arr(address(token), to, value))
+        external {
+
+        require(value > 0);
+
+        if (token == ETH) {
+            require(to.call.value(value)(data));
+        } else {
+            require(ERC20(token).transfer(to, value));
         }
 
-        // if return data size is less than 32 bytes, it will revert
-        delegatedFwd(connectors[token], msg.data, 32);
-    }
-
-    function registerStandard(uint32 erc, uint32 interfaceDetectionERC, bytes4 interfaceID, address connector)
-             authP(REGISTER_TOKEN_STANDARD, arr(uint256(erc), interfaceDetectionERC))
-             public
-    {
-
-        _registerStandard(erc, interfaceDetectionERC, interfaceID, connector);
-    }
-
-    function detectTokenStandard(address token) public view returns (uint256 standardId) {
-        // skip index 0 which is erc20 and it is not conformant to any
-        for (uint256 i = 1; i < standards.length; i++) {
-            if (conformsToStandard(token, i)) {
-                return i;
-            }
-        }
-
-        // no definition, return ERC20 standard
-        return 0;
-    }
-
-    function conformsToStandard(address token, uint256 standardId) public view returns (bool) {
-        if (standards[standardId].interfaceDetectionERC == ERC165) {
-            return conformsToERC165(token, bytes4(standards[standardId].interfaceID));
-        }
-
-        return false;
-    }
-
-    function isInterfaceDetectionERCSupported(uint32 interfaceDetectionERC) public view returns (bool) {
-        return supportedInterfaceDetectionERCs[interfaceDetectionERC];
-    }
-
-    function _registerStandard(uint32 erc, uint32 interfaceDetectionERC, bytes4 interfaceID, address connector) internal {
-        require(isInterfaceDetectionERCSupported(interfaceDetectionERC));
-
-        for (uint256 i = 0; i < standards.length; i++) {
-            require(standards[i].erc != erc);
-        }
-
-        standards.push(TokenStandard(erc, interfaceDetectionERC, interfaceID, connector));
-
-        NewTokenStandard(erc, interfaceDetectionERC, interfaceID, connector);
+        Transfer(token, to, value);
     }
 }
--- apps/vault/test/vault.js
+++ apps/vault/test/vault.js
@@ -2,23 +2,18 @@ const { assertRevert, assertInvalidOpcode } = require('@aragon/test-helpers/asse
 const getBalance = require('@aragon/test-helpers/balance')(web3)
 
 const Vault = artifacts.require('Vault')
-const IVaultConnector = artifacts.require('IVaultConnector')
 
 const getContract = name => artifacts.require(name)
 
 const NULL_ADDRESS = '0x00'
 
 contract('Vault app', (accounts) => {
-  let token, token777
+  let token
 
   const ETH = '0x0'
-  const ERC165 = 165
-  const ERC777 = 777
-  const NO_DETECTION = 2**32 - 1
 
   before(async () => {
     token = await getContract('SimpleERC20').new()
-    token777 = await getContract('SimpleERC777').new()
   })
 
   context('Deposits and transfers', async() => {
@@ -39,65 +34,6 @@ contract('Vault app', (accounts) => {
     it('deposits ETH trhough callback', async() => {
       await vault.sendTransaction( { value: 1 })
       assert.equal((await getBalance(vault.address)).toString(), 1, "should hold 1 wei")
-    })
-  })
-
-  context('Standards', async() => {
-    let vaultBase, vault, erc20Connector, erc777Connector
-
-    before(async() => {
-      vaultBase = await Vault.new()
-      vault = await IVaultConnector.at(vaultBase.address)
-      await vaultBase.initializeWithBase(vaultBase.address)
-
-      erc20Connector = await getContract('ERC20Connector').new()
-      erc777Connector = await getContract('ERC777Connector').new()
-      await vaultBase.registerStandard(ERC777, ERC165, ["0x01", "0x02", "0x03", "0x04"], erc777Connector.address)
-    })
-
-    it('fails trying to register standard again (ERC20)', async() => {
-      return assertRevert(async() => {
-        await vaultBase.registerStandard(20, ERC165, ["0x01", "0x02", "0x03", "0x04"], erc20Connector.address)
-      })
-    })
-
-    it('fails trying to register standard again (ERC777)', async() => {
-      return assertRevert(async() => {
-        await vaultBase.registerStandard(ERC777, ERC165, ["0x01", "0x02", "0x03", "0x04"], erc777Connector.address)
-      })
-    })
-
-    it('fails trying to detect standard for ERC20 (no standard)', async() => {
-      return assertRevert(async() => {
-        let ts = await vaultBase.detectTokenStandard(token.address)
-      })
-    })
-
-    /* TODO
-    it('fails checking ERC20 against ERC165', async() => {
-      return assertRevert(async() => {
-        await vaultBase.conformsToStandard(token.address, ERC165)
-      })
-    })
-     */
-
-    it('detects standard', async() => {
-      let ts = await vaultBase.detectTokenStandard(token777.address)
-      assert.equal(ts.toString(), 1, "standard ID should be 1")
-    })
-
-    it('token conforms to standard', async() => {
-      assert.isTrue(await vaultBase.conformsToStandard(token777.address, 1), "should return false")
-    })
-
-    /* TODO
-    it('token doesn\'t conform to standard (ERC165)', async() => {
-      assert.isFalse(await vaultBase.conformsToStandard(tokenXXX.address, ERC777), "should return false")
-    })
-     */
-
-    it('token doesn\'t conform to standard (not ERC165)', async() => {
-      assert.isFalse(await vaultBase.conformsToStandard(token.address, 0), "should return false")
     })
   })
 })
