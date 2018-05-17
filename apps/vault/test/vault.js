14f98492413570e39aec3c8f3e7e9febfd2f39e8
Brett Sun <qisheng.brett.sun@gmail.com>
2018-05-17 08:56:34 -0700

    Small adjustments to old Vault with ETH (#304)
    * Fix whitespace
    
    * Vault: add quality of life overloads for deposit() and transfer()
    
    * Vault: require deposits to always have positive value
    
    * Vault: remove migrations import

--- apps/vault/test/vault.js
+++ apps/vault/test/vault.js
@@ -23,9 +23,19 @@ contract('Vault app', (accounts) => {
 
   context('ETH:', async () => {
     it('deposits ETH', async () => {
-      await vault.deposit(ETH, accounts[0], 1, [0], { value: 1 })
-      assert.equal((await getBalance(vault.address)).toString(), 1, "should hold 1 wei")
-      assert.equal((await vault.balance(ETH)).toString(), 1, "should return 1 wei balance")
+      const value = 1
+
+      // Deposit without data param
+      await vault.deposit(ETH, accounts[0], value, { value })
+      assert.equal((await getBalance(vault.address)).toString(), value, `should hold ${value} wei`)
+      assert.equal((await vault.balance(ETH)).toString(), value, `should return ${value} wei balance`)
+
+      // Deposit with data param
+      /* Waiting for truffle to get overloading...
+      await vault.deposit(ETH, accounts[0], value, [0], { value })
+      assert.equal((await getBalance(vault.address)).toString(), value * 2, `should hold ${value * 2} wei`)
+      assert.equal((await vault.balance(ETH)).toString(), value * 2, `should return ${value * 2} wei balance`)
+      */
     })
 
     it('deposits ETH through callback', async () => {
@@ -34,19 +44,33 @@ contract('Vault app', (accounts) => {
     })
 
     it('transfers ETH', async () => {
-      await vault.sendTransaction( { value: 100 }) 
+      const depositValue = 100
+      const transferValue = 10
+
+      await vault.sendTransaction( { value: depositValue })
       const testAccount = '0xbeef000000000000000000000000000000000000'
       const initialBalance = await getBalance(testAccount)
 
-      await vault.transfer(ETH, testAccount, 10, NO_DATA)
+      // Transfer with data param
+      await vault.transfer(ETH, testAccount, transferValue, NO_DATA)
 
-      assert.equal((await getBalance(testAccount)).toString(), initialBalance.add(10).toString(), "should have sent eth")
-      assert.equal((await getBalance(vault.address)).toString(), 90, "should have remaining balance")
+      assert.equal((await getBalance(testAccount)).toString(), initialBalance.add(transferValue).toString(), "should have sent eth")
+      assert.equal((await getBalance(vault.address)).toString(), depositValue - transferValue, "should have remaining balance")
+
+      // Transfer without data param
+      /* Waiting for truffle to get overloading...
+      await vault.transfer(ETH, testAccount, transferValue)
+
+      assert.equal((await getBalance(testAccount)).toString(), initialBalance.add(transferValue * 2).toString(), "should have sent eth")
+      assert.equal((await getBalance(vault.address)).toString(), depositValue - transferValue * 2, "should have remaining balance")
+      */
     })
 
     it('fails if depositing a different amount of ETH than sent', async () => {
+      const value = 1
+
       return assertRevert(async () => {
-        await vault.deposit(ETH, accounts[0], 1, [0], { value: 2 })
+        await vault.deposit(ETH, accounts[0], value, { value: value * 2 })
       })
     })
   })
@@ -60,25 +84,45 @@ contract('Vault app', (accounts) => {
 
     it('deposits ERC20s', async () => {
       await token.approve(vault.address, 10)
-      await vault.deposit(token.address, accounts[0], 5, '')
+
+      // Deposit half without data param
+      await vault.deposit(token.address, accounts[0], 5)
 
       assert.equal(await token.balanceOf(vault.address), 5, "token accounting should be correct")
       assert.equal(await vault.balance(token.address), 5, "vault should know its balance")
+
+      // Deposit half with data param
+      /* Waiting for truffle to get overloading...
+      await vault.deposit(token.address, accounts[0], 5, '')
+
+      assert.equal(await token.balanceOf(vault.address), 10, "token accounting should be correct")
+      assert.equal(await vault.balance(token.address), 10, "vault should know its balance")
+      */
     })
 
     it('transfers tokens', async () => {
       const tokenReceiver = accounts[2]
       await token.transfer(vault.address, 10)
+
+      // Transfer half with data param
       await vault.transfer(token.address, tokenReceiver, 5, '')
 
       assert.equal(await token.balanceOf(tokenReceiver), 5, "receiver should have correct token balance")
+
+      // Transfer half without data param
+      /* Waiting for truffle to get overloading...
+      await vault.transfer(token.address, tokenReceiver, 5)
+
+      assert.equal(await token.balanceOf(tokenReceiver), 10, "receiver should have correct token balance")
+      */
     })
 
     it('fails if not sufficient token balance available', async () => {
-      await token.approve(vault.address, 10)
+      const approvedAmount = 10
+      await token.approve(vault.address, approvedAmount)
 
       return assertRevert(async () => {
-          await vault.deposit(token.address, accounts[0], 15, '')
+          await vault.deposit(token.address, accounts[0], approvedAmount * 2)
       })
     })
   })
