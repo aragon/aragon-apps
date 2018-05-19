57317210dbff40f57b4fd3ca760f03474d1ffce2
Derek Alia <derekalia@me.com>
2018-05-19 12:49:03 -0700

    removed payroll2

--- future-apps/payroll2/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var App = artifacts.require('./App.sol')
-
-module.exports = function (deployer) {
-  deployer.deploy(App)
-}
--- future-apps/payroll2/test/app.js
+++
@@ -1,5 +0,0 @@
-const App = artifacts.require('./App.sol')
-
-contract('App', (accounts) => {
-  it('should be tested')
-})
