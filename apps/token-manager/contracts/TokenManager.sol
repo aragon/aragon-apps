27f43fe96ce00c42040c5b1899eac4558781be8e
Brett Sun <qisheng.brett.sun@gmail.com>
2018-05-21 01:39:51 -0600

    Revert "Remove migrations import in app contracts (#305)" (#319)
    This reverts commit 4135078a2497cc807009b92069b8d5a7046b8b0e.

--- apps/token-manager/contracts/TokenManager.sol
+++ apps/token-manager/contracts/TokenManager.sol
@@ -9,6 +9,8 @@ import "@aragon/os/contracts/common/IForwarder.sol";
 import "@aragon/os/contracts/lib/zeppelin/token/ERC20.sol";
 import "@aragon/os/contracts/lib/zeppelin/math/SafeMath.sol";
 
+import "@aragon/os/contracts/lib/misc/Migrations.sol";
+
 
 contract TokenManager is ITokenController, AragonApp, IForwarder {
     using SafeMath for uint256;
