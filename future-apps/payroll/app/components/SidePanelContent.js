f8bc339eec8e3a951e6dbc7e5321e159f2bbb366
Derek Alia <derekalia@me.com>
2018-05-23 23:08:25 +0200

    added templating and sidebar functionality

--- future-apps/payroll/app/components/SidePanelContent.js
+++ future-apps/payroll/app/components/SidePanelContent.js
@@ -1,109 +1,172 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Text, theme, Button, Info, DropDown,TextInput } from '@aragon/ui';
+import { Text, theme, Button, Info, DropDown, TextInput, Slider } from '@aragon/ui';
 import { sciNot } from '../math-utils';
 
 class SidePanelContent extends React.Component {
   state = {
     tokenList: ['ETH', 'ANT', 'SNT'],
-    list: [{ symbol: 'ETH', percentage: 50 }, { symbol: 'ANT', percentage: 50 }],
+    distribution: [{ symbol: 'ETH', value: 0.5 }, { symbol: 'ANT', value: 0.5 }],
     activeItem: 0
   };
 
-  handleChange(index) {
-    //check if this symbol is already in the list/ if not then add it to the list with 0
-    let symbol = this.state.tokenList[index];
-    let taken = false;
-    let list = this.state.list;
-    list.forEach(item => {
-      if (item.symbol == symbol) taken = true;
-    });
+  handleSliderChange = (value, index) => {
+    this.setState(({ distribution }) => {
 
-    if (!taken) {
-      list.push({ symbol, percentage: 0 });
-      this.setState({ list });
-    } else {
-    }
+      console.log("distribution", distribution)
+      console.log({value});
+
+      let d = this.updateDistributionValue(index, value, distribution);
+      console.log('d', d);
+      return { distribution: d };
+    });
+  };
+
+  distributionPairs() {
+    const { distribution } = this.state;
+    const pairs = distribution.map(item => ({
+      value: item.value,
+      percentage: Math.round(item.value * 100),
+      symbol: item.symbol
+    }));
+
+    // Add / remove the missing percentage after rounding the values
+    pairs[0].percentage += pairs.reduce((total, { percentage }) => total - percentage, 100);
+    return pairs;
   }
 
-  updatePercent(e, i) {
-    let list = this.state.list;
-    let prevVal = list[i].percentage;
-    list[i].percentage = e.target.value;
+  // Update the distribution by changing one of the values
+  updateDistributionValue(index, value, distribution) {
+    // Other values than the one being directly updated
 
-    let increase;
-    if (prevVal < e.target.value) {
-      increase = true;
-    } else {
-      increase = false;
+    console.log('updateDistributionValue', value);
+
+    const othersTotal = distribution.reduce((total, { value }, i) => total + (i === index ? 0 : value), 0);
+
+    const updateOtherValue = prevValue =>
+      prevValue === 0 ? 0 : prevValue - (othersTotal + value - 1) * prevValue / othersTotal;
+
+    return distribution.map((item, i) => ({ ...item, value: i === index ? value : updateOtherValue(item.value) }));
+  }
+
+  addToken() {
+    //check if this symbol is already in the list/ if not then add it to the list with 0
+    let tokenList = [...this.state.tokenList];
+    let list = [...this.state.list];
+
+    list.forEach(item => {
+      let indexOfToken = tokenList.indexOf(item.symbol);
+
+      if (indexOfToken !== -1) {
+        tokenList.splice(indexOfToken, 1);
+      }
+    });
+
+    if (tokenList.length) {
+      list.push({ symbol: tokenList[0], value: 0 });
+      this.setState({ list });
+      return;
     }
+    console.log('no more tokens');
+  }
 
-    let split = list.length - 1;
-    // list.forEach((item, index) => {
-    //   if (i !== index) {
-    //     if (increase) {
-    //       item.percentage -= 1 / split;
-    //     } else {
-    //       item.percentage += 1 / split;
-    //     }
-    //   }
-    // });
+  handleUpdate = (index, value) => {
+    const { distribution } = this.state;
+    this.setState({
+      distribution: this.updateDistributionValue(index, value, distribution)
+    });
+  };
+
+  // add = () => {
+  //   this.setState(({ distribution }) => ({
+  //     distribution: this.updateDistributionValue(0, distribution[0], [...distribution, 0.1].slice(0, ROWS_MAX))
+  //   }));
+  // };
+
+  //rename to token something
+  handleChange(targetIndex, localTokenList) {
+    let list = [...this.state.distribution];
+    let indexToRemove;
+    list = list.map((item, index) => {
+      if (item.symbol == localTokenList[0]) {
+        let obj = { ...item, symbol: localTokenList[targetIndex] };
+
+        return obj;
+      } else {
+        return item;
+      }
+    });
 
     this.setState({ list });
   }
 
-  getTokenList(list, symbol) {
+  getTokenList(symbol) {
+    let list = this.state.distribution;
     let tokenList = [...this.state.tokenList];
 
     list.forEach(item => {
-      let index = tokenList[item.symbol];
+      let index = tokenList.indexOf(item.symbol);
       if (index !== -1) {
         tokenList.splice(index, 1);
       }
     });
-
     tokenList.unshift(symbol);
 
     return tokenList;
   }
 
   render() {
+    const distributionPairs = this.distributionPairs();
+
     return (
       <Container>
         <Info.Action title="Choose which tokens you get paid in">
           You can add as many tokens as you like, as long as your DAO has these tokens.
         </Info.Action>
+        <div>
+          <SliderHeader>
+            <Text color="textSecondary" smallcaps>
+              token
+            </Text>
+            <Text style={{ textAlign: 'right' }} color="textSecondary" smallcaps>
+              percentage
+            </Text>
+          </SliderHeader>
+          {distributionPairs.map((item, index) => {
+            let newTokenList = this.getTokenList(item.symbol);
+            let tokenNumber = newTokenList.indexOf(item.symbol);
+            console.log({item});
+            return (
+              <TokenInfo key={index}>
+                <DropDown
+                  items={newTokenList}
+                  active={tokenNumber}
+                  onChange={e => this.handleChange(e, newTokenList)}
+                />
+                <SliderWrapper
+                  index={index}
+                  onChange={this.handleSliderChange}
+                  value={item.value}
+                  percentage={item.percentage}
+                />
 
-        {this.state.list.map((item, index) => {
-          let newTokenList = this.getTokenList(this.state.list, item.symbol);
-          let tokenNumber = newTokenList.indexOf(item.symbol);
-
-          return (
-            <TokenInfo>
-              <DropDown items={newTokenList} active={tokenNumber} onChange={this.handleChange} />
-
-              <input
-                id={index}
-                value={this.state.list[index].percentage}
-                onChange={e => this.updatePercent(e, index)}
-                type="range"
-              />
-              <TextInput
-                id={index}
-                type="text"
-                value={this.state.list[index].percentage}
-                onChange={e => this.updatePercent(e, index)}
-              />
-            </TokenInfo>
-          );
-        })}
-
-        <DropDown
-          mode="secondary"
-          items={this.state.tokenList}
-          active={this.state.activeItem}
-          onChange={e => this.handleChange(e)}
-        />
+                <TextInputWrapper>
+                  <NumberInput id={index} type="text" value={item.value} onChange={e => this.updatePercent(e, index)} />
+                </TextInputWrapper>
+              </TokenInfo>
+            );
+          })}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-end'
+            }}
+          >
+            <Button mode="secondary" onClick={() => this.addToken()}>
+              + New token
+            </Button>
+          </div>
+        </div>
 
         <Info.Permissions title="Submission note">
           Your split contract will be updated on the blockchain, and you cannot request salary until it’s complete{' '}
@@ -115,21 +178,63 @@ class SidePanelContent extends React.Component {
   }
 }
 
+class SliderWrapper extends React.Component {
+  handleChange = value => {
+    console.log('value', value);
+    this.props.onChange(value, this.props.id);
+  };
+  render() {
+    const { value } = this.props;
+    console.log("this is in render sW", this.props.value)
+    return (
+      <div>
+        <Slider onUpdate={this.handleChange} value={value} />
+      </div>
+    );
+  }
+}
+
 const Container = styled.div`
-  height: 100vh;
   display: grid;
+  grid-template-rows: 100px 380px 100px 50px;
+  grid-gap: 30px;
 `;
 
 const TokenInfo = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 75px auto 70px;
   align-self: center;
-  flex-direction: row;
+
+  height: 40px;
+  margin-bottom: 32px;
+`;
+
+const TextInputWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  height: 40px;
+  &:after {
+    content: '%';
+    position: absolute;
+    right: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    pointer-events: none;
+    color: #b3b3b3;
+  }
 `;
 
 const NumberInput = styled(TextInput)`
-align-content: center;
-width: 70px;
-padding-left: 15px;
-`
+  align-content: center;
+  width: 70px;
+  padding-left: 16px;
+  height: 40px;
+`;
+
+const SliderHeader = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  margin: 20px 0px 10px 0px;
+`;
 
 export default SidePanelContent;
---
+++ templates/dev-payroll/ipfs.js
@@ -0,0 +1,4 @@
+module.exports = {
+  finance: 'ipfs:QmVx4gi6VCoxYHQjZBdgoVHT1oG1faYcbi8ERpMGVc7Ag6',  
+  vault: 'ipfs:QmPhd9aQGGoHJNcVVAmbzudShvhpYmyKxLWgqLoqSGD81G',  
+}
