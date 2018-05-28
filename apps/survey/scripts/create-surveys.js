3876094f2a6044b52cefb6714277617507c93e72
Luke Duncan <luke@lukeduncan.net>
2018-05-28 14:08:08 -0300

    Added suggested initial surveys

--- apps/survey/scripts/create-surveys.js
+++ apps/survey/scripts/create-surveys.js
@@ -23,6 +23,30 @@ const surveys = [
 	}
 ]
 
+const surveys = [
+	{
+		question: "What type of grants would you like Aragon Nest to fund in the future?"
+		description: "",
+		options: [ "Aragon Apps", "Ethereum user experience", "Ethereum scalability","Dev Tooling for Aragon and Ethereum ecosystems","None of the above" ],
+	}
+]
+
+const surveys = [
+	{
+		question: "Do your values align with those described in the Aragon Manifesto?"
+		description: "",
+		options: [ "Yes", "No" ],
+	}
+]
+
+const surveys = [
+	{
+		question: "Do you support EIP999?"
+		description: "",
+		options: [ "Yes", "No", "I do not feel strongly one way or the other" ],
+	}
+]
+
 module.exports = async (callback) => {
 	const app = artifacts.require('Survey').at(SURVEY_APP)
 	const receipts = await Promise.all(surveys.map(s => app.newSurvey(getMetadata(s), s.options.length)))
