7de2d637b1e1cd8503b88c098daf43701b60d64f
Jorge Izquierdo <jorge@izqui.me>
2018-05-28 15:33:45 -0300

    Final survey creation script

--- apps/survey/scripts/create-surveys.js
+++ apps/survey/scripts/create-surveys.js
@@ -17,26 +17,26 @@ const getMetadata = ({ question, description, options, url }) => {
 
 const surveys = [
 	{
-		question: "How should the following features be prioritized?"
-		description: "",
+		question: "How should the following features be prioritized?",
+		description: "This proposals is intended to discuss and gauge sentiment of the community on how to prioritize between 3 short term features for Aragon Core.",
 		options: [ "Identity", "Permissions app", "App Manager" ],
 		url: "https://github.com/aragon/governance/issues/19",
 	},
 	{
-		question: "What type of grants would you like Aragon Nest to fund in the future?"
-		description: "",
+		question: "What type of grants would you like Aragon Nest to fund in the future?",
+		description: "We would like to hear from the community what types of grants we should be pursued by the Nest program.",
 		options: [ "Aragon Apps", "Ethereum user experience", "Ethereum scalability", "Dev Tooling for Aragon and Ethereum ecosystems", "None of the above" ],
 		url: "https://github.com/aragon/governance/issues/20"
 	},
 	{
-		question: "Do your values align with those described in the Aragon Manifesto?"
-		description: "",
+		question: "Do your values align with those described in the Aragon Manifesto?",
+		description: "We would like to understand if the Aragon Manifesto as presented by the Aragon One Team aligns well with the Aragon community.",
 		options: [ "Yes", "No" ],
 		url: "https://github.com/aragon/governance/issues/21",
 	},
 	{
-		question: "Do you support EIP999?"
-		description: "",
+		question: "Do you support EIP999?",
+		description: "We would like to understand how ANT holders feel about EIP999, as it is a significant discussion within the wider Ethereum community that impacts the Aragon project and its community members. ",
 		options: [ "Yes", "No", "I do not feel strongly one way or the other" ],
 		url: "https://github.com/aragon/governance/issues/22",
 	}
