e1176c556d54ac3cd72ea20abf4019307f848671
Jorge Izquierdo <jorge@aragon.one>
2018-05-28 14:16:32 -0300

    Survey schema fix

--- apps/survey/scripts/create-surveys.js
+++ apps/survey/scripts/create-surveys.js
@@ -3,15 +3,15 @@ const namehash = require('eth-ens-namehash').hash
 const SURVEY_APP = process.env.SURVEY || '0xffe447378285b1637305f4f0d05639afe156791d' 
 
 const getMetadata = ({ question, description, options, url }) => {
-    const metadata = {
-      specId: namehash('1.metadata.survey.aragonpm.eth'),
-      metadata: {
-        question,
-        description,
-        options,
-        url,
-      }
-    }
+	const metadata = {
+		specId: namehash('1.metadata.survey.aragonpm.eth'),
+		metadata: {
+			question,
+			description,
+			options,
+			url,
+		}
+	}
 	return JSON.stringify(metadata)
 }
 
@@ -20,26 +20,17 @@ const surveys = [
 		question: "How should Aragon One prioritize the following features?"
 		description: "",
 		options: [ "Identity", "Permissions app", "App Manager" ],
-	}
-]
-
-const surveys = [
+	},
 	{
 		question: "What type of grants would you like Aragon Nest to fund in the future?"
 		description: "",
-		options: [ "Aragon Apps", "Ethereum user experience", "Ethereum scalability","Dev Tooling for Aragon and Ethereum ecosystems","None of the above" ],
-	}
-]
-
-const surveys = [
+		options: [ "Aragon Apps", "Ethereum user experience", "Ethereum scalability", "Dev Tooling for Aragon and Ethereum ecosystems", "None of the above" ],
+	},
 	{
 		question: "Do your values align with those described in the Aragon Manifesto?"
 		description: "",
 		options: [ "Yes", "No" ],
-	}
-]
-
-const surveys = [
+	},
 	{
 		question: "Do you support EIP999?"
 		description: "",
