274444e77caaf2ffe5b29c93a22dc45b4dec536d
ßingen <bingen@aragon.one>
2018-06-06 23:49:32 +0200

    Payroll: Use new Passive Price Feed

--- future-apps/payroll/test/payroll.js
+++ future-apps/payroll/test/payroll.js
@@ -5,38 +5,40 @@ const MiniMeToken = artifacts.require('@aragon/os/contracts/common/MiniMeToken')
 const Vault = artifacts.require('Vault');
 const Finance = artifacts.require('Finance');
 const Payroll = artifacts.require("PayrollMock");
-const OracleMock = artifacts.require("./oracle/OracleMock.sol");
-const OracleFailMock = artifacts.require("./oracle/OracleFailMock.sol");
+const PriceFeedMock = artifacts.require("./feed/PriceFeedMock.sol");
+const PriceFeedFailMock = artifacts.require("./feed/PriceFeedFailMock.sol");
 const Zombie = artifacts.require("Zombie.sol");
 
 contract('Payroll', function(accounts) {
+  const rateExpiryTime = 1000;
+  const USD_PRECISION = 10**18;
+  const SECONDS_IN_A_YEAR = 31557600; // 365.25 days
+  const ONE = 1e18
+  const ETH = '0x0'
   let payroll;
   let payroll2;
   let finance;
   let vault;
   let owner = accounts[0];
-  let oracle;
+  let priceFeed;
   let employee1_1 = accounts[2];
   let employee1 = employee1_1;
   let employee2 = accounts[3];
   let employee1_2 = accounts[4];
   let unused_account = accounts[7];
   let total_salary = 0;
-  let salary1_1 = 100000;
-  let salary1_2 = 110000;
+  let salary1_1 = (new web3.BigNumber(100000)).times(USD_PRECISION);
+  let salary1_2 = (new web3.BigNumber(110000)).times(USD_PRECISION);
   let salary1 = salary1_1;
-  let salary2_1 = 120000;
-  let salary2_2 = 125000;
+  let salary2_1 = (new web3.BigNumber(120000)).times(USD_PRECISION);
+  let salary2_2 = (new web3.BigNumber(125000)).times(USD_PRECISION);
   let salary2 = salary2_1;
   let usdToken;
   let erc20Token1;
   let erc20Token2;
-  let erc20Token1ExchangeRate = web3.toWei(5, 'gwei');
-  let erc20Token2ExchangeRate = web3.toWei(300, 'mwei');
-  let etherExchangeRate = web3.toWei(1, 'mwei')
-  const USD_PRECISION = 10**9;
-  const SECONDS_IN_A_YEAR = 31557600; // 365.25 days
-  const ETH = '0x0'
+  let erc20Token1ExchangeRate
+  let erc20Token2ExchangeRate
+  let etherExchangeRate
 
   const deployErc20Token = async (name="ERC20Token") => {
     let token = await MiniMeToken.new("0x0", "0x0", 0, name, 18, 'E20', true); // dummy parameters for minime
@@ -65,59 +67,56 @@ contract('Payroll', function(accounts) {
     return new Promise(resolve => {resolve(timePassed);});
   };
 
-  it("deploys and initializes contract", async () => {
-
+  before(async () => {
     vault = await Vault.new();
     await vault.initializeWithBase(vault.address)
     finance = await Finance.new();
     await finance.initialize(vault.address, 100);
-    payroll = await Payroll.new();
+
     usdToken = await deployErc20Token("USD");
-    oracle = await OracleMock.new();
-    await payroll.initialize(finance.address, usdToken.address);
+    priceFeed = await PriceFeedMock.new();
+    payroll = await Payroll.new();
+
+    // Deploy ERC 20 Tokens
+    erc20Token1 = await deployErc20Token();
+    erc20Token2 = await deployErc20Token();
+
+    // get exchange rates
+    erc20Token1ExchangeRate = (await priceFeed.get(usdToken.address, erc20Token1.address))[0]
+    erc20Token2ExchangeRate = (await priceFeed.get(usdToken.address, erc20Token2.address))[0]
+    etherExchangeRate = (await priceFeed.get(usdToken.address, ETH))[0]
+
     // transfer ETH to Payroll contract
     for (let i = 1; i < 9; i++)
       await finance.sendTransaction({ from: accounts[i], value: web3.toWei(90, 'ether') });
-  });
+  })
 
-  it('fails on reinitialization', async () => {
+  it("fails to initialize with empty finance", async () => {
     return assertRevert(async () => {
-      await payroll.initialize(finance.address, usdToken.address);
-    });
-  });
+      await payroll.initialize('0x0', usdToken.address, priceFeed.address, rateExpiryTime);
+    })
+  })
 
-  it("checks that initial values match", async () => {
+  it("initializes contract and checks that initial values match", async () => {
+    await payroll.initialize(finance.address, usdToken.address, priceFeed.address, rateExpiryTime);
     let tmpFinance = await payroll.finance();
     assert.equal(tmpFinance.valueOf(), finance.address, "Finance address is wrong");
     let tmpUsd = await payroll.denominationToken();
     assert.equal(tmpUsd.valueOf(), usdToken.address, "USD Token address is wrong");
   });
 
-  it("deploys tokens", async () => {
-    let totalSupply;
+  it('fails on reinitialization', async () => {
+    return assertRevert(async () => {
+      await payroll.initialize(finance.address, usdToken.address, priceFeed.address, rateExpiryTime);
+    });
+  });
 
-    const setAndCheckRate = async (payroll, token, exchangeRate, name='') => {
-      await oracle.setRate(payroll.address, token.address, exchangeRate);
-      let result = await payroll.getExchangeRate(token.address);
-      assert.equal(result.toString(), exchangeRate.toString(), "Exchange rate for " + name + " doesn't match!");
-    };
-
-    // ERC 20 Tokens
-    erc20Token1 = await deployErc20Token();
-    erc20Token2 = await deployErc20Token();
-
+  it("add allowed tokens", async () => {
     // add them to payroll allowed tokens
     await addAllowedTokens(payroll, [usdToken, erc20Token1, erc20Token2])
-
-    // finally set exchange rates
-    await oracle.setRate(payroll.address, ETH, etherExchangeRate);
-    let tmpEthExchange = await payroll.getExchangeRate(ETH);
-    assert.equal(tmpEthExchange.toString(), etherExchangeRate.toString(), "Exchange rate for Ether doesn't match!");
-    let tmpUsdExchange = await payroll.getExchangeRate(usdToken.address);
-    // Denomination Token (USD in this test) rate should be always 1
-    assert.equal(tmpUsdExchange.valueOf(), 1, "Exchange rate for USD Token doesn't match!");
-    await setAndCheckRate(payroll, erc20Token1, erc20Token1ExchangeRate, "ERC20 Token 1");
-    await setAndCheckRate(payroll, erc20Token2, erc20Token2ExchangeRate, "ERC 20 Token 2");
+    assert.isTrue(await payroll.isTokenAllowed(usdToken.address), "USD Token should be allowed")
+    assert.isTrue(await payroll.isTokenAllowed(erc20Token1.address), "ERC 20 Token 1 should be allowed")
+    assert.isTrue(await payroll.isTokenAllowed(erc20Token2.address), "ERC 20 Token 2 should be allowed")
   });
 
   it("fails trying to add an already allowed token", async () => {
@@ -126,14 +125,8 @@ contract('Payroll', function(accounts) {
     });
   });
 
-  it("fails trying to set rate of denomination token", async () => {
-    return assertRevert(async () => {
-      await oracle.setRate(payroll.address, usdToken.address, 999);
-    });
-  });
-
   const convertAndRoundSalary = function (a) {
-    return Math.floor(Math.floor(a * USD_PRECISION / SECONDS_IN_A_YEAR) * SECONDS_IN_A_YEAR / USD_PRECISION);
+    return a.dividedToIntegerBy(SECONDS_IN_A_YEAR).times(SECONDS_IN_A_YEAR);
   };
 
   it("adds employee", async () => {
@@ -143,7 +136,7 @@ contract('Payroll', function(accounts) {
     salary1 = salary1_1;
     let employee = await payroll.getEmployee(employeeId);
     assert.equal(employee[0], employee1_1, "Employee account doesn't match");
-    assert.equal(employee[1].toString(), convertAndRoundSalary(salary1_1), "Employee salary doesn't match");
+    assert.equal(employee[1].toString(), convertAndRoundSalary(salary1_1).toString(), "Employee salary doesn't match");
     assert.equal(employee[2], name, "Employee name doesn't match");
   });
 
@@ -160,7 +153,7 @@ contract('Payroll', function(accounts) {
     salary2 = salary2_1;
     let employee = await payroll.getEmployee(employeeId);
     assert.equal(employee[0], employee2, "Employee account doesn't match");
-    assert.equal(employee[1].toString(), convertAndRoundSalary(salary2_1), "Employee salary doesn't match");
+    assert.equal(employee[1].toString(), convertAndRoundSalary(salary2_1).toString(), "Employee salary doesn't match");
     assert.equal(employee[2], name, "Employee name doesn't match");
   });
 
@@ -184,7 +177,7 @@ contract('Payroll', function(accounts) {
     salary2 = salary2_1;
     let employee = await payroll.getEmployee(employeeId);
     assert.equal(employee[0], employee2, "Employee account doesn't match");
-    assert.equal(employee[1].toString(), convertAndRoundSalary(salary2_1), "Employee salary doesn't match");
+    assert.equal(employee[1].toString(), convertAndRoundSalary(salary2_1).toString(), "Employee salary doesn't match");
     assert.equal(employee[2], name, "Employee name doesn't match");
   });
 
@@ -193,7 +186,7 @@ contract('Payroll', function(accounts) {
     await payroll.determineAllocation([usdToken.address], [100], {from: employee2});
     let initialBalance = await usdToken.balanceOf(employee2);
     let timePassed = await getTimePassed(employeeId);
-    let owed = (new web3.BigNumber(salary2)).times(USD_PRECISION).dividedToIntegerBy(SECONDS_IN_A_YEAR).times(100).dividedToIntegerBy(100).times(timePassed);
+    let owed = salary2.dividedToIntegerBy(SECONDS_IN_A_YEAR).times(timePassed);
     await payroll.removeEmployee(employeeId);
     salary2 = 0;
     let finalBalance = await usdToken.balanceOf(employee2);
@@ -213,7 +206,7 @@ contract('Payroll', function(accounts) {
     let transaction = await payroll.addEmployeeWithNameAndStartDate(employee2, salary2_2, name, Math.floor((new Date()).getTime() / 1000) - 2628600);
     let employee = await payroll.getEmployee(employeeId);
     assert.equal(employee[0], employee2, "Employee account doesn't match");
-    assert.equal(employee[1].toString(), convertAndRoundSalary(salary2_2), "Employee salary doesn't match");
+    assert.equal(employee[1].toString(), convertAndRoundSalary(salary2_2).toString(), "Employee salary doesn't match");
     assert.equal(employee[2], name, "Employee name doesn't match");
     salary2 = salary2_2;
   });
@@ -223,7 +216,7 @@ contract('Payroll', function(accounts) {
     await payroll.setEmployeeSalary(employeeId, salary1_2);
     salary1 = salary1_2;
     let employee = await payroll.getEmployee(employeeId);
-    assert.equal(employee[1].toString(), convertAndRoundSalary(salary1_2), "Salary doesn't match");
+    assert.equal(employee[1].toString(), convertAndRoundSalary(salary1_2).toString(), "Salary doesn't match");
   });
 
   it("fails modifying non-existent employee salary", async () => {
@@ -295,7 +288,7 @@ contract('Payroll', function(accounts) {
 
   it("fails on payday with no token allocation", async () => {
     payroll2 = await Payroll.new();
-    await payroll2.initialize(finance.address, usdToken.address);
+    await payroll2.initialize(finance.address, usdToken.address, priceFeed.address, rateExpiryTime);
     // add allowed tokens
     await addAllowedTokens(payroll2, [usdToken, erc20Token1])
     // make sure this payroll has enough funds
@@ -319,10 +312,10 @@ contract('Payroll', function(accounts) {
   });
 
   it("fails on payday with a zero exchange rate token", async () => {
-    let oracleFail = await OracleFailMock.new();
+    let priceFeedFail = await PriceFeedFailMock.new();
+    await payroll2.setPriceFeed(priceFeedFail.address)
     // Allocation
     await payroll2.determineAllocation([ETH, usdToken.address, erc20Token1.address], [10, 20, 70], {from: employee1_1});
-    await oracleFail.setRate(payroll2.address, ETH, 0);
     // Zero exchange rate
     return assertRevert(async () => {
       await payroll2.payday({from: employee1_1});
@@ -337,9 +330,8 @@ contract('Payroll', function(accounts) {
   });
 
   it("fails on payday after 0 seconds", async () => {
-    // correct oracle, make sure rates are correct
-    await oracle.setRate(payroll2.address, ETH, etherExchangeRate);
-    await oracle.setRate(payroll2.address, erc20Token1.address, erc20Token1ExchangeRate);
+    // correct priceFeed, make sure rates are correct
+    await payroll2.setPriceFeed(priceFeed.address)
     // correct payday
     await payroll2.payday({from: employee1_1});
     // payday called again too early: if 0 seconds have passed, payroll would be 0
@@ -507,7 +499,7 @@ contract('Payroll', function(accounts) {
     };
 
     const checkTokenBalances = async (token, salary, timePassed, initialBalancePayroll, initialBalanceEmployee, exchangeRate, allocation, name='') => {
-      let payed = (new web3.BigNumber(salary)).times(USD_PRECISION).dividedToIntegerBy(SECONDS_IN_A_YEAR).times(exchangeRate).times(allocation).dividedToIntegerBy(100).times(timePassed);
+      let payed = salary.dividedToIntegerBy(SECONDS_IN_A_YEAR).times(exchangeRate).times(allocation).times(timePassed).dividedToIntegerBy(100).dividedToIntegerBy(ONE)
       let expectedPayroll = initialBalancePayroll.minus(payed);
       let expectedEmployee = initialBalanceEmployee.plus(payed);
       let newBalancePayroll;
@@ -526,14 +518,14 @@ contract('Payroll', function(accounts) {
       let txFee = gasPrice.times(transaction.receipt.cumulativeGasUsed);
       let newEthPayroll = await getBalance(vault.address);
       let newEthEmployee2 = await getBalance(employee2);
-      let payed = (new web3.BigNumber(salary2)).times(USD_PRECISION).dividedToIntegerBy(SECONDS_IN_A_YEAR).times(etherExchangeRate).times(ethAllocation).dividedToIntegerBy(100).times(timePassed);
+      let payed = salary2.dividedToIntegerBy(SECONDS_IN_A_YEAR).times(etherExchangeRate).times(ethAllocation).times(timePassed).dividedToIntegerBy(100).dividedToIntegerBy(ONE)
       let expectedPayroll = initialEthPayroll.minus(payed);
       let expectedEmployee2 = initialEthEmployee2.plus(payed).minus(txFee);
       //logPayroll(salary2, initialEthPayroll, initialEthEmployee2, payed, newEthPayroll, newEthEmployee2, expectedPayroll, expectedEmployee2, "ETH");
       assert.equal(newEthPayroll.toString(), expectedPayroll.toString(), "Payroll Eth Balance doesn't match");
       assert.equal(newEthEmployee2.toString(), expectedEmployee2.toString(), "Employee Eth Balance doesn't match");
       // Check Tokens
-      await checkTokenBalances(usdToken, salary2, timePassed, initialUsdTokenPayroll, initialUsdTokenEmployee2, 1, usdTokenAllocation, "USD");
+      await checkTokenBalances(usdToken, salary2, timePassed, initialUsdTokenPayroll, initialUsdTokenEmployee2, ONE, usdTokenAllocation, "USD");
       await checkTokenBalances(erc20Token1, salary2, timePassed, initialErc20Token1Payroll, initialErc20Token1Employee2, erc20Token1ExchangeRate, erc20Token1Allocation, "ERC20 1");
     };
 
@@ -546,25 +538,64 @@ contract('Payroll', function(accounts) {
     let transaction = await payroll.payday({from: employee2});
     await checkPayday(transaction, timePassed);
 
-    // check that time restrictions for payday and determineAllocation work in a positive way (i.e. when time has gone by)
-
-    // payday
+    // check that we can call payday again after some time
     // set time forward, 1 month
-    let newTime = parseInt(await payroll.getTimestampPublic()) + 2678400; // 31 * 24 * 3600
-    await payroll.mockSetTimestamp(newTime); // 31 * 24 * 3600
+    let newTime = parseInt(await payroll.getTimestampPublic(), 10) + 2678400; // 31 * 24 * 3600
+    await payroll.mockSetTimestamp(newTime);
+    // we need to forward time in price feed, or rate will be obsolete
+    await priceFeed.mockSetTimestamp(newTime);
     await setInitialBalances();
     timePassed = await getTimePassed(employeeId); // get employee 2
     // call payday again
     let transaction2 = await payroll.payday({from: employee2});
     await checkPayday(transaction2, timePassed);
 
-    // determineAllocation
+    // check that time restriction for determineAllocation works in a positive way (i.e. when time has gone by)
     // set time forward, 5 more months
-    await payroll.mockSetTimestamp(parseInt(await payroll.getTimestampPublic()) + 13392000); // 5 * 31 * 24 * 3600
+    let newTime2 = parseInt(await payroll.getTimestampPublic(), 10) + 13392000; // 5 * 31 * 24 * 3600
+    await payroll.mockSetTimestamp(newTime2)
+    // we need to forward time in price feed, or rate will be obsolete
+    await priceFeed.mockSetTimestamp(newTime2);
     await payroll.determineAllocation([ETH, usdToken.address, erc20Token1.address], [15, 60, 25], {from: employee2});
     assert.equal((await payroll.getAllocation(ETH, {from: employee2})).valueOf(), 15, "ETH allocation doesn't match");
     assert.equal((await payroll.getAllocation(usdToken.address, {from: employee2})).valueOf(), 60, "USD allocation doesn't match");
     assert.equal((await payroll.getAllocation(erc20Token1.address, {from: employee2})).valueOf(), 25, "ERC 20 Token 1 allocation doesn't match");
   });
 
+  it('fails to pay if rates are obsolete', async () => {
+    const usdTokenAllocation = 50;
+    const erc20Token1Allocation = 20;
+    const ethAllocation = 100 - usdTokenAllocation - erc20Token1Allocation;
+    // determine allocation
+    await payroll.determineAllocation([ETH, usdToken.address, erc20Token1.address], [ethAllocation, usdTokenAllocation, erc20Token1Allocation], {from: employee2});
+    const employeeId = 4;
+    await getTimePassed(employeeId); // get employee 2
+    // set old date in price feed
+    const oldTime = parseInt(await payroll.getTimestampPublic(), 10) - rateExpiryTime - 1
+    await priceFeed.mockSetTimestamp(oldTime)
+    // call payday
+    return assertRevert(async () => {
+      await payroll.payday({from: employee2});
+    })
+
+  })
+
+  it('fails to change the price feed time to 0', async () => {
+    return assertRevert(async () => {
+      await payroll.setPriceFeed('0x0');
+    })
+  })
+
+  it('changes the rate expiry time', async () => {
+    const newTime = rateExpiryTime * 2;
+    await payroll.setRateExpiryTime(newTime);
+    assert.equal(await payroll.rateExpiryTime(), newTime);
+  })
+
+  it('fails to change the rate expiry time to 0', async () => {
+    const newTime = 0;
+    return assertRevert(async () => {
+      await payroll.setRateExpiryTime(newTime);
+    })
+  })
 });
