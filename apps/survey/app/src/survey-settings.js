05bf9e858e5234429f13bda22b7db9f79cba2f9e
Pierre Bertet <hello@pierre.world>
2018-06-06 14:06:03 +0100

    Survey app (#310)

---
+++ apps/survey/app/src/survey-settings.js
@@ -0,0 +1,18 @@
+const surveySettings = [
+  ['token', 'tokenAddress'],
+  ['minParticipationPct', 'minParticipationPct', 'number'],
+  ['PCT_BASE', 'pctBase', 'number'],
+  ['surveyTime', 'surveyTime', 'time'],
+]
+
+export function hasLoadedSurveySettings(state) {
+  state = state || {}
+  return surveySettings.reduce(
+    (loaded, [_, key]) => loaded && !!state[key],
+    true
+  )
+}
+
+export const DURATION_SLICES = 16
+
+export default surveySettings
