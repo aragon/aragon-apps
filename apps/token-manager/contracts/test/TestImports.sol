62b8378f8db03e7e6aca71fccf0b419f9d50a64c
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-28 12:33:44 +0200

    tests: re-order imports in TestImports

--- apps/token-manager/contracts/test/TestImports.sol
+++ apps/token-manager/contracts/test/TestImports.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.4.24;
 
-import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
-import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/kernel/Kernel.sol";
+import "@aragon/os/contracts/factory/DAOFactory.sol";
+import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 
 import "@aragon-apps/minime/contracts/MiniMeToken.sol";
 import "@aragon-apps/migrations/contracts/Migrations.sol";
