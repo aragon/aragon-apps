ddcc56fc4069db0f3fc513cf854f26d3c0fcace6
ßingen <bingen@aragon.one>
2018-08-31 13:05:28 +0200

    Templates: remove them
    Deprecated in favor of external dao-kits repo.

--- templates/dev/ipfs.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  finance: 'ipfs:QmZzLQdMjALTNc2aJRa4iyUxXadZZQnxWe82bYT8EGZ5nk',
-  tokenManager: 'ipfs:QmQ94bjem1wcBB2UFeHFRa59D2DSYDzft43DRytDREEbAv',
-  vault: 'ipfs:QmfWoMrEWMaskEFssgUCuTeG1B4y9BC9DG6zi2jtCvWaYg',
-  voting: 'ipfs:QmfX6udPSMuadNtKHAZKPdf92hxpcGykvWWE6cgLAKjx7Z',
-}
