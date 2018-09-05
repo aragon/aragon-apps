dfae97a53c2841bd34f6e6029118d515ebfcee43
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-05 16:07:54 +0200

    Address PR comments

--- apps/vault/contracts/Vault.sol
+++ apps/vault/contracts/Vault.sol
@@ -1,11 +1,11 @@
 pragma solidity 0.4.24;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
-
+import "@aragon/os/contracts/common/EtherTokenConstant.sol";
 import "@aragon/os/contracts/lib/token/ERC20.sol";
 
 
-contract Vault is AragonApp {
+contract Vault is EtherTokenConstant, AragonApp {
     bytes32 constant public TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
 
     event Transfer(address indexed token, address indexed to, uint256 amount);
