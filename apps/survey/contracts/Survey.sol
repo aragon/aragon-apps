b99c093621372c2cef917ba373f7e2cec74f0a91
ßingen <bingen@aragon.one>
2018-09-14 17:27:06 +0200

    Survey: add tests for out of bounds in getOptionPower
    Rebase and update to last changes too.

--- apps/survey/contracts/Survey.sol
+++ apps/survey/contracts/Survey.sol
@@ -290,10 +290,10 @@ contract Survey is AragonApp {
     }
 
     function getOptionPower(uint256 _surveyId, uint256 _optionId) public view surveyExists(_surveyId) returns (uint256) {
-        Survey storage survey_ = surveys[_surveyId];
-        require(_optionId <= survey_.options);
+        SurveyStruct storage survey = surveys[_surveyId];
+        require(_optionId <= survey.options);
 
-        return survey_.optionPower[_optionId];
+        return survey.optionPower[_optionId];
     }
 
     function isParticipationAchieved(uint256 _surveyId) public view surveyExists(_surveyId) returns (bool) {
