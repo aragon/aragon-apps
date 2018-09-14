e9387b822bc9048a837561144d4cc7290e83edea
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-14 16:57:04 +0300

    Fix most tests

--- apps/voting/contracts/test/mocks/VotingMockExt.sol
+++ apps/voting/contracts/test/mocks/VotingMockExt.sol
@@ -6,18 +6,21 @@ import "../../Voting.sol";
 contract VotingMockExt {
     Voting voting;
 
-    constructor (Voting _voting) {
+    event StartVote(uint256 indexed voteId);
+
+    constructor (Voting _voting) public {
         voting = _voting;
     }
 
-    /** Ugly hack to work around this issue:
+    /* Ugly hack to work around this issue:
      * https://github.com/trufflesuite/truffle/issues/569
      * https://github.com/trufflesuite/truffle/issues/737
      */
-    function newVote(bytes _executionScript, string _metadata)
+    function newVote(bytes _executionScript, string _metadata, bool _castVote, bool _executesIfDecided)
         external
         returns (uint256 voteId)
     {
-        return voting.newVote(_executionScript, _metadata);
+        voteId = voting.newVote(_executionScript, _metadata, _castVote, _executesIfDecided);
+        emit StartVote(voteId);
     }
 }
