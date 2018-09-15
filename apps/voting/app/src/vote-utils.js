4dfb2ee00402e0d935af3236c53fdb8fd5ac31f0
Pierre Bertet <hello@pierre.world>
2018-09-15 19:09:14 +0100

    Vote utils: get a vote type from the contract enum

--- apps/voting/app/src/vote-utils.js
+++ apps/voting/app/src/vote-utils.js
@@ -1,6 +1,8 @@
 import { safeDiv } from './math-utils'
 import {
   VOTE_ABSENT,
+  VOTE_YEA,
+  VOTE_NAY,
   VOTE_STATUS_ONGOING,
   VOTE_STATUS_REJECTED,
   VOTE_STATUS_ACCEPTED,
@@ -27,6 +29,18 @@ export const getVoteStatus = (vote, support, quorum) => {
   return hasSupport && hasMinQuorum
     ? VOTE_STATUS_ACCEPTED
     : VOTE_STATUS_REJECTED
+}
+
+// Enums are not supported by the ABI yet:
+// https://solidity.readthedocs.io/en/latest/frequently-asked-questions.html#if-i-return-an-enum-i-only-get-integer-values-in-web3-js-how-to-get-the-named-values
+export function voteTypeFromContractEnum(value) {
+  if (value === '1') {
+    return VOTE_YEA
+  }
+  if (value === '2') {
+    return VOTE_NAY
+  }
+  return VOTE_ABSENT
 }
 
 export const getQuorumProgress = ({ yea, totalVoters }) =>
