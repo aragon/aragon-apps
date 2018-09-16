2f7223aca59d801674ce3fb019e135414b3e12c7
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-17 01:09:31 +0300

    Revert changes to Vault

--- apps/vault/contracts/Vault.sol
+++ apps/vault/contracts/Vault.sol
@@ -6,14 +6,14 @@ import "@aragon/os/contracts/lib/token/ERC20.sol";
 
 
 contract Vault is EtherTokenConstant, AragonApp {
-    bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+    bytes32 constant public TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
 
     event Transfer(address indexed token, address indexed to, uint256 amount);
     event Deposit(address indexed token, address indexed sender, uint256 amount);
 
     /**
     * @dev On a normal send() or transfer() this fallback is never executed as it will be
-    *      intercepted by the Proxy (see aragonOS#281)
+    * intercepted by the Proxy (see aragonOS#281)
     */
     function () external payable {
         require(msg.data.length == 0);
@@ -29,78 +29,78 @@ contract Vault is EtherTokenConstant, AragonApp {
     }
 
     /**
-    * @notice Deposit `_value` `_token` to the vault
-    * @param _token Address of the token being transferred
-    * @param _from Entity that currently owns the tokens
-    * @param _value Amount of tokens being transferred
+    * @notice Deposit `value` `token` to the vault
+    * @param token Address of the token being transferred
+    * @param from Entity that currently owns the tokens
+    * @param value Amount of tokens being transferred
     */
-    function deposit(address _token, address _from, uint256 _value) public payable isInitialized {
-        require(_value > 0);
-        require(msg.sender == _from);
+    function deposit(address token, address from, uint256 value) public isInitialized payable {
+        require(value > 0);
+        require(msg.sender == from);
 
-        if (_token == ETH) {
+        if (token == ETH) {
             // Deposit is implicit in this case
-            require(msg.value == _value);
+            require(msg.value == value);
         } else {
-            require(ERC20(_token).transferFrom(_from, this, _value));
+            require(ERC20(token).transferFrom(from, this, value));
         }
 
-        emit Deposit(_token, _from, _value);
+        emit Deposit(token, from, value);
     }
 
     /*
-    * TODO: Function could be brought back when https://github.com/ethereum/solidity/issues/526 is fixed
-    * @notice Deposit `_value` `_token` to the vault
-    * @param _token Address of the token being transferred
-    * @param _from Entity that currently owns the tokens
-    * @param _value Amount of tokens being transferred
-    * @param _data Extra data associated with the deposit (currently unused)
-    function deposit(address _token, address _from, uint256 _value, bytes _data) external isInitialized payable {
-        _deposit(_token, _from, _value);
+    TODO: Function could be brought back when https://github.com/ethereum/solidity/issues/526 is fixed
+    * @notice Deposit `value` `token` to the vault
+    * @param token Address of the token being transferred
+    * @param from Entity that currently owns the tokens
+    * @param value Amount of tokens being transferred
+    * @param data Extra data associated with the deposit (currently unused)
+    function deposit(address token, address from, uint256 value, bytes data) payable external {
+        deposit(token, from, value);
     }
     */
 
     /**
-    * @notice Transfer `_value` `_token` from the Vault to `_to`
-    * @param _token Address of the token being transferred
-    * @param _to Address of the recipient of tokens
-    * @param _value Amount of tokens being transferred
+    * @notice Transfer `value` `token` from the Vault to `to`
+    * @param token Address of the token being transferred
+    * @param to Address of the recipient of tokens
+    * @param value Amount of tokens being transferred
     */
-    /* solium-disable-next-line function-order */
-    function transfer(address _token, address _to, uint256 _value)
+    /* solium-disable function-order */
+    function transfer(address token, address to, uint256 value)
         external
-        authP(TRANSFER_ROLE, arr(_token, _to, _value))
+        authP(TRANSFER_ROLE, arr(token, to, value))
     {
-        transfer(_token, _to, _value, new bytes(0));
+        transfer(token, to, value, new bytes(0));
     }
 
     /**
-    * @notice Transfer `_value` `_token` from the Vault to `_to`
-    * @param _token Address of the token being transferred
-    * @param _to Address of the recipient of tokens
-    * @param _value Amount of tokens being transferred
-    * @param _data Extra data associated with the transfer (only used for ETH)
+    * @notice Transfer `value` `token` from the Vault to `to`
+    * @param token Address of the token being transferred
+    * @param to Address of the recipient of tokens
+    * @param value Amount of tokens being transferred
+    * @param data Extra data associated with the transfer (only used for ETH)
     */
-    function transfer(address _token, address _to, uint256 _value, bytes _data)
+    function transfer(address token, address to, uint256 value, bytes data)
         public
-        authP(TRANSFER_ROLE, arr(_token, _to, _value))
+        authP(TRANSFER_ROLE, arr(token, to, value))
     {
-        require(_value > 0);
+        require(value > 0);
 
-        if (_token == ETH) {
-            require(_to.call.value(_value)(_data));
+        if (token == ETH) {
+            require(to.call.value(value)(data));
         } else {
-            require(ERC20(_token).transfer(_to, _value));
+            require(ERC20(token).transfer(to, value));
         }
 
-        emit Transfer(_token, _to, _value);
+        emit Transfer(token, to, value);
     }
 
-    function balance(address _token) public view returns (uint256) {
-        if (_token == ETH) {
+    function balance(address token) public view returns (uint256) {
+        if (token == ETH) {
             return address(this).balance;
         } else {
-            return ERC20(_token).balanceOf(this);
+            return ERC20(token).balanceOf(this);
         }
     }
 
