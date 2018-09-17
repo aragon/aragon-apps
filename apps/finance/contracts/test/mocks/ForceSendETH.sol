b5f409884c3cb1be3f95c1856d575ffe31bd49ea
Jorge Izquierdo <jorge@izqui.me>
2018-09-17 14:54:28 +0200

    Fix linter

--- apps/finance/contracts/test/mocks/ForceSendETH.sol
+++ apps/finance/contracts/test/mocks/ForceSendETH.sol
@@ -5,7 +5,7 @@ contract ForceSendETH {
   // Truffle doesn't support selfdestructing on constructor:
   // Error: The contract code couldn't be stored, please check your gas amount.
 
-  function sendByDying(address recipient) payable {
+  function sendByDying(address recipient) external payable {
     selfdestruct(recipient);
   }
 }
