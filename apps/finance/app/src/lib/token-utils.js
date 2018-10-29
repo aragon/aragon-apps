fe33b799ff49888bee38b9cc78419f9f059bf9c9
Brett Sun <qisheng.brett.sun@gmail.com>
2018-10-29 19:11:54 +0100

    Fix verified token comparision with checksummed addresses

--- apps/finance/app/src/lib/token-utils.js
+++ apps/finance/app/src/lib/token-utils.js
@@ -4,4 +4,7 @@ export const ETHER_TOKEN_FAKE_ADDRESS =
   '0x0000000000000000000000000000000000000000'
 
 export const isTokenVerified = (tokenAddress, networkType) =>
-  networkType === 'mainnet' ? ETHER_TOKEN_VERIFIED_LIST.has(tokenAddress) : true
+  networkType !== 'mainnet'
+    // The verified list is without checksums
+    ? ETHER_TOKEN_VERIFIED_LIST.has(tokenAddress.toLowerCase())
+    : true
