9361d66ab069e1cf74631e28e2be75d055f14bb4
Brett Sun <qisheng.brett.sun@gmail.com>
2018-10-30 06:28:38 +0100

    Finance: add custom token deposits

--- apps/finance/app/src/components/NewTransfer/Deposit.js
+++ apps/finance/app/src/components/NewTransfer/Deposit.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import BN from 'bn.js'
 import {
   Button,
-  DropDown,
   Field,
   IconCross,
   IdentityBadge,
   Info,
+  SafeLink,
   Text,
   TextInput,
   theme,
 } from '@aragon/ui'
 import QRCode from 'qrcode.react'
+import tokenBalanceOfAbi from '../../abi/token-balanceof.json'
+import tokenDecimalsAbi from '../../abi/token-decimals.json'
+import tokenSymbolAbi from '../../abi/token-symbol.json'
+import { fromDecimals, toDecimals } from '../../lib/math-utils'
+import provideNetwork from '../../lib/provideNetwork'
+import { ETHER_TOKEN_FAKE_ADDRESS } from '../../lib/token-utils'
+import { addressesEqual, isAddress } from '../../lib/web3-utils'
+import { combineLatest } from '../../rxjs'
 import ToggleContent from '../ToggleContent'
-import { toDecimals } from '../../lib/math-utils'
-import { addressPattern, isAddress } from '../../lib/web3-utils'
+import TokenBadge from './TokenBadge'
+import TokenSelector from './TokenSelector'
 
 const NO_ERROR = Symbol('NO_ERROR')
 const BALANCE_NOT_ENOUGH_ERROR = Symbol('BALANCE_NOT_ENOUGH_ERROR')
 const DECIMALS_TOO_MANY_ERROR = Symbol('DECIMALS_TOO_MANY_ERROR')
+const TOKEN_NOT_FOUND_ERROR = Symbol('TOKEN_NOT_FOUND_ERROR')
+
+const MAINNET_RISKS_BLOG_POST =
+  'https://blog.aragon.org/aragon-06-is-live-on-mainnet'
+const TOKEN_ALLOWANCE_WEBSITE = 'https://tokenallowance.io/'
+
+const tokenAbi = [].concat(tokenBalanceOfAbi, tokenDecimalsAbi, tokenSymbolAbi)
 
 const initialState = {
   amount: {
@@ -26,7 +42,15 @@ const initialState = {
     value: '',
   },
   reference: '',
-  selectedToken: 0,
+  selectedToken: {
+    data: {
+      loading: false,
+    },
+    error: NO_ERROR,
+    index: 0,
+    value: '',
+  },
+  showTokenBadge: false,
 }
 
 class Deposit extends React.Component {
@@ -37,78 +61,230 @@ class Deposit extends React.Component {
     ...initialState,
   }
   handleAmountUpdate = event => {
-    this.setState({
+    this.validateInputs({
       amount: {
-        error: NO_ERROR,
         value: event.target.value,
       },
+      selectedToken: this.state.selectedToken,
     })
   }
-  handleSelectToken = index => {
-    this.setState({ selectedToken: index })
+  handleSelectToken = async ({ address, index, value }) => {
+    const tokenIsAddress = isAddress(address)
+    const selectedToken = {
+      index,
+      coerced: tokenIsAddress && address !== value,
+      value: address,
+      data: { loading: true },
+    }
+
+    if (!tokenIsAddress) {
+      this.validateInputs({
+        selectedToken,
+        amount: this.state.amount,
+      })
+      return
+    }
+
+    // Set the initial loading state before we go async
+    this.setState({ selectedToken })
+
+    const tokenData = await this.loadTokenData(address)
+
+    // Make sure we still want the information about this token after the async call,
+    // in case the token was changed before this finished loading
+    if (this.state.selectedToken.value === address) {
+      this.validateInputs({
+        amount: this.state.amount,
+        selectedToken: {
+          ...this.state.selectedToken,
+          data: {
+            ...tokenData,
+            loading: false,
+          },
+        },
+      })
+    }
   }
   handleReferenceUpdate = event => {
     this.setState({ reference: event.target.value })
   }
   handleSubmit = event => {
     event.preventDefault()
-    const { onDeposit, tokens } = this.props
+    const { onDeposit } = this.props
     const { amount, reference, selectedToken } = this.state
-    const token = tokens[selectedToken]
 
-    // Adjust but without truncation in case the user entered a value with more
-    // decimals than possible
-    const adjustedAmount = toDecimals(amount.value, token.decimals, {
-      truncate: false,
-    })
+    if (this.validateInputs({ amount, selectedToken })) {
+      const adjustedAmount = toDecimals(
+        amount.value,
+        selectedToken.data.decimals
+      )
+      onDeposit(selectedToken.value, adjustedAmount, reference)
+    }
+  }
+  canSubmit() {
+    const { selectedToken } = this.state
+    return selectedToken.value && !selectedToken.data.loading
+  }
+  loadTokenData(address) {
+    const { app, userAccount } = this.props
 
-    if (adjustedAmount.indexOf('.') !== -1) {
-      this.setState(({ amount }) => ({
-        amount: { ...amount, error: DECIMALS_TOO_MANY_ERROR },
-      }))
-      return
+    // ETH
+    if (addressesEqual(address, ETHER_TOKEN_FAKE_ADDRESS)) {
+      return new Promise((resolve, reject) =>
+        app
+          .web3Eth('getBalance', userAccount)
+          .first()
+          .subscribe(
+            ethBalance =>
+              resolve({
+                decimals: 18,
+                loading: false,
+                symbol: 'ETH',
+                userBalance: ethBalance,
+              }),
+            reject
+          )
+      )
     }
 
-    onDeposit(token.address, adjustedAmount, reference)
+    // Tokens
+    const token = app.external(address, tokenAbi)
+
+    return new Promise((resolve, reject) =>
+      combineLatest(
+        token.symbol(),
+        token.decimals(),
+        token.balanceOf(userAccount)
+      )
+        .first()
+        .subscribe(
+          ([symbol, decimals, userBalance]) =>
+            resolve({
+              symbol,
+              userBalance,
+              decimals: parseInt(decimals, 10),
+              loading: false,
+            }),
+          reject
+        )
+    )
+  }
+  validateInputs({ amount, selectedToken }) {
+    if (selectedToken.value && !isAddress(selectedToken.value)) {
+      this.setState(({ amount }) => ({
+        amount: { ...amount },
+        selectedToken: { ...selectedToken, error: TOKEN_NOT_FOUND_ERROR },
+      }))
+      return false
+    }
+
+    if (amount.value && selectedToken.data.decimals) {
+      // Adjust but without truncation in case the user entered a value with more
+      // decimals than possible
+      const adjustedAmount = toDecimals(
+        amount.value,
+        selectedToken.data.decimals,
+        {
+          truncate: false,
+        }
+      )
+
+      if (adjustedAmount.indexOf('.') !== -1) {
+        this.setState(({ amount }) => ({
+          amount: { ...amount, error: DECIMALS_TOO_MANY_ERROR },
+          selectedToken: { ...selectedToken },
+        }))
+        return false
+      }
+
+      if (
+        selectedToken.data.userBalance &&
+        new BN(adjustedAmount).gt(new BN(selectedToken.data.userBalance))
+      ) {
+        this.setState({
+          amount: { ...amount, error: BALANCE_NOT_ENOUGH_ERROR },
+          selectedToken: { ...selectedToken },
+        })
+        return false
+      }
+    }
+
+    // No errors! Set the new state with no errors!
+    this.setState({
+      amount: {
+        ...amount,
+        error: NO_ERROR,
+      },
+      selectedToken: {
+        ...selectedToken,
+        error: NO_ERROR,
+      },
+    })
+    return true
   }
 
   render() {
-    const { title, tokens, proxyAddress } = this.props
+    const { network, title, tokens, proxyAddress } = this.props
     const { amount, reference, selectedToken } = this.state
-    const symbols = tokens.map(({ symbol }) => symbol)
 
     let errorMessage
-    if (amount.error === BALANCE_NOT_ENOUGH_ERROR) {
-      errorMessage = 'Amount is greater than balance held by vault'
+    if (selectedToken.error === TOKEN_NOT_FOUND_ERROR) {
+      errorMessage = 'Token not found'
+    } else if (amount.error === BALANCE_NOT_ENOUGH_ERROR) {
+      errorMessage = 'Amount is greater than balance held'
     } else if (amount.error === DECIMALS_TOO_MANY_ERROR) {
       errorMessage = 'Amount contains too many decimal places'
     }
+    const disabled = errorMessage || !this.canSubmit()
+
+    const selectedTokenIsAddress = isAddress(selectedToken.value)
+    const showTokenBadge = selectedTokenIsAddress && selectedToken.coerced
+    const tokenBalanceMessage = selectedToken.data.userBalance
+      ? `You have ${fromDecimals(
+          selectedToken.data.userBalance,
+          selectedToken.data.decimals
+        )} ${selectedToken.data.symbol} available`
+      : ''
+
+    const ethSelected =
+      selectedTokenIsAddress &&
+      addressesEqual(selectedToken.value, ETHER_TOKEN_FAKE_ADDRESS)
+    const tokenSelected = selectedToken.value && !ethSelected
+    const isMainnet = network.type === 'mainnet'
 
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>{title}</h1>
-        <AmountField>
-          <label>
-            <Text.Block color={theme.textSecondary} smallcaps>
-              Amount
-            </Text.Block>
-          </label>
-          <CombinedInput>
-            <TextInput.Number
-              value={amount.value}
-              onChange={this.handleAmountUpdate}
-              min={0}
-              step="any"
-              required
-              wide
-            />
-            <DropDown
-              items={symbols}
-              active={selectedToken}
-              onChange={this.handleSelectToken}
-            />
-          </CombinedInput>
-        </AmountField>
+        <TokenSelector
+          activeIndex={selectedToken.index}
+          onChange={this.handleSelectToken}
+          tokens={tokens}
+        />
+        {showTokenBadge && (
+          <TokenBadge
+            address={selectedToken.value}
+            symbol={selectedToken.data.symbol}
+          />
+        )}
+        <TokenBalance>
+          <Text
+            size="small"
+            color={theme.textSecondary}
+            style={{ opacity: selectedToken.data.userBalance ? 1 : 0 }}
+          >
+            {tokenBalanceMessage}
+          </Text>
+        </TokenBalance>
+        <Field label="Amount">
+          <TextInput.Number
+            value={amount.value}
+            onChange={this.handleAmountUpdate}
+            min={0}
+            step="any"
+            required
+            wide
+          />
+        </Field>
         <Field label="Reference (optional)">
           <TextInput
             onChange={this.handleReferenceUpdate}
@@ -117,7 +293,7 @@ class Deposit extends React.Component {
           />
         </Field>
         <ButtonWrapper>
-          <Button mode="strong" type="submit" wide>
+          <Button wide mode="strong" type="submit" disabled={disabled}>
             Submit deposit
           </Button>
         </ButtonWrapper>
@@ -125,42 +301,65 @@ class Deposit extends React.Component {
 
         <VSpace size={6} />
         <Info.Action title="Depositing funds to your organization">
-          <p>
-            Remember, Mainnet organizations use real (not test) funds. Learn
-            more about the risks and what's been done to mitigate them here.
-          </p>
-          <VSpace size={2} />
+          {isMainnet && (
+            <React.Fragment>
+              <p>
+                Remember, Mainnet organizations use real (not test) funds.{' '}
+                <StyledSafeLink href={MAINNET_RISKS_BLOG_POST} target="_blank">
+                  Learn more
+                </StyledSafeLink>{' '}
+                about the risks and what's been done to mitigate them here.
+              </p>
+              <VSpace size={2} />
+            </React.Fragment>
+          )}
           <p>
             Configure your deposit above, and sign the transaction with your
             wallet after clicking “Submit Transfer”. It will then show up in
             your Finance app once processed.
           </p>
+          {tokenSelected && (
+            <React.Fragment>
+              <VSpace size={2} />
+              <p>
+                Tokens may require a pretransaction to approve the Finance app
+                for your deposit.{' '}
+                <StyledSafeLink href={TOKEN_ALLOWANCE_WEBSITE} target="_blank">
+                  Find out why.
+                </StyledSafeLink>{' '}
+              </p>
+            </React.Fragment>
+          )}
         </Info.Action>
 
-        {proxyAddress && (
-          <div>
-            <VSpace size={6} />
-            <ToggleContent label="Show address for direct ETH transfer ">
-              <VSpace size={4} />
-              <QRCode
-                value={proxyAddress}
-                style={{ width: '80px', height: '80px' }}
-              />
-              <VSpace size={4} />
-              <IdentityBadge
-                entity={proxyAddress}
-                shorten={false}
-                fontSize="small"
-              />
-              <VSpace size={4} />
-              <Info>
-                Use the above address or QR code to transfer ETH directly to
-                your organization’s Finance app. Note that ERC-20 tokens cannot
-                be transferred with the QR code.
-              </Info>
-            </ToggleContent>
-          </div>
-        )}
+        {proxyAddress &&
+          ethSelected && (
+            <div>
+              <VSpace size={6} />
+              <ToggleContent label="Show address for direct ETH transfer ">
+                <VSpace size={4} />
+                <QRCode
+                  value={proxyAddress}
+                  style={{ width: '80px', height: '80px' }}
+                />
+                <VSpace size={4} />
+                <IdentityBadge
+                  entity={proxyAddress}
+                  shorten={false}
+                  fontSize="small"
+                />
+                <VSpace size={2} />
+                <Info>
+                  Use the above address or QR code to transfer ETH directly to
+                  your organization’s Finance app.
+                  <Text.Paragraph size="xsmall" style={{ marginTop: '10px' }}>
+                    (Note that ERC-20 tokens cannot be transferred with the QR
+                    code)
+                  </Text.Paragraph>
+                </Info>
+              </ToggleContent>
+            </div>
+          )}
       </form>
     )
   }
@@ -170,21 +369,13 @@ const ButtonWrapper = styled.div`
   padding-top: 10px;
 `
 
-const AmountField = styled.div`
-  margin-bottom: 20px;
+const TokenBalance = styled.div`
+  margin: 10px 0 20px;
 `
 
-const CombinedInput = styled.div`
-  display: flex;
-  input[type='text'] {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    border-right: 0;
-  }
-  input[type='text'] + div > div:first-child {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-  }
+const StyledSafeLink = styled(SafeLink)`
+  text-decoration-color: ${theme.accent};
+  color: ${theme.accent};
 `
 
 const VSpace = styled.div`
@@ -203,4 +394,4 @@ const ValidationError = ({ message }) => (
   </div>
 )
 
-export default Deposit
+export default provideNetwork(Deposit)
