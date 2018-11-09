b5b2e107afb39338261a00aaae632f27c60cfa0d
Santiago <sgobotta@gmail.com>
2018-11-09 13:59:07 -0300

    Implements a format currency function

--- future-apps/payroll/app/src/utils/formatting.js
+++ future-apps/payroll/app/src/utils/formatting.js
@@ -3,15 +3,18 @@ import { round } from './math-utils'
 
 const DEFAULT_DATE_FORMAT = 'LL/dd/yyyy'
 
+export const SECONDS_IN_A_YEAR = 31557600 // 365.25 days
+
 export function formatDate (date, format = DEFAULT_DATE_FORMAT) {
   return dateFormatter(date, format)
 }
 
-export const formatCurrency = (
+export function formatCurrency (
   amount,
   symbol,
-  decimals = 2,
+  decimals = 10,
   pow = 18,
   rounding = 2
-) =>
-  Number(round(amount / Math.pow(pow, decimals), rounding)) + ' ' + symbol
+) {
+  return round(((amount / Math.pow(decimals, pow)) * SECONDS_IN_A_YEAR), 2) + ' ' + symbol
+}
