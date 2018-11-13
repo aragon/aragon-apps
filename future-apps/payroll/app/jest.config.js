4f5e54e8473519efb295dcffcc0f4d116bf6f6a3
Lisandro Corbalan <lmcorbalan@gmail.com>
2018-11-13 12:26:00 -0300

    Create model-root element for all tests using setupFile

--- future-apps/payroll/app/jest.config.js
+++ future-apps/payroll/app/jest.config.js
@@ -118,7 +118,9 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: [
+    "<rootDir>/test-setup.js"
+  ],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
   // setupTestFrameworkScriptFile: null,
