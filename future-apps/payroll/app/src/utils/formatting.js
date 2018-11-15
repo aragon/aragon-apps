38b1071335d05884165a4575c08a66abcd43e4cd
Santiago <sgobotta@gmail.com>
2018-11-15 17:09:47 -0300

    Formatting Utils: adds a format token amount function

--- future-apps/payroll/app/src/utils/formatting.js
+++ future-apps/payroll/app/src/utils/formatting.js
@@ -1,4 +1,5 @@
 import { format as dateFormatter } from 'date-fns'
+import { round } from './math.utils'
 
 const DEFAULT_DATE_FORMAT = 'LL/dd/yyyy'
 
@@ -13,3 +14,19 @@ export const formatCurrency = currencyFormatter.format
 export function formatDate (date, format = DEFAULT_DATE_FORMAT) {
   return dateFormatter(date, format)
 }
+
+export const formatTokenAmount = ({
+  amount,
+  isIncoming,
+  decimals = 0,
+  displaySign = false,
+  rounding = 2
+}) =>
+  (displaySign ? (isIncoming ? '+' : '-') : '') +
+  Number(round(amount / Math.pow(10, decimals), rounding)).toLocaleString(
+    'latn',
+    {
+      style: 'decimal',
+      maximumFractionDigits: 18
+    }
+  )
