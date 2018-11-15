06cb2eda482907fe746f91cab538912a603c6017
Santiago <sgobotta@gmail.com>
2018-11-15 16:03:43 -0300

    TokenFilter: Creates a basic filter component

---
+++ future-apps/payroll/app/src/screens/components/filters/TokenFilter.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { DropDown } from '@aragon/ui'
+
+import InlineField from '../../../components/Field/InlineField'
+
+const options = [
+  { label: 'All', filter: null }
+  // { label: 'Active', filter: employee => !employee.terminated },
+  // { label: 'Inactive', filter: employee => employee.terminated }
+]
+
+const TokenFilter = ({ active, onChange }) => {
+  const activeIndex = active &&
+    options.indexOf(options.find(f => f.label === active.label))
+
+  return (
+    <InlineField label='Token:'>
+      <DropDown
+        items={options.map(opt => opt.label)}
+        active={activeIndex || 0}
+        onChange={index => {
+          if (typeof onChange === 'function') {
+            onChange(options[index])
+          }
+        }}
+      />
+    </InlineField>
+  )
+}
+
+TokenFilter.propTypes = {
+  active: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    filter: PropTypes.func
+  }),
+  onChange: PropTypes.func
+}
+
+export default TokenFilter
