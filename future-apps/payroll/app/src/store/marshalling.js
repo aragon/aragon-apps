2bcc3ec474e260a16c6a210f3eb4eea5e3281239
Santiago <sgobotta@gmail.com>
2018-11-22 15:04:44 -0300

    Store/Marshalling: implements a function that creates a payment

--- future-apps/payroll/app/src/store/marshalling.js
+++ future-apps/payroll/app/src/store/marshalling.js
@@ -34,3 +34,22 @@ export function tokenAllocation (data) {
     allocation: parseInt(data.allocation) || 0
   }
 }
+
+export function payment (data) {
+  return {
+    accountAddress: data.returnValues.employee,
+    amount: {
+      amount: data.returnValues.amount,
+      isIncoming: true, // FIXME: Assumption: all salaries are incoming - sgobotta
+      displaySign: true, // FIXME: The send payroll event should provide the displaysign option
+      token: data.token.symbol,
+      decimals: data.token.decimals
+    },
+    transactionAddress: data.transactionHash,
+    date: (new Date()).getTime(), // FIXME: The send payroll event should provide the payment transaction timestamp - - sgobotta
+    status: 'Pending...', // FIXME: Find out how the status is calculated - - sgobotta
+    exchangeRate: { // FIXME: Get the exchange rate from the contract - - sgobotta
+      amount: 123456789
+    }
+  }
+}
