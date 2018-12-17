d2ea0f060347e4a32c0a6645eee85dfba7f384a7
Brett Sun <qisheng.brett.sun@gmail.com>
2018-12-17 21:53:55 +0100

    Voting: text clarifications (#605)

--- apps/voting/app/src/components/NewVotePanelContent.js
+++ apps/voting/app/src/components/NewVotePanelContent.js
@@ -51,7 +51,8 @@ class NewVotePanelContent extends React.Component {
             Begin Vote
           </Button>
           <Warning>
-            By opening this vote, you will automatically vote yay.
+            If you are allowed to directly create this vote, you will
+            automatically vote yes for it.
           </Warning>
         </Form>
       </div>
