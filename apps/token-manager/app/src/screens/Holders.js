6104579dac36d832e2fa5f1154288ee58c6227ba
Pierre Bertet <hello@pierre.world>
2019-01-26 18:00:48 +0100

    Remove tabs transition

--- apps/token-manager/app/src/screens/Holders.js
+++ apps/token-manager/app/src/screens/Holders.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Spring, animated } from 'react-spring'
 import {
   TabBar,
   Table,
@@ -20,7 +19,6 @@ class Holders extends React.Component {
 
   static defaultProps = {
     holders: [],
-    offset: 0,
   }
   render() {
     const {
@@ -28,7 +26,6 @@ class Holders extends React.Component {
       holders,
       maxAccountTokens,
       onAssignTokens,
-      offset,
       onRemoveTokens,
       tabbedNavigation,
       tokenAddress,
@@ -51,51 +48,49 @@ class Holders extends React.Component {
               onSelect={this.handleSelectTab}
             />
           </ResponsiveTabBar>
-          <Screen
-            Component={ResponsiveTable}
-            selected={!tabbedNavigation || selectedTab === 0}
-            offset={-offset}
-            header={
-              <TableRow>
-                <StyledTableHeader
-                  title={groupMode ? 'Owner' : 'Holder'}
-                  groupmode={groupMode}
+          <Screen selected={!tabbedNavigation || selectedTab === 0}>
+            <ResponsiveTable
+              header={
+                <TableRow>
+                  <StyledTableHeader
+                    title={groupMode ? 'Owner' : 'Holder'}
+                    groupmode={groupMode}
+                  />
+                  {!groupMode && (
+                    <StyledTableHeader title="Balance" align="right" />
+                  )}
+                  <TableHeader title="" />
+                </TableRow>
+              }
+            >
+              {holders.map(({ address, balance }) => (
+                <HolderRow
+                  key={address}
+                  address={address}
+                  balance={balance}
+                  groupMode={groupMode}
+                  isCurrentUser={userAccount && userAccount === address}
+                  maxAccountTokens={maxAccountTokens}
+                  tokenDecimalsBase={tokenDecimalsBase}
+                  onAssignTokens={onAssignTokens}
+                  onRemoveTokens={onRemoveTokens}
                 />
-                {!groupMode && (
-                  <StyledTableHeader title="Balance" align="right" />
-                )}
-                <TableHeader title="" />
-              </TableRow>
-            }
-          >
-            {holders.map(({ address, balance }) => (
-              <HolderRow
-                key={address}
-                address={address}
-                balance={balance}
-                groupMode={groupMode}
-                isCurrentUser={userAccount && userAccount === address}
-                maxAccountTokens={maxAccountTokens}
-                tokenDecimalsBase={tokenDecimalsBase}
-                onAssignTokens={onAssignTokens}
-                onRemoveTokens={onRemoveTokens}
-              />
-            ))}
+              ))}
+            </ResponsiveTable>
           </Screen>
         </Main>
-        <Screen
-          Component={ResponsiveSideBar}
-          selected={!tabbedNavigation || selectedTab === 1}
-          offset={offset}
-          groupMode={groupMode}
-          holders={holders}
-          tokenAddress={tokenAddress}
-          tokenDecimalsBase={tokenDecimalsBase}
-          tokenName={tokenName}
-          tokenSupply={tokenSupply}
-          tokenSymbol={tokenSymbol}
-          tokenTransfersEnabled={tokenTransfersEnabled}
-        />
+        <Screen selected={!tabbedNavigation || selectedTab === 1}>
+          <ResponsiveSideBar
+            groupMode={groupMode}
+            holders={holders}
+            tokenAddress={tokenAddress}
+            tokenDecimalsBase={tokenDecimalsBase}
+            tokenName={tokenName}
+            tokenSupply={tokenSupply}
+            tokenSymbol={tokenSymbol}
+            tokenTransfersEnabled={tokenTransfersEnabled}
+          />
+        </Screen>
       </TwoPanels>
     )
   }
@@ -105,41 +100,7 @@ class Holders extends React.Component {
   }
 }
 
-const Screen = ({
-  offset,
-  children,
-  Component,
-  selected,
-  ...props
-}) => {
-  return (
-    <Spring
-      from={{ progress: 0 }}
-      to={{ progress: !!selected }}
-      config={springs.clamp}
-      native
-    >
-      {({ progress }) =>
-        selected && (
-          <AnimatedDiv
-            style={{
-              opacity: progress,
-              transform: progress.interpolate(
-                v => `translate3d(${offset - v * offset}px, 0, 0)`
-              ),
-            }}
-          >
-            <Component {...props} children={children} />
-          </AnimatedDiv>
-        )
-      }
-    </Spring>
-  )
-}
-
-const AnimatedDiv = styled(animated.div)`
-  position: relative;
-`
+const Screen = ({ selected, children }) => selected && children
 
 const StyledTableHeader = styled(TableHeader)`
   width: ${({ groupmode }) => (groupmode ? 100 : 50)}%;
@@ -214,7 +175,7 @@ const TwoPanels = styled.div`
 export default props => (
   <React.Fragment>
     <BreakPoint to="medium">
-      <Holders {...props} tabbedNavigation offset={50} />
+      <Holders {...props} tabbedNavigation />
     </BreakPoint>
     <BreakPoint from="medium">
       <Holders {...props} />
