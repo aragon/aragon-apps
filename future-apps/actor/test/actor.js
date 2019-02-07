090988addb42b8a4812ca74cade1202050c093d5
Brett Sun <qisheng.brett.sun@gmail.com>
2019-02-07 15:32:06 +0100

    test: parameterize execution for deposit amounts

--- future-apps/actor/test/actor.js
+++ future-apps/actor/test/actor.js
@@ -82,169 +82,121 @@ contract('Actor app', (accounts) => {
     const [_, nonExecutor, executor] = accounts
     let executionTarget
 
-    beforeEach(async () => {
-      await acl.createPermission(executor, actor.address, EXECUTE_ROLE, root, { from: root })
+    for (const depositAmount of [0, 3]) {
+      context(depositAmount ? '> With ETH' : '> Without ETH', () => {
+        beforeEach(async () => {
+          await acl.createPermission(executor, actor.address, EXECUTE_ROLE, root, { from: root })
 
-      executionTarget = await ExecutionTarget.new()
-    })
+          executionTarget = await ExecutionTarget.new()
+          assert.equal(await executionTarget.counter(), 0)
+          assert.equal(await getBalance(executionTarget.address), 0)
 
-    it('can execute actions', async () => {
-      const N = 1102
+          if (depositAmount) {
+            await actor.deposit(ETH, depositAmount, { value: depositAmount })
+          }
+          assert.equal(await getBalance(actor.address), depositAmount)
+        })
 
-      assert.equal(await executionTarget.counter(), 0)
+        it('can execute actions', async () => {
+          const N = 1102
 
-      const { to, data } = encodeFunctionCall(executionTarget, 'setCounter', N)
-      const receipt = await actor.execute(to, 0, data, { from: executor })
+          const data = executionTarget.contract.setCounter.getData(N)
+          const receipt = await actor.execute(executionTarget.address, depositAmount, data, { from: executor })
 
-      assert.equal(await executionTarget.counter(), N)
-      assertEvent(receipt, 'Execute')
-    })
+          assertEvent(receipt, 'Execute')
+          assert.equal(await executionTarget.counter(), N)
+          assert.equal(await getBalance(executionTarget.address), depositAmount)
+          assert.equal(await getBalance(actor.address), 0)
+        })
 
-    it('can execute actions without data', async () => {
-      assert.equal(await executionTarget.counter(), 0)
+        it('can execute actions without data', async () => {
+          const noData = '0x'
+          const receipt = await actor.execute(executionTarget.address, depositAmount, noData, { from: executor })
 
-      const noData = '0x'
-      const receipt = await actor.execute(executionTarget.address, 0, noData, { from: executor })
+          assertEvent(receipt, 'Execute')
+          // Fallback just runs ExecutionTarget.execute()
+          assert.equal(await executionTarget.counter(), 1)
+          assert.equal(await getBalance(executionTarget.address), depositAmount)
+          assert.equal(await getBalance(actor.address), 0)
+        })
 
-      // Fallback just runs ExecutionTarget.execute()
-      assert.equal(await executionTarget.counter(), 1)
-      assertEvent(receipt, 'Execute')
-    })
+        it('can execute cheap fallback actions', async () => {
+          const cheapFallbackTarget = await DestinationMock.new(false)
+          const noData = '0x'
+          const receipt = await actor.execute(cheapFallbackTarget.address, depositAmount, noData, { from: executor })
 
-    it('can execute cheap fallback actions', async () => {
-      const cheapFallbackTarget = await DestinationMock.new(false)
-      const noData = '0x'
-      const receipt = await actor.execute(cheapFallbackTarget.address, 0, noData, { from: executor })
+          assertEvent(receipt, 'Execute')
+          assert.equal(await getBalance(executionTarget.address), depositAmount)
+          assert.equal(await getBalance(actor.address), 0)
+        })
 
-      assertEvent(receipt, 'Execute')
-    })
+        it('can execute expensive fallback actions', async () => {
+          const expensiveFallbackTarget = await DestinationMock.new(true)
+          assert.equal(await expensiveFallbackTarget.counter(), 0)
 
-    it('can execute expensive fallback actions', async () => {
-      const expensiveFallbackTarget = await DestinationMock.new(true)
-      assert.equal(await expensiveFallbackTarget.counter(), 0)
+          const noData = '0x'
+          const receipt = await actor.execute(expensiveFallbackTarget.address, depositAmount, noData, { from: executor })
 
-      const noData = '0x'
-      const receipt = await actor.execute(expensiveFallbackTarget.address, 0, noData, { from: executor })
+          assertEvent(receipt, 'Execute')
+          // Fallback increments counter
+          assert.equal(await expensiveFallbackTarget.counter(), 1)
+          assert.equal(await getBalance(executionTarget.address), depositAmount)
+          assert.equal(await getBalance(actor.address), 0)
+        })
 
-      // Fallback increments counter
-      assert.equal(await expensiveFallbackTarget.counter(), 1)
-      assertEvent(receipt, 'Execute')
-    })
+        it('fails to execute without permissions', async () => {
+          const data = executionTarget.contract.execute.getData()
 
-    it('fails to execute without permissions', async () => {
-      const { to, data } = encodeFunctionCall(executionTarget, 'execute')
+          await assertRevert(() =>
+            actor.execute(executionTarget.address, depositAmount, data, { from: nonExecutor })
+          )
+        })
 
-      await assertRevert(() =>
-        actor.execute(to, 0, data, { from: nonExecutor })
-      )
-    })
+        it('fails to execute when target is not a contract', async () => {
+          const nonContract = accounts[8] // random account
+          const randomData = '0x12345678'
+          const noData = '0x'
 
-    it('fails to execute when target is not a contract', async () => {
-      const nonContract = accounts[8] // random account
-      const randomData = '0x12345678'
-      const noData = '0x'
+          await assertRevert(() =>
+            actor.execute(nonContract, depositAmount, randomData, { from: executor })
+          )
 
-      await assertRevert(() =>
-        actor.execute(nonContract, 0, randomData, { from: executor })
-      )
+          await assertRevert(() =>
+            actor.execute(nonContract, depositAmount, noData, { from: executor })
+          )
+        })
 
-      await assertRevert(() =>
-        actor.execute(nonContract, 0, noData, { from: executor })
-      )
-    })
+        it('fails to execute actions with more ETH than the actor owns', async () => {
+          const data = executionTarget.contract.execute.getData()
 
-    it('execution forwards success return data', async () => {
-      assert.equal(await executionTarget.counter(), 0)
+          await assertRevert(() =>
+            actor.execute(executionTarget.address, depositAmount + 1, data, { from: executor })
+          )
+        })
 
-      const { to, data } = encodeFunctionCall(executionTarget, 'execute')
+        it('execution forwards success return data', async () => {
+          const { to, data } = encodeFunctionCall(executionTarget, 'execute')
 
-      // We make a call to easily get what data could be gotten inside the EVM
-      // Contract -> Actor.execute -> Target.func (would allow Contract to have access to this data)
-      const call = encodeFunctionCall(actor, 'execute', to, 0, data, { from: executor })
-      const returnData = await web3Call(call)
+          // We make a call to easily get what data could be gotten inside the EVM
+          // Contract -> Actor.execute -> Target.func (would allow Contract to have access to this data)
+          const call = encodeFunctionCall(actor, 'execute', to, depositAmount, data, { from: executor })
+          const returnData = await web3Call(call)
 
-      // ExecutionTarget.execute() increments the counter by 1
-      assert.equal(ethABI.decodeParameter('uint256', returnData), 1)
-    })
+          // ExecutionTarget.execute() increments the counter by 1
+          assert.equal(ethABI.decodeParameter('uint256', returnData), 1)
+        })
 
-    it('it reverts if executed action reverts', async () => {
-      // TODO: Check revert data was correctly forwarded
-      // ganache currently doesn't support fetching this data
+        it('it reverts if executed action reverts', async () => {
+          // TODO: Check revert data was correctly forwarded
+          // ganache currently doesn't support fetching this data
 
-      const { to, data } = encodeFunctionCall(executionTarget, 'fail')
-
-      await assertRevert(() =>
-        actor.execute(to, 0, data, { from: executor })
-      )
-    })
-
-    context('> With ETH:', () => {
-      const depositValue = 3
-      let to, data
-
-      beforeEach(async () => {
-        await actor.deposit(ETH, depositValue, { value: depositValue })
-
-        const call = encodeFunctionCall(executionTarget, 'execute')
-        to = call.to
-        data = call.data
-
-        assert.equal(await executionTarget.counter(), 0)
-        assert.equal(await getBalance(executionTarget.address), 0)
-        assert.equal(await getBalance(actor.address), depositValue)
+          const data = executionTarget.contract.fail.getData()
+          await assertRevert(() =>
+            actor.execute(executionTarget.address, depositAmount, data, { from: executor })
+          )
+        })
       })
-
-      it('can execute actions', async () => {
-        await actor.execute(to, depositValue, data, { from: executor })
-
-        // ExecutionTarget.execute() increments the counter by 1
-        assert.equal(await executionTarget.counter(), 1)
-        assert.equal(await getBalance(executionTarget.address), depositValue)
-        assert.equal(await getBalance(actor.address), 0)
-      })
-
-      it('can execute actions without data', async () => {
-        const noData = '0x'
-        await actor.execute(to, depositValue, noData, { from: executor })
-
-        // Fallback just runs ExecutionTarget.execute()
-        assert.equal(await executionTarget.counter(), 1)
-        assert.equal(await getBalance(executionTarget.address), depositValue)
-        assert.equal(await getBalance(actor.address), 0)
-      })
-
-      it('can execute cheap fallback actions', async () => {
-        const cheapFallbackTarget = await DestinationMock.new(false)
-        const noData = '0x'
-        const receipt = await actor.execute(cheapFallbackTarget.address, depositValue, noData, { from: executor })
-
-        assertEvent(receipt, 'Execute')
-      })
-
-      it('can execute expensive fallback actions', async () => {
-        const expensiveFallbackTarget = await DestinationMock.new(true)
-        assert.equal(await expensiveFallbackTarget.counter(), 0)
-
-        const noData = '0x'
-        const receipt = await actor.execute(expensiveFallbackTarget.address, depositValue, noData, { from: executor })
-
-        // Fallback increments counter
-        assert.equal(await expensiveFallbackTarget.counter(), 1)
-        assertEvent(receipt, 'Execute')
-      })
-
-      it('fails to execute actions with more ETH than the actor owns', async () => {
-        await assertRevert(() =>
-          actor.execute(to, depositValue + 1, data, { from: executor })
-        )
-      })
-
-      it('fails to execute when sending ETH and no data', async () => {
-        await assertRevert(() =>
-          actor.execute(to, depositValue, '0x', { from: executor })
-        )
-      })
-    })
+    }
   })
 
   context('> Running scripts', () => {
