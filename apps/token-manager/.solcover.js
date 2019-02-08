441772ef3896cf9f0513dfbf678ef029f5e1a6ca
Brett Sun <qisheng.brett.sun@gmail.com>
2019-02-08 19:30:38 +0100

    test: avoid instrumenting all test files

--- apps/token-manager/.solcover.js
+++ apps/token-manager/.solcover.js
@@ -2,7 +2,6 @@ module.exports = {
     norpc: true,
     copyPackages: ['@aragon/os', '@aragon/apps-shared-minime'],
     skipFiles: [
-        'test/TestImports.sol',
-        'test/mocks/ExecutionTarget.sol',
+        'test',
     ]
 }
--- apps/voting/.solcover.js
+++ apps/voting/.solcover.js
@@ -2,9 +2,6 @@ module.exports = {
     norpc: true,
     copyPackages: ['@aragon/os', '@aragon/apps-shared-minime'],
     skipFiles: [
-        'test/TestImports.sol',
-        'test/mocks/ExecutionTarget.sol',
-        'test/mocks/VotingMock.sol',
-        'test/mocks/VotingMockExt.sol',
+        'test',
     ]
 }
