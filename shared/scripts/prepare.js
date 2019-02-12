d96b9a63e26bb4fc3b9058dbf8d8514b5d206cc9
Brett Sun <qisheng.brett.sun@gmail.com>
2019-02-13 00:03:56 +0100

    chore: fix travis install command

--- shared/scripts/prepare.js
+++ shared/scripts/prepare.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const execute = require('child_process').execSync
-const shouldInstallFrontend = process.env.INSTALL_FRONTEND === 'true';
+const shouldInstallFrontend = process.env.INSTALL_FRONTEND === 'true'
 
 if (shouldInstallFrontend) {
-  execute('npm run install:frontend', { stdio: 'inherit' });
+  execute('npm run install:frontend', { stdio: 'inherit' })
 } else {
   console.log('Skipping frontend installation')
 }
