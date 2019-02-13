6565540b35577afffdc65448b868db2b920cbf62
Brett Sun <qisheng.brett.sun@gmail.com>
2019-02-13 14:29:44 +0100

    test: PR comments for DestinationMock

--- future-apps/agent/contracts/test/mocks/DestinationMock.sol
+++ future-apps/agent/contracts/test/mocks/DestinationMock.sol
@@ -5,11 +5,13 @@ contract DestinationMock {
     bool public expensiveFallback;
     uint256 public counter;
 
-    constructor(bool _expensiveFallback) {
+    constructor(bool _expensiveFallback) public {
         expensiveFallback = _expensiveFallback;
     }
 
     function () external payable {
+        // If expensiveFallback is used, this breaks the 2300 gas stipend given by
+        // .send() and .transfer()
         if (expensiveFallback) {
             counter = counter + 1;
         }
