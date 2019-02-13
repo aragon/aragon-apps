7fb014b15020e880bfb513906a3b9f01698f85ca
Brett Sun <qisheng.brett.sun@gmail.com>
2019-02-13 23:26:02 +0100

    Payroll: ignore test contracts from coverage

--- future-apps/payroll/.solcover.js
+++ future-apps/payroll/.solcover.js
@@ -2,9 +2,6 @@ module.exports = {
     norpc: true,
     copyPackages: ['@aragon/os', '@aragon/apps-finance', '@aragon/apps-vault'],
     skipFiles: [
-        'test/TestImports.sol',
-        'test/mocks/ExecutionTarget.sol',
-        'test/mocks/PayrollMock.sol',
-        'test/mocks/Zombie.sol',
+        'test',
     ]
 }
