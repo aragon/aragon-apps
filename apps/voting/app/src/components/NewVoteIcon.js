da5f12cc887c5921973bf80504273b9b2e9a3c20
Pierre Bertet <hello@pierre.world>
2019-02-14 12:10:26 +0100

    Voting: move to Viewport
    - Remove uses of BreakPoint.
    - Moved the app layout structure into AppLayout.
    - Moved the main action icon into an icon component.

---
+++ apps/voting/app/src/components/NewVoteIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NewVoteIcon = props => (
+  <svg width="24px" height="24px" viewBox="0 0 24 24" {...props}>
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export default NewVoteIcon
