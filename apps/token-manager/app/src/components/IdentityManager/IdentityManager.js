2ddbe93ab2f7d735038184f1fc440e57c21395e8
Gorka <gorka@aquigorka.com>
2019-03-27 10:20:36 +0100

    Combine identity contexts and define a custom hook

--- apps/token-manager/app/src/components/IdentityManager/IdentityManager.js
+++ apps/token-manager/app/src/components/IdentityManager/IdentityManager.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Subject } from 'rxjs'
+
+const updates$ = new Subject()
 
 const IdentityContext = React.createContext({
   resolve: () =>
     Promise.reject(Error('Please set resolve using IdentityProvider')),
 })
 
-const IdentityProvider = ({ onResolve, children }) => {
+const IdentityProvider = ({
+  onResolve,
+  onShowLocalIdentityModal,
+  children,
+}) => {
+  const handleShowLocalIdentityModal = address => {
+    return onShowLocalIdentityModal(address)
+      .then(() => updates$.next(address))
+      .catch(e => null)
+  }
+
   return (
-    <IdentityContext.Provider value={{ resolve: onResolve }}>
+    <IdentityContext.Provider
+      value={{
+        resolve: onResolve,
+        showLocalIdentityModal: handleShowLocalIdentityModal,
+        updates$,
+      }}
+    >
       {children}
     </IdentityContext.Provider>
   )
 }
 
 IdentityProvider.propTypes = {
-  onResolve: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
+  onResolve: PropTypes.func.isRequired,
+  onShowLocalIdentityModal: PropTypes.func.isRequired,
 }
 
 const IdentityConsumer = IdentityContext.Consumer
