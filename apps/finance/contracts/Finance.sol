6abc68393eb2336085f7357cbab65b6c967da75b
Brett Sun <qisheng.brett.sun@gmail.com>
2019-03-28 12:21:07 +0100

    Finance: reorder newImmediatePayment() to be above newScheduledPayment()

--- apps/finance/contracts/Finance.sol
+++ apps/finance/contracts/Finance.sol
@@ -212,6 +212,37 @@ contract Finance is EtherTokenConstant, IsContract, AragonApp {
     }
 
     /**
+    * @notice Create a new payment of `@tokenAmount(_token, _amount)` to `_receiver` for '`_reference`'
+    * @dev Note that this function is protected by the `CREATE_PAYMENTS_ROLE` but uses `MAX_UINT`
+    *      as its interval auth parameter (as a sentinel value for "never repeating").
+    *      While this protects against most cases (you typically want to set a baseline requirement
+    *      for interval time), it does mean users will have to explicitly check for this case when
+    *      granting a permission that includes a upperbound requirement on the interval time.
+    * @param _token Address of token for payment
+    * @param _receiver Address that will receive payment
+    * @param _amount Tokens that are paid every time the payment is due
+    * @param _reference String detailing payment reason
+    */
+    function newImmediatePayment(address _token, address _receiver, uint256 _amount, string _reference)
+        external
+        // Use MAX_UINT256 as the interval parameter, as this payment will never repeat
+        // Payment time parameter is left as the last param as it was added later
+        authP(CREATE_PAYMENTS_ROLE, _arr(_token, _receiver, _amount, MAX_UINT, uint256(1), getTimestamp()))
+        transitionsPeriod
+    {
+        require(_amount > 0, ERROR_NEW_PAYMENT_AMOUNT_ZERO);
+
+        _makePaymentTransaction(
+            _token,
+            _receiver,
+            _amount,
+            NO_RECURRING_PAYMENT,   // unrelated to any payment id; it isn't created
+            0,   // also unrelated to any payment repeats
+            _reference
+        );
+    }
+
+    /**
     * @notice Create a new payment of `@tokenAmount(_token, _amount)` to `_receiver` for `_reference`, executing `_maxRepeats` times at intervals of `@transformTime(_interval)`
     * @dev See `newImmediatePayment()` for limitations on how the interval auth parameter can be used
     * @param _token Address of token for payment
@@ -264,37 +295,6 @@ contract Finance is EtherTokenConstant, IsContract, AragonApp {
         // We skip checking how many times the new payment was executed to allow creating new
         // recurring payments before having enough vault balance
         _executePayment(paymentId);
-    }
-
-    /**
-    * @notice Create a new payment of `@tokenAmount(_token, _amount)` to `_receiver` for '`_reference`'
-    * @dev Note that this function is protected by the `CREATE_PAYMENTS_ROLE` but uses `MAX_UINT`
-    *      as its interval auth parameter (as a sentinel value for "never repeating").
-    *      While this protects against most cases (you typically want to set a baseline requirement
-    *      for interval time), it does mean users will have to explicitly check for this case when
-    *      granting a permission that includes a upperbound requirement on the interval time.
-    * @param _token Address of token for payment
-    * @param _receiver Address that will receive payment
-    * @param _amount Tokens that are paid every time the payment is due
-    * @param _reference String detailing payment reason
-    */
-    function newImmediatePayment(address _token, address _receiver, uint256 _amount, string _reference)
-        external
-        // Use MAX_UINT256 as the interval parameter, as this payment will never repeat
-        // Payment time parameter is left as the last param as it was added later
-        authP(CREATE_PAYMENTS_ROLE, _arr(_token, _receiver, _amount, MAX_UINT, uint256(1), getTimestamp()))
-        transitionsPeriod
-    {
-        require(_amount > 0, ERROR_NEW_PAYMENT_AMOUNT_ZERO);
-
-        _makePaymentTransaction(
-            _token,
-            _receiver,
-            _amount,
-            NO_RECURRING_PAYMENT,   // unrelated to any payment id; it isn't created
-            0,   // also unrelated to any payment repeats
-            _reference
-        );
     }
 
     /**
