549669b2501cfbdf323ea9ff82afb3c28afd428f
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-03-28 12:02:25 -0300

    Payroll: Remove unused mock contracts

--- future-apps/payroll/contracts/test/TestImports.sol
+++ future-apps/payroll/contracts/test/TestImports.sol
@@ -1,11 +1,10 @@
 pragma solidity 0.4.24;
 
-import "@aragon/apps-vault/contracts/Vault.sol";
-import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
-import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/factory/DAOFactory.sol";
+import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 
-import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
+import "@aragon/apps-vault/contracts/Vault.sol";
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
 
 contract TestImports {
