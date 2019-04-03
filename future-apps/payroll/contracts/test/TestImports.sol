f5505f7692d7c9e9c6321943df17c58ed973621d
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-04-03 14:45:51 -0300

    Payroll: Re-add migrations contract

--- future-apps/payroll/contracts/test/TestImports.sol
+++ future-apps/payroll/contracts/test/TestImports.sol
@@ -6,6 +6,7 @@ import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 
 import "@aragon/apps-vault/contracts/Vault.sol";
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
 
 contract TestImports {
 
