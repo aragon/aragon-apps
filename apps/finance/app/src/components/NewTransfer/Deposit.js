f22fe0bd5a14d874a6317f2f74097927100dbcde
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-14 12:55:55 +0200

    Finance: clarify when user has no tokens to deposit

--- apps/finance/app/src/components/NewTransfer/Deposit.js
+++ apps/finance/app/src/components/NewTransfer/Deposit.js
@@ -256,10 +256,14 @@ class Deposit extends React.Component {
     const selectedTokenIsAddress = isAddress(selectedToken.value)
     const showTokenBadge = selectedTokenIsAddress && selectedToken.coerced
     const tokenBalanceMessage = selectedToken.data.userBalance
-      ? `You have ${fromDecimals(
-          selectedToken.data.userBalance,
-          selectedToken.data.decimals
-        )} ${selectedToken.data.symbol} available`
+      ? `You have ${
+          selectedToken.data.userBalance === '0'
+            ? 'no'
+            : fromDecimals(
+                selectedToken.data.userBalance,
+                selectedToken.data.decimals
+              )
+        } ${selectedToken.data.symbol} available`
       : ''
 
     const ethSelected =
