be9714051316a1fe71511b79fbd2cfdc17103ae9
Brett Sun <qisheng.brett.sun@gmail.com>
2019-05-10 19:57:20 +0200

    feat: modernize testing kit

--- future-apps/payroll/contracts/PayrollKit.sol
+++ future-apps/payroll/contracts/PayrollKit.sol
@@ -18,8 +18,8 @@ import "./Payroll.sol";
 
 
 contract PPFMock is IFeed {
-  function get(address base, address quote) external view returns (uint128 xrt, uint64 when) {
-      xrt = 7500000000000000;
+  function get(address, address) external view returns (uint128 xrt, uint64 when) {
+      xrt = 7500000000000000; // 1 ETH = ~133USD
       when = uint64(now);
   }
 }
@@ -32,7 +32,7 @@ contract KitBase is APMNamehash, EVMScriptRegistryConstants {
     event DeployInstance(address dao);
     event InstalledApp(address appProxy, bytes32 appId);
 
-    constructor(DAOFactory _fac, ENS _ens) {
+    constructor(DAOFactory _fac, ENS _ens) public {
         ens = _ens;
 
         // If no factory is passed, get it from on-chain bare-kit
@@ -70,12 +70,9 @@ contract KitBase is APMNamehash, EVMScriptRegistryConstants {
 contract PayrollKit is KitBase {
     MiniMeTokenFactory tokenFactory;
 
-    uint64 financePeriodDuration = 31557600;
-    uint64 rateExpiryTime = 1000;
-    uint64 amount = uint64(-1);
-    address constant ANY_ENTITY = address(-1);
-    address[] allowedTokens;
-    uint256[] distribution;
+    uint64 internal constant FINANCE_PERIOD_DURATION = 31557600;
+    uint64 internal constant RATE_EXPIRY_TIME = 1000;
+    uint64 internal constant MAX_UINT64 = uint64(-1);
 
     constructor(ENS ens) KitBase(DAOFactory(0), ens) public {
         tokenFactory = new MiniMeTokenFactory();
@@ -90,147 +87,124 @@ contract PayrollKit is KitBase {
         dao = fac.newDAO(this);
         ACL acl = ACL(dao.acl());
 
+        // Payroll prerequisites
         PPFMock priceFeed = new PPFMock();
-
         MiniMeToken denominationToken = newToken("USD Dolar", "USD");
 
+        // Allow this contract to install new apps for now
         acl.createPermission(this, dao, dao.APP_MANAGER_ROLE(), this);
 
         Vault vault;
         Finance finance;
-        (vault, finance, payroll) = deployApps(dao);
+        (vault, finance) = installBaseApps(dao, acl, root);
+        payroll = installPayroll(dao, acl, root, finance, denominationToken, priceFeed);
 
-        // Setup the permissions for the Finance App
-        setFinancePermissions(acl, finance, payroll, root);
-
-        // Setup the permissions for the vault
-        setVaultPermissions(acl, vault, finance, root);
-
-        // Payroll permissions
-        setPayrollPermissions(acl, payroll, root);
-
-        vault.initialize();
-        finance.initialize(vault, financePeriodDuration);
-        payroll.initialize(finance, denominationToken, priceFeed, rateExpiryTime);
-
-        // deployTokens(dao, finance, acl, root);
-        MiniMeToken token1 = deployAndDepositToken(dao, finance, acl, root, "Token 1", "TK1");
+        // Deploy payroll tokens
+        MiniMeToken token1 = installManagedToken(dao, acl, root, finance, "Token 1", "TK1");
         payroll.addAllowedToken(token1);
 
-        MiniMeToken token2 = deployAndDepositToken(dao, finance, acl, root, "Token 2", "TK2");
+        MiniMeToken token2 = installManagedToken(dao, acl, root, finance, "Token 2", "TK2");
         payroll.addAllowedToken(token2);
 
-        MiniMeToken token3 = deployAndDepositToken(dao, finance, acl, root, "Token 3", "TK3");
+        MiniMeToken token3 = installManagedToken(dao, acl, root, finance, "Token 3", "TK3");
         payroll.addAllowedToken(token3);
 
+        // Add employees to payroll
+        addEmployees(payroll, root, token1, token2);
 
-        address(finance).send(10 ether);
-
-        addEmployees(payroll, root);
-
-        // set salary allocation for this then change protofire.aragon.eth address to root so it can be used
-        allowedTokens.push(address(token1));
-        allowedTokens.push(address(token2));
-
-        distribution.push(45);
-        distribution.push(55);
-
-        payroll.determineAllocation(allowedTokens, distribution);
-        payroll.changeAddressByEmployee(root);
-
+        // Clean up
         cleanupDAOPermissions(dao, acl, root);
+        cleanupPermission(acl, root, payroll, payroll.ALLOWED_TOKENS_MANAGER_ROLE());
+        cleanupPermission(acl, root, payroll, payroll.ADD_EMPLOYEE_ROLE());
 
         emit DeployInstance(dao);
     }
 
-    function deployApps(Kernel dao) internal returns (Vault, Finance, Payroll) {
+    function installBaseApps(Kernel dao, ACL acl, address root) internal returns (Vault, Finance) {
         bytes32 vaultAppId = apmNamehash("vault");
         bytes32 financeAppId = apmNamehash("finance");
-        bytes32 payrollAppId = apmNamehash("payroll");
-        bytes32 tokenManagerAppId = apmNamehash("token-manager");
 
         Vault vault = Vault(dao.newAppInstance(vaultAppId, latestVersionAppBase(vaultAppId)));
-        Finance finance = Finance(dao.newAppInstance(financeAppId, latestVersionAppBase(financeAppId)));
-        Payroll payroll = Payroll(dao.newAppInstance(payrollAppId, latestVersionAppBase(payrollAppId)));
-
         emit InstalledApp(vault, vaultAppId);
+
+        Finance finance = Finance(dao.newAppInstance(financeAppId, latestVersionAppBase(financeAppId)));
         emit InstalledApp(finance, financeAppId);
-        emit InstalledApp(payroll, payrollAppId);
 
-        return (vault, finance, payroll);
+        vault.initialize();
+        finance.initialize(vault, FINANCE_PERIOD_DURATION);
+        acl.createPermission(finance, vault, vault.TRANSFER_ROLE(), root);
+
+        return (vault, finance);
     }
 
-    function setVaultPermissions(ACL acl, Vault vault, Finance finance, address root) internal {
-        bytes32 vaultTransferRole = vault.TRANSFER_ROLE();
-        acl.createPermission(finance, vault, vaultTransferRole, this); // manager is this to allow 2 grants
-        acl.grantPermission(root, vault, vaultTransferRole);
-        acl.setPermissionManager(root, vault, vaultTransferRole); // set root as the final manager for the role
-    }
-
-    function setFinancePermissions(ACL acl, Finance finance, Payroll payroll, address root) internal {
-        acl.createPermission(payroll, finance, finance.CREATE_PAYMENTS_ROLE(), this); // manager is this to allow 2 grants
-        acl.grantPermission(root, finance, finance.CREATE_PAYMENTS_ROLE());
-        acl.setPermissionManager(root, finance, finance.CREATE_PAYMENTS_ROLE()); // set root as the
-    }
-
-    function setPayrollPermissions(ACL acl, Payroll payroll, address root) internal {
-        acl.createPermission(this, payroll, payroll.ADD_EMPLOYEE_ROLE(), this);
-        acl.grantPermission(root, payroll, payroll.ADD_EMPLOYEE_ROLE());
-        acl.setPermissionManager(root, payroll, payroll.ADD_EMPLOYEE_ROLE());
-
-        acl.createPermission(this, payroll, payroll.ALLOWED_TOKENS_MANAGER_ROLE(), this);
-        acl.grantPermission(root, payroll, payroll.ALLOWED_TOKENS_MANAGER_ROLE());
-        acl.setPermissionManager(root, payroll, payroll.ALLOWED_TOKENS_MANAGER_ROLE());
-    }
-
-    function deployTokens(Kernel dao, Finance finance, ACL acl, address root) internal {
-        deployAndDepositToken(dao, finance, acl, root, "Token 1", "TK1");
-        deployAndDepositToken(dao, finance, acl, root, "Token 2", "TK2");
-        deployAndDepositToken(dao, finance, acl, root, "Token 3", "TK3");
-    }
-
-    function deployAndDepositToken(
+    function installPayroll(
         Kernel dao,
-        Finance finance,
         ACL acl,
         address root,
+        Finance finance,
+        MiniMeToken denominationToken,
+        IFeed priceFeed
+    )
+        internal
+        returns (Payroll)
+    {
+        bytes32 payrollAppId = apmNamehash("payroll");
+
+        Payroll payroll = Payroll(dao.newAppInstance(payrollAppId, latestVersionAppBase(payrollAppId)));
+        emit InstalledApp(payroll, payrollAppId);
+
+        payroll.initialize(finance, denominationToken, priceFeed, RATE_EXPIRY_TIME);
+        acl.createPermission(payroll, finance, finance.CREATE_PAYMENTS_ROLE(), root);
+        acl.createPermission(root, payroll, payroll.TERMINATE_EMPLOYEE_ROLE(), root);
+
+        // Allow this contract to add tokens to Payroll for now
+        acl.createPermission(this, payroll, payroll.ALLOWED_TOKENS_MANAGER_ROLE(), this);
+
+        // Allow this contract to add employees to Payroll for now
+        acl.createPermission(this, payroll, payroll.ADD_EMPLOYEE_ROLE(), this);
+
+        return payroll;
+    }
+
+    function installManagedToken(
+        Kernel dao,
+        ACL acl,
+        address root,
+        Finance finance,
         string name,
         string symbol
     )
         internal
         returns (MiniMeToken)
     {
-        TokenManager tokenManager = newTokenManager(dao, acl, root);
+        bytes32 tokenManagerAppId = apmNamehash("token-manager");
+
         MiniMeToken token = newToken(name, symbol);
+        TokenManager tokenManager = TokenManager(dao.newAppInstance(tokenManagerAppId, latestVersionAppBase(tokenManagerAppId)));
+        emit InstalledApp(tokenManager, tokenManagerAppId);
+
         token.changeController(tokenManager);
         tokenManager.initialize(token, true, 0);
-        tokenManager.mint(this, amount);
-        token.approve(finance, amount);
-        finance.deposit(token, amount, "Initial deployment");
+
+        // Allow this contract to mint tokens for now
+        acl.createPermission(this, tokenManager, tokenManager.MINT_ROLE(), this);
+
+        // Add some of the tokens to the Finance app
+        tokenManager.mint(this, MAX_UINT64);
+        token.approve(finance, MAX_UINT64);
+        finance.deposit(token, MAX_UINT64, "Initial deposit");
+
+        // Clean up permissions
+        cleanupPermission(acl, root, tokenManager, tokenManager.MINT_ROLE());
 
         return token;
-    }
-
-    function addAllowedToken(MiniMeToken token) internal {
-        // solium-disable-previous-line no-empty-blocks
     }
 
     function newToken(string name, string symbol) internal returns (MiniMeToken token) {
         token = tokenFactory.createCloneToken(MiniMeToken(0), 0, name, 18, symbol, true);
     }
 
-    function newTokenManager(Kernel dao, ACL acl, address root) internal returns (TokenManager tokenManager) {
-        bytes32 tokenManagerAppId = apmNamehash("token-manager");
-        tokenManager = TokenManager(dao.newAppInstance(tokenManagerAppId, latestVersionAppBase(tokenManagerAppId)));
-        emit InstalledApp(tokenManager, tokenManagerAppId);
-        setTokenManagerPermissions(acl, tokenManager, root);
-    }
-
-    function setTokenManagerPermissions(ACL acl, TokenManager tokenManager, address root) internal {
-        acl.createPermission(this, tokenManager, tokenManager.MINT_ROLE(), root);
-    }
-
-    function addEmployees(Payroll payroll, address root) internal {
+    function addEmployees(Payroll payroll, address root, MiniMeToken token1, MiniMeToken token2) internal {
         address account2 = 0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb;
         address account3 = 0x306469457266CBBe7c0505e8Aad358622235e768;
         address account4 = 0xd873F6DC68e3057e4B7da74c6b304d0eF0B484C7;
@@ -242,10 +216,24 @@ contract PayrollKit is KitBase {
         uint256 salary4 = 2218166146982026; // 70000
         uint256 salary5 = 1901285268841737; // 60000
 
-        payroll.addEmployee(this, salary1, "Organization", uint64(now - 172800));
-        payroll.addEmployee(account2, salary2, "Project Manager",  uint64(now - 86400));
-        payroll.addEmployee(account3, salary3, "Developer", uint64(now - 172800));
-        payroll.addEmployee(account4, salary4, "Developer", uint64(now - 172800));
-        payroll.addEmployee(account5, salary5, "Developer", uint64(now - 172800));
+        // Set up first user; use this contract as the account so we can set up the initial distribution
+        payroll.addEmployee(this, salary1, uint64(now - 86400), "CEO");
+
+        address[] memory allowedTokens = new address[](2);
+        allowedTokens[0] = address(token1);
+        allowedTokens[1] = address(token2);
+
+        uint256[] memory distribution = new uint256[](2);
+        distribution[0] = 45;
+        distribution[1] = 55;
+
+        payroll.determineAllocation(allowedTokens, distribution);
+        payroll.changeAddressByEmployee(root); // Set account to root
+
+        // Create more users
+        payroll.addEmployee(account2, salary2, uint64(now - 86400), "Project Manager");
+        payroll.addEmployee(account3, salary3, uint64(now - 172800), "Developer");
+        payroll.addEmployee(account4, salary4, uint64(now - 172800), "Developer");
+        payroll.addEmployee(account5, salary5, uint64(now - 172800), "Developer");
     }
 }
