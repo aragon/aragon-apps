62fc9c0270697216b235fca551c2b133f8e4d7d4
Daniel Norman <daniel@2color.me>
2019-05-28 18:57:15 +0800

    Remove unneeded import

--- apps/token-manager/app/src/App.js
+++ apps/token-manager/app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import BN from 'bn.js'
 import { Badge, Main, SidePanel, SyncIndicator } from '@aragon/ui'
