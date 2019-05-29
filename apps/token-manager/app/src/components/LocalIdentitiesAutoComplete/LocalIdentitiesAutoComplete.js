a600aff85562a68653c53c2054712b080a4cea0e
Daniel Norman <daniel@2color.me>
2019-05-29 14:55:27 +0800

    Integrate local identities autocomplete with updated api (#872)
    * Use api to search identities
    
    * LocalIdentitiesAutoComplete: remove mock data and integrate with aragon api
    
    * LocalIdentitiesAutoComplete: fix deps on hooks

--- apps/token-manager/app/src/components/LocalIdentitiesAutoComplete/LocalIdentitiesAutoComplete.js
+++ apps/token-manager/app/src/components/LocalIdentitiesAutoComplete/LocalIdentitiesAutoComplete.js
@@ -1,101 +1,39 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react'
+import { useAragonApi } from '@aragon/api-react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { ButtonBase, EthIdenticon, IdentityBadge, theme } from '@aragon/ui'
 import AutoComplete from '../AutoComplete/AutoComplete'
 
-const mockItems = [
-  {
-    name: 'A good soul',
-    address: '0x39a4d265db942361d92e2b0039cae73ea72a2ff9',
-  },
-  {
-    name: 'Vault app',
-    address: '0x3b72de356b960974deaaa5f25ad6dead0dff5e05',
-  },
-  {
-    name:
-      'Somebody (maybe @ Aragon) and this is very large, indeed very large, come on large',
-    address: '0x3bd60bafea8a7768c6f4352af4cfe01701884ff2',
-  },
-  {
-    name: 'Metamask @ Chrome',
-    address: '0x447ae38c0dc4126b10b5560bedb2c9c837b69dc9',
-  },
-  {
-    name: 'Voting app',
-    address: '0x4fcd89c67a296756187be2962c6e4719c4a89cab',
-  },
-  {
-    name: 'dev.aragon.id',
-    address: '0x52d709f95d940d474d01c6f50d7686c3eff03831',
-  },
-  {
-    name: 'Token Manager app',
-    address: '0x7aa282a67195b5bca2295cddaedb62516898e7d0',
-  },
-  {
-    name: 'Coinbase @ iPhone',
-    address: '0xa244ebd51056cd9fdc476b87028c2a372bc64c9e',
-  },
-  {
-    name: 'Status @ iPhone',
-    address: '0xa47967b5127ef0dfaaef1d607e53eab00285c84f',
-  },
-  {
-    name: 'Aragon default 1',
-    address: '0xb4124ceb3451635dacedd11767f004d8a28c6ee7',
-  },
-  {
-    name: 'Finance app',
-    address: '0xb983e1754e96144bedacbaa249fda8ac300ccdaf',
-  },
-  {
-    name: '0x Who is this dude?',
-    address: '0xd395d4a9753310f3940de2673c70c251224e3d07',
-  },
-]
-
-const search = value => {
-  const searchAddress = value.substr(0, 2) === '0x'
-  const items = mockItems
-    .filter(
-      ({ address, name }) =>
-        (searchAddress &&
-          value.length > 3 &&
-          address.toLowerCase().indexOf(value.toLowerCase()) === 0) ||
-        name.toLowerCase().indexOf(value.toLowerCase()) > -1
-    )
-    .map(i => ({ ...i, key: i.address }))
-    .sort((a, b) => a.name.localeCompare(b.name))
-  return items
-}
-
 const LocalAutoComplete = React.memo(
   React.forwardRef(({ onChange, wide, value, required }, ref) => {
+    const { api } = useAragonApi()
     const [items, setItems] = useState([])
     const [selected, setSelected] = useState(null)
     const [searchTerm, setSearchTerm] = useState('')
     const selectedRef = useRef()
 
-    const handleSelectedClick = useCallback(() => {
-      setSelected(null)
-      onChange(selected.name)
-      setTimeout(() => {
-        ref.current.select()
-        ref.current.focus()
-      }, 0)
-    }, [ref, selected, onChange])
+    const handleSelectedClick = useCallback(
+      () => {
+        setSelected(null)
+        onChange(selected.name)
+        setTimeout(() => {
+          ref.current.select()
+          ref.current.focus()
+        }, 0)
+      },
+      [ref, selected, onChange]
+    )
     const handleSearch = useCallback(
-      term => {
+      async term => {
         if (term.length < 3) {
           setItems([])
           return
         }
-        const items = search(term)
+        const items = await api.searchIdentities(term).toPromise()
         setItems(items)
       },
-      [search]
+      [api]
     )
     const handleChange = useCallback(
       value => {
@@ -118,8 +56,8 @@ const LocalAutoComplete = React.memo(
       },
       [onChange]
     )
-    const renderItem = useCallback(({ address, name }, search) => {
-      if (search.indexOf('0x') === 0) {
+    const renderItem = useCallback(({ address, name }, searchTerm) => {
+      if (searchTerm.indexOf('0x') === 0) {
         return (
           <Option>
             <IdentityBadge compact badgeOnly entity={address} />
@@ -135,32 +73,38 @@ const LocalAutoComplete = React.memo(
       )
     })
 
-    useEffect(() => {
-      // reset
-      if (value === '') {
-        setSelected(null)
-        setSearchTerm(value)
-        handleSearch(value)
-        return
-      }
-      // value coming from up the tree not from typing
-      if (searchTerm === '') {
-        const exists = search(value)
-        if (exists && exists.length === 1) {
-          const item = exists[0]
-          if (
-            item.name.toLowerCase() === value.toLowerCase() ||
-            item.address.toLowerCase() === value.toLowerCase()
-          ) {
-            setSelected(item)
-            setSearchTerm(item.name)
-            handleSearch(item.name)
+    useEffect(
+      () => {
+        const effect = async () => {
+          // reset
+          if (value === '') {
+            setSelected(null)
+            setSearchTerm(value)
+            handleSearch(value)
             return
           }
+          // value coming from up the tree not from typing
+          if (searchTerm === '') {
+            const exists = await api.searchIdentities(value).toPromise()
+            if (exists && exists.length === 1) {
+              const item = exists[0]
+              if (
+                item.name.toLowerCase() === value.toLowerCase() ||
+                item.address.toLowerCase() === value.toLowerCase()
+              ) {
+                setSelected(item)
+                setSearchTerm(item.name)
+                handleSearch(item.name)
+                return
+              }
+            }
+            setSearchTerm(value)
+          }
         }
-        setSearchTerm(value)
-      }
-    }, [selected, value])
+        effect()
+      },
+      [selected, value, api]
+    )
 
     if (selected) {
       const { address, name } = selected
