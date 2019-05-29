479203ef516dcc356585687f1814de1027b99a19
Gorka <gorka@aquigorka.com>
2019-05-29 11:49:31 +0200

    LocalIdentitiesAutoComplete: refactor to use AutoCompleteSelected

--- apps/token-manager/app/src/components/LocalIdentitiesAutoComplete/LocalIdentitiesAutoComplete.js
+++ apps/token-manager/app/src/components/LocalIdentitiesAutoComplete/LocalIdentitiesAutoComplete.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useAragonApi } from '@aragon/api-react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { ButtonBase, EthIdenticon, IdentityBadge, theme } from '@aragon/ui'
-import AutoComplete from '../AutoComplete/AutoComplete'
+import { EthIdenticon, IdentityBadge, theme } from '@aragon/ui'
+import AutoCompleteSelected from '../AutoComplete/AutoCompleteSelected'
+
+const withKey = item => ({ key: item.address, ...item })
+const sortAlphAsc = (a, b) => a.name.localeCompare(b.name)
 
 const LocalAutoComplete = React.memo(
   React.forwardRef(({ onChange, wide, value, required }, ref) => {
@@ -11,19 +14,7 @@ const LocalAutoComplete = React.memo(
     const [items, setItems] = useState([])
     const [selected, setSelected] = useState(null)
     const [searchTerm, setSearchTerm] = useState('')
-    const selectedRef = useRef()
 
-    const handleSelectedClick = useCallback(
-      () => {
-        setSelected(null)
-        onChange(selected.name)
-        setTimeout(() => {
-          ref.current.select()
-          ref.current.focus()
-        }, 0)
-      },
-      [ref, selected, onChange]
-    )
     const handleSearch = useCallback(
       async term => {
         if (term.length < 3) {
@@ -31,7 +22,7 @@ const LocalAutoComplete = React.memo(
           return
         }
         const items = await api.searchIdentities(term).toPromise()
-        setItems(items)
+        setItems(items.map(withKey).sort(sortAlphAsc))
       },
       [api]
     )
@@ -50,12 +41,13 @@ const LocalAutoComplete = React.memo(
         handleSearch(name)
         setSelected(selected)
         onChange(address)
-        setTimeout(() => {
-          selectedRef.current.focus()
-        }, 0)
       },
       [onChange]
     )
+    const handleSelectedClick = () => {
+      setSelected(null)
+      onChange(selected.name)
+    }
     const renderItem = useCallback(({ address, name }, searchTerm) => {
       if (searchTerm.indexOf('0x') === 0) {
         return (
@@ -72,54 +64,47 @@ const LocalAutoComplete = React.memo(
         </Option>
       )
     })
-
-    useEffect(
-      () => {
-        const effect = async () => {
-          // reset
-          if (value === '') {
-            setSelected(null)
-            setSearchTerm(value)
-            handleSearch(value)
-            return
-          }
-          // value coming from up the tree not from typing
-          if (searchTerm === '') {
-            const exists = await api.searchIdentities(value).toPromise()
-            if (exists && exists.length === 1) {
-              const item = exists[0]
-              if (
-                item.name.toLowerCase() === value.toLowerCase() ||
-                item.address.toLowerCase() === value.toLowerCase()
-              ) {
-                setSelected(item)
-                setSearchTerm(item.name)
-                handleSearch(item.name)
-                return
-              }
-            }
-            setSearchTerm(value)
-          }
-        }
-        effect()
-      },
-      [selected, value, api]
-    )
-
-    if (selected) {
-      const { address, name } = selected
+    const renderSelected = useCallback(({ address, name }) => {
       return (
-        <SelectedWrap onClick={handleSelectedClick} ref={selectedRef}>
-          <Option selected>
-            <EthIdenticon address={address} scale={0.6} radius={2} />
-            <Name>{name}</Name>
-          </Option>
-        </SelectedWrap>
+        <Option selected>
+          <EthIdenticon address={address} scale={0.6} radius={2} />
+          <Name>{name}</Name>
+        </Option>
       )
-    }
+    })
+
+    useEffect(() => {
+      const effect = async () => {
+        // reset
+        if (value === '') {
+          setSelected(null)
+          setSearchTerm(value)
+          handleSearch(value)
+          return
+        }
+        // value coming from up the tree not from typing
+        if (searchTerm === '') {
+          const exists = await api.searchIdentities(value).toPromise()
+          if (exists && exists.length === 1) {
+            const item = exists[0]
+            if (
+              item.name.toLowerCase() === value.toLowerCase() ||
+              item.address.toLowerCase() === value.toLowerCase()
+            ) {
+              setSelected(item)
+              setSearchTerm(item.name)
+              handleSearch(item.name)
+              return
+            }
+          }
+          setSearchTerm(value)
+        }
+      }
+      effect()
+    }, [selected, value, api])
 
     return (
-      <AutoComplete
+      <AutoCompleteSelected
         itemButtonStyles={`
           border-left: 3px solid transparent;
           cursor: pointer;
@@ -135,9 +120,20 @@ const LocalAutoComplete = React.memo(
         items={items}
         onChange={handleChange}
         onSelect={handleSelect}
+        onSelectedClick={handleSelectedClick}
         ref={ref}
         renderItem={renderItem}
+        renderSelected={renderSelected}
         required={required}
+        selected={selected}
+        selectedButtonStyles={`
+          &:hover,
+          &:focus {
+            outline: none;
+            border: 1px solid ${theme.accent};
+            border-radius: 3px;
+          }
+        `}
         value={searchTerm}
         wide={wide}
       />
@@ -151,27 +147,6 @@ LocalAutoComplete.propTypes = {
   value: PropTypes.string,
   wide: PropTypes.bool,
 }
-
-const SelectedWrap = styled(ButtonBase)`
-  height: 40px;
-  width: 100%;
-  background: #fff;
-  display: grid;
-  align-items: center;
-  grid-gap: 8px;
-  grid-template-columns: auto 1fr;
-  padding: 0 8px;
-  cursor: pointer;
-  border: 1px solid ${theme.contentBorder};
-  border-radius: 3px;
-
-  &:hover,
-  &:focus {
-    outline: none;
-    border: 1px solid ${theme.accent};
-    border-radius: 3px;
-  }
-`
 
 const Option = styled.div`
   padding: 8px;
