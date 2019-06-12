3259c0515d682339293ec5e3fc725f52a5231bad
Gorka <gorka@aquigorka.com>
2019-06-12 15:26:22 +0200

    useArrowKeysFocus: update var names

--- apps/finance/app/src/hooks.js
+++ apps/finance/app/src/hooks.js
@@ -1,5 +1,8 @@
 import { useRef, useEffect, useCallback, useState } from 'react'
 
+const KEYCODE_UP = 38
+const KEYCODE_DOWN = 40
+
 export function useClickOutside(cb, existingRef) {
   const ref = existingRef || useRef()
   const handleClick = useCallback(
@@ -39,13 +42,15 @@ export function useOnBlur(cb, existingRef) {
 }
 
 export function useArrowKeysFocus(query, containerRef = useRef()) {
-  const [index, setIndex] = useState(-1)
+  /* eslint-enable react-hooks/rules-of-hooks */
+  const [highlightedIndex, setHighlightedIndex] = useState(-1)
 
-  const reset = () => setIndex(-1)
+  const reset = () => setHighlightedIndex(-1)
   const cycleFocus = useCallback(
-    change => {
+    (e, change) => {
+      e.preventDefault()
       const elements = document.querySelectorAll(query)
-      let next = index + change
+      let next = highlightedIndex + change
       if (next > elements.length - 1) {
         next = 0
       }
@@ -55,34 +60,38 @@ export function useArrowKeysFocus(query, containerRef = useRef()) {
       if (!elements[next]) {
         next = -1
       }
-      setIndex(next)
+      setHighlightedIndex(next)
     },
-    [index]
+    [highlightedIndex, query]
   )
   const handleKeyDown = useCallback(
-    ({ keyCode }) =>
-      keyCode === 38 ? cycleFocus(-1) : keyCode === 40 ? cycleFocus(1) : null,
+    e => {
+      const { keyCode } = e
+      if (keyCode === KEYCODE_UP || keyCode === KEYCODE_DOWN) {
+        cycleFocus(e, keyCode === KEYCODE_UP ? -1 : 1)
+      }
+    },
     [cycleFocus]
   )
 
   const { handleBlur: handleContainerBlur } = useOnBlur(reset, containerRef)
   useEffect(
     () => {
-      if (index === -1) {
+      if (highlightedIndex === -1) {
         return
       }
       const elements = document.querySelectorAll(query)
-      if (!elements[index]) {
+      if (!elements[highlightedIndex]) {
         return
       }
-      elements[index].focus()
+      elements[highlightedIndex].focus()
     },
-    [index]
+    [highlightedIndex, query]
   )
   useEffect(
     () => {
-      window.addEventListener('keydown', handleKeyDown)
-      return () => window.removeEventListener('keydown', handleKeyDown)
+      document.addEventListener('keydown', handleKeyDown)
+      return () => document.removeEventListener('keydown', handleKeyDown)
     },
     [handleKeyDown]
   )
