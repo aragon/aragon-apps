9b32d5b5b7606e4c308993abe090f9b0f731740c
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-07-11 18:17:06 -0300

    survey: fix solcover file

--- apps/survey/.solcover.js
+++ apps/survey/.solcover.js
@@ -1,11 +1,12 @@
 module.exports = {
     norpc: true,
-    copyPackages: ['@aragon/os', '@aragon/apps-shared-minime', '@aragon/test-helpers'],
+    copyPackages: ['@aragon/os', '@aragon/apps-shared-minime', '@aragon/test-helpers', '@aragon/apps-vault'],
     skipFiles: [
         'test',
         'examples',
         '@aragon/os',
-        '@aragon/apps-shared-minime/contracts/MiniMeToken.sol',
-        '@aragon/test-helpers/contracts/TimeHelpersMock.sol',
+        '@aragon/apps-vault',
+        '@aragon/test-helpers',
+        '@aragon/apps-shared-minime',
     ]
 }
