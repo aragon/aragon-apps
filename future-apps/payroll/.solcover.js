a4ae7fb6d126941fdde17248298892c084ff378b
Brett Sun <qisheng.brett.sun@gmail.com>
2019-07-17 16:31:06 +0200

    chore: upgrade solidity-coverage to v0.6+ (#921)

--- future-apps/payroll/.solcover.js
+++ future-apps/payroll/.solcover.js
@@ -1,13 +1,21 @@
 module.exports = {
     norpc: true,
-    copyPackages: ['@aragon/os', '@aragon/apps-finance', '@aragon/apps-vault', '@aragon/test-helpers'],
+    copyPackages: [
+        '@aragon/os',
+        '@aragon/apps-finance',
+        '@aragon/apps-vault',
+        '@aragon/ppf-contracts',
+        '@aragon/test-helpers'
+    ],
     skipFiles: [
-	'test',
+        'test',
         'examples',
         '@aragon/os',
         '@aragon/apps-vault',
         '@aragon/apps-finance',
         '@aragon/ppf-contracts',
         '@aragon/test-helpers',
-    ]
+    ],
+    // Turn on deep skip to avoid preprocessing (e.g. removing view/pure modifiers) for skipped files
+    deepSkip: true
 }
