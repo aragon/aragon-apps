821e3d50b5b2ead093ecc5eeebe60164a10637d0
Gorka <gorka@aquigorka.com>
2019-07-18 15:30:44 +0200

    App: refactor to new styles

--- apps/finance/app/src/App.js
+++ apps/finance/app/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { SyncIndicator, EmptyStateCard, Main, SidePanel } from '@aragon/ui'
+import {
+  SyncIndicator,
+  SidePanel,
+  Button,
+  Layout,
+  Header,
+  IconPlus,
+  useViewport,
+} from '@aragon/ui'
 import { useAragonApi } from '@aragon/api-react'
 import Balances from './components/Balances'
 import NewTransferPanelContent from './components/NewTransfer/PanelContent'
 import Transfers from './components/Transfers'
-import AppLayout from './components/AppLayout'
-import NewTransferIcon from './components/NewTransferIcon'
 import { ETHER_TOKEN_FAKE_ADDRESS } from './lib/token-utils'
 import { IdentityProvider } from './components/IdentityManager/IdentityManager'
-
-import addFundsIcon from './components/assets/add-funds-icon.svg'
 
 class App extends React.Component {
   static propTypes = {
@@ -85,90 +88,73 @@ class App extends React.Component {
   }
 
   render() {
-    const { appState, isSyncing } = this.props
+    const { appState, isSyncing, compactMode } = this.props
     const { newTransferOpened } = this.state
     const { balances, transactions, tokens, proxyAddress } = appState
 
     return (
-      <Main assetsUrl="./aragon-ui">
+      <IdentityProvider
+        onResolve={this.handleResolveLocalIdentity}
+        onShowLocalIdentityModal={this.handleShowLocalIdentityModal}
+      >
         <div css="min-width: 320px">
-          <IdentityProvider
-            onResolve={this.handleResolveLocalIdentity}
-            onShowLocalIdentityModal={this.handleShowLocalIdentityModal}
+          <SyncIndicator visible={isSyncing} />
+          <Layout>
+            <Header
+              primary="Finance"
+              secondary={
+                <Button
+                  mode="strong"
+                  onClick={this.handleNewTransferOpen}
+                  css={`
+                    ${compactMode &&
+                      `
+                        min-width: 40px;
+                        padding: 0;
+                      `}
+                  `}
+                >
+                  {compactMode ? <IconPlus /> : 'New transfer'}
+                </Button>
+              }
+            />
+            <Balances balances={balances} compactMode={compactMode} />
+            <Transfers
+              dao={proxyAddress}
+              transactions={transactions}
+              tokens={tokens}
+            />
+          </Layout>
+          <SidePanel
+            opened={newTransferOpened}
+            onClose={this.handleNewTransferClose}
+            title="New transfer"
           >
-            <SyncIndicator visible={isSyncing} />
-            <AppLayout
-              title="Finance"
-              mainButton={{
-                label: 'New transfer',
-                icon: <NewTransferIcon />,
-                onClick: this.handleNewTransferOpen,
-              }}
-              smallViewPadding={0}
-            >
-              {balances.length > 0 && (
-                <SpacedBlock>
-                  <Balances balances={balances} />
-                </SpacedBlock>
-              )}
-              {transactions.length > 0 && (
-                <SpacedBlock>
-                  <Transfers
-                    dao={proxyAddress}
-                    transactions={transactions}
-                    tokens={tokens}
-                  />
-                </SpacedBlock>
-              )}
-              {!isSyncing &&
-                balances.length === 0 &&
-                transactions.length === 0 && (
-                  <EmptyScreen>
-                    <EmptyStateCard
-                      icon={<img src={addFundsIcon} alt="" />}
-                      title="There are no funds yet"
-                      text="Create a new transfer to get started."
-                      actionText="New transfer"
-                      onActivate={this.handleNewTransferOpen}
-                    />
-                  </EmptyScreen>
-                )}
-            </AppLayout>
-            <SidePanel
+            <NewTransferPanelContent
               opened={newTransferOpened}
-              onClose={this.handleNewTransferClose}
-              title="New transfer"
-            >
-              <NewTransferPanelContent
-                opened={newTransferOpened}
-                tokens={tokens}
-                onWithdraw={this.handleWithdraw}
-                onDeposit={this.handleDeposit}
-                proxyAddress={proxyAddress}
-              />
-            </SidePanel>
-          </IdentityProvider>
+              tokens={tokens}
+              onWithdraw={this.handleWithdraw}
+              onDeposit={this.handleDeposit}
+              proxyAddress={proxyAddress}
+            />
+          </SidePanel>
         </div>
-      </Main>
+      </IdentityProvider>
     )
   }
 }
 
-const EmptyScreen = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-grow: 1;
-`
-
-const SpacedBlock = styled.div`
-  margin-top: 30px;
-  &:first-child {
-    margin-top: 0;
-  }
-`
-
 export default () => {
   const { api, appState } = useAragonApi()
-  return <App api={api} appState={appState} isSyncing={appState.isSyncing} />
+  const { below } = useViewport()
+  const compactMode = below('medium')
+
+  return (
+    <App
+      api={api}
+      appState={appState}
+      isSyncing={appState.isSyncing}
+      compactMode={compactMode}
+    />
+  )
 }
