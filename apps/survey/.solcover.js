af093c6011e76929db29f397197c36ad18422c85
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-07-22 10:11:17 -0300

    Survey: Add example survey kit (#915)

--- apps/survey/.solcover.js
+++ apps/survey/.solcover.js
@@ -1,11 +1,13 @@
 module.exports = {
     norpc: true,
-    copyPackages: ['@aragon/os', '@aragon/apps-shared-minime', '@aragon/test-helpers'],
+    copyPackages: ['@aragon/os', '@aragon/apps-shared-minime', '@aragon/test-helpers', '@aragon/apps-vault'],
     skipFiles: [
         'test',
+        'examples',
         '@aragon/os',
-        '@aragon/apps-shared-minime',
+        '@aragon/apps-vault',
         '@aragon/test-helpers',
+        '@aragon/apps-shared-minime',
     ],
     // Turn on deep skip to avoid preprocessing (e.g. removing view/pure modifiers) for skipped files
     deepSkip: true
