aba16f8d9b468c4df6822e04c5fdf0dd46e3f534
Gorka <gorka@aquigorka.com>
2019-07-26 10:07:16 +0200

    Votes: setup boilerplate for new layout

--- apps/voting/app/src/screens/Votes.js
+++ apps/voting/app/src/screens/Votes.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BackButton, Bar } from '@aragon/ui'
 import VotingCard from '../components/VotingCard/VotingCard'
 import VotingCardGroup from '../components/VotingCard/VotingCardGroup'
+import Vote from '../components/Vote'
 
-const Votes = React.memo(({ votes, onSelectVote }) => {
-  const sortedVotes = votes.sort((a, b) => {
-    const dateDiff = b.data.endDate - a.data.endDate
-    // Order by descending voteId if there's no end date difference
-    return dateDiff !== 0 ? dateDiff : b.voteId - a.voteId
-  })
+const sortVotes = (a, b) => {
+  const dateDiff = b.data.endDate - a.data.endDate
+  // Order by descending voteId if there's no end date difference
+  return dateDiff !== 0 ? dateDiff : b.voteId - a.voteId
+}
 
+const useFilterVotes = votes => {
+  return { filteredVotes: votes }
+}
+
+const useVotes = votes => {
+  const sortedVotes = votes.sort(sortVotes)
   const openVotes = sortedVotes.filter(vote => vote.data.open)
   const closedVotes = sortedVotes.filter(vote => !openVotes.includes(vote))
+  return { openVotes, closedVotes }
+}
+
+const LayoutVotes = ({ votes, selectedVote, selectVote }) => {
+  const { filteredVotes } = useFilterVotes(votes)
+  const { openVotes, closedVotes } = useVotes(filteredVotes)
+  const handleBackClick = () => selectVote(-1)
+
+  return (
+    <React.Fragment>
+      {selectedVote !== null && (
+        <React.Fragment>
+          <Bar>
+            <BackButton onClick={handleBackClick} />
+          </Bar>
+          <Vote vote={selectedVote} />
+        </React.Fragment>
+      )}
+      {!selectedVote && (
+        <React.Fragment>
+          <Bar>Filters</Bar>
+          <Votes
+            openVotes={openVotes}
+            closedVotes={closedVotes}
+            onSelectVote={selectVote}
+          />
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
+}
+
+const Votes = React.memo(({ openVotes, closedVotes, onSelectVote }) => {
   const votingGroups = [['Open votes', openVotes], ['Past votes', closedVotes]]
 
   return (
@@ -32,4 +72,4 @@ const Votes = React.memo(({ votes, onSelectVote }) => {
   )
 })
 
-export default Votes
+export default LayoutVotes
