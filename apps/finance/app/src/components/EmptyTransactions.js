8891cead11d4b95ce147f3535c5437e1c36af917
Brett Sun <qisheng.brett.sun@gmail.com>
2019-08-18 02:12:31 +0200

    Finance: update empty transactions to say loading if still syncing

--- apps/finance/app/src/components/EmptyTransactions.js
+++ apps/finance/app/src/components/EmptyTransactions.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useAragonApi } from '@aragon/api-react'
 import { Box, GU, useTheme } from '@aragon/ui'
 import noTransfersSvg from './assets/no-transfers.svg'
 
 function EmptyTransactions() {
+  const { appState } = useAragonApi()
   const theme = useTheme()
 
   return (
@@ -29,7 +31,7 @@ function EmptyTransactions() {
             color: ${theme.content};
           `}
         >
-          No transfers yet!
+          {appState.isSyncing ? 'Loading…' : 'No transfers yet!'}
         </h3>
       </div>
     </Box>
