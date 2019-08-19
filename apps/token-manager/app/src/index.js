3c552395a8954ee64a7fe4d645076e006a1c2b83
Pierre Bertet <hello@pierre.world>
2019-08-19 12:45:11 +0200

    Tokens: new style (#918)

--- apps/token-manager/app/src/index.js
+++ apps/token-manager/app/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AragonApi } from '@aragon/api-react'
+import { Main } from '@aragon/ui'
 import appStateReducer from './app-state-reducer'
 import App from './App'
 
 ReactDOM.render(
   <AragonApi reducer={appStateReducer}>
-    <App />
+    <Main assetsUrl="./aragon-ui">
+      <App />
+    </Main>
   </AragonApi>,
   document.getElementById('root')
 )
--- apps/token-manager/app/src/screens/EmptyState.js
+++ apps/token-manager/app/src/screens/EmptyState.js
@@ -1,26 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
-import { EmptyStateCard } from '@aragon/ui'
+import { Button, EmptyStateCard, GU } from '@aragon/ui'
 import emptyIcon from '../assets/empty-card-icon.svg'
 
-const EmptyState = ({ onActivate }) => (
-  <Main>
-    <EmptyStateCard
-      icon={<img src={emptyIcon} alt="" />}
-      title="There are no token holders yet"
-      text="Add tokens to an address to get started."
-      actionText="Add tokens"
-      onActivate={onActivate}
-    />
-  </Main>
-)
-
-const Main = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-grow: 1;
-  padding: 20px 0;
-`
+const EmptyState = React.memo(function NoVotes({ onAssignHolder }) {
+  return (
+    <div
+      css={`
+        height: calc(100vh - ${8 * GU}px);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <EmptyStateCard
+        text="There are no token holders yet!"
+        action={
+          <Button wide mode="strong" onClick={onAssignHolder}>
+            Add tokens to an address
+          </Button>
+        }
+        illustration={
+          <img css="margin: auto;" src={emptyIcon} alt="No token holders" />
+        }
+      />
+    </div>
+  )
+})
 
 export default EmptyState
