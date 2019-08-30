7aa4a96f292665dbe874335f30fa8db877df8972
Gorka <gorka@aquigorka.com>
2019-08-30 10:30:08 +0200

    Transfer: update rendering according to new mocked data

--- apps/agent/app/src/components/Transfers.js
+++ apps/agent/app/src/components/Transfers.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import {
   compareDesc,
@@ -32,6 +33,11 @@ import EmptyFilteredTransfers from './EmptyFilteredTransfers'
 import { useIdentity, IdentityContext } from './IdentityManager/IdentityManager'
 import LocalIdentityBadge from './LocalIdentityBadge/LocalIdentityBadge'
 
+const TYPES = {
+  'direct transfer': 'Transfer',
+  'direct deposit': 'Deposit',
+  'contract interaction': 'Contract interaction',
+}
 const INITIAL_DATE_RANGE = { start: null, end: null }
 const TRANSFER_TYPES = [
   TransferTypes.All,
@@ -77,9 +83,9 @@ const getDownloadData = async (transfers, tokenDetails, resolveAddress) => {
         date,
         numData: { amount },
         reference,
-        isIncoming,
         entity,
         token,
+        isIncoming,
       }) => {
         const { symbol, decimals } = tokenDetails[toChecksumAddress(token)]
         const formattedAmount = formatTokenAmount(
@@ -221,9 +227,10 @@ const Transfers = React.memo(({ dao, tokens, transactions }) => {
           ? []
           : [
               { label: 'Date', priority: 2 },
-              { label: 'Source/recipient', priority: 3 },
+              { label: 'Source/recipient', priority: 5 },
+              { label: 'Type', priority: 3 },
               { label: 'Reference', priority: 1 },
-              { label: 'Amount', priority: 2 },
+              { label: 'Amount', priority: 4 },
             ]
       }
       entries={filteredTransfers.sort(
@@ -231,31 +238,78 @@ const Transfers = React.memo(({ dao, tokens, transactions }) => {
           // Sort by date descending
           compareDesc(dateLeft, dateRight)
       )}
+      renderEntryExpansion={({ tokenTransfers }) => {
+        if (tokenTransfers.length === 1) {
+          return
+        }
+
+        return tokenTransfers.map(({ to, from, amount, token }) => {
+          const isIncoming = !!from
+          const { symbol, decimals } = tokenDetails[toChecksumAddress(token)]
+          const formattedAmount = formatTokenAmount(
+            amount,
+            isIncoming,
+            decimals,
+            true,
+            { rounding: 5 }
+          )
+
+          return (
+            <div
+              css={`
+                width: 100%;
+                display: grid;
+                grid-template-columns: auto auto 1fr;
+                grid-gap: ${3 * GU}px;
+                align-items: center;
+                justify-content: space-between;
+              `}
+            >
+              <InnerEntryColumn above={above} theme={theme}>
+                From: <AgentOrEntity entity={from} />
+              </InnerEntryColumn>
+              <InnerEntryColumn above={above} theme={theme}>
+                To: <AgentOrEntity entity={to} />
+              </InnerEntryColumn>
+              <div
+                css={`
+                  text-align: right;
+                  color: ${isIncoming
+                    ? theme.positive
+                    : theme.surfaceContentSecondary};
+                `}
+              >
+                {formattedAmount} {symbol}
+              </div>
+            </div>
+          )
+        })
+      }}
       renderEntry={({
         date,
-        entity,
-        reference,
+        description: reference,
+        type,
+        tokenTransfers,
+        onlyOne,
         isIncoming,
-        numData: { amount },
-        token,
       }) => {
-        const formattedDate = format(date, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
-        const { symbol, decimals } = tokenDetails[toChecksumAddress(token)]
-        const formattedAmount = formatTokenAmount(
-          amount,
-          isIncoming,
-          decimals,
-          true,
-          { rounding: 5 }
+        const [{ token, amount, to, from }] = tokenTransfers
+        const formattedDate = format(date, "yyyy-MM-dd'T'HH:mm:ss")
+        const dateDiv = (
+          <time
+            dateTime={formattedDate}
+            title={formattedDate}
+            css={`
+              ${textStyle('body2')};
+              color: ${theme.surfaceContent};
+            `}
+          >
+            {format(date, 'MM/DD/YY')}
+          </time>
         )
-
-        return [
-          <time dateTime={formattedDate} title={formattedDate}>
-            {format(date, 'dd/MM/yy')}
-          </time>,
+        const badgeDiv = onlyOne ? (
           <div
             css={`
-              padding: 0 ${0.5 * GU}px;
               ${above('medium') &&
                 `
                   display: inline-flex;
@@ -265,34 +319,80 @@ const Transfers = React.memo(({ dao, tokens, transactions }) => {
           >
             <LocalIdentityBadge
               networkType={network.type}
-              entity={entity}
-              address={entity}
+              entity={to || from}
             />
-          </div>,
+          </div>
+        ) : (
+          <LocalIdentityBadge badgeOnly entity="Multiple accounts" />
+        )
+        const typeDiv = (
           <div
             css={`
-              padding: 0 ${0.5 * GU}px;
+              ${textStyle('body2')};
+              color: ${theme.surfaceContent};
+            `}
+          >
+            {TYPES[type]}
+          </div>
+        )
+        const referenceDiv = (
+          <div
+            css={`
+              ${textStyle('body2')};
+              color: ${theme.surfaceContent};
             `}
           >
             {reference}
-          </div>,
-          <span
-            css={`
-              font-weight: 600;
-              color: ${isIncoming ? theme.positive : theme.negative};
-            `}
-          >
-            {formattedAmount} {symbol}
-          </span>,
-        ]
+          </div>
+        )
+        const amountDiv = (() => {
+          if (!onlyOne) {
+            const uniqueTokens = Object.keys(
+              tokenTransfers.reduce((p, { token }) => {
+                p[token] = 1
+                return p
+              }, {})
+            ).length
+            return (
+              <div
+                css={`
+                  ${textStyle('body2')};
+                  color: ${theme.surfaceContent};
+                `}
+              >
+                {uniqueTokens} token{uniqueTokens > 1 ? 's' : ''}
+              </div>
+            )
+          }
+
+          const { symbol, decimals } = tokenDetails[toChecksumAddress(token)]
+          const formattedAmount = formatTokenAmount(
+            amount,
+            isIncoming,
+            decimals,
+            true,
+            { rounding: 5 }
+          )
+          return (
+            <span
+              css={`
+                ${textStyle('body2')};
+                color: ${isIncoming ? theme.positive : theme.surfaceContent};
+              `}
+            >
+              {formattedAmount} {symbol}
+            </span>
+          )
+        })()
+
+        return [dateDiv, badgeDiv, typeDiv, referenceDiv, amountDiv]
       }}
-      renderEntryActions={({ entity, transactionHash }) => (
+      renderEntryActions={({ transactionHash }) => (
         <ContextMenu>
           <ContextMenuViewTransaction
             transactionHash={transactionHash}
             network={network}
           />
-          <ContextMenuItemCustomLabel entity={entity} />
         </ContextMenu>
       )}
     />
@@ -305,17 +405,36 @@ Transfers.propTypes = {
   transactions: PropTypes.array.isRequired,
 }
 
-const ContextMenuItemCustomLabel = ({ entity }) => {
-  const [label, showLocalIdentityModal] = useIdentity(entity)
-  const handleEditLabel = useCallback(() => showLocalIdentityModal(entity))
+const InnerEntryColumn = styled.div`
+  color: ${({ theme }) => theme.surfaceContentSecondary};
+  ${textStyle('label2')};
+  display: inline-grid;
+  grid-template-columns: 40px 1fr;
+  grid-gap: ${0.5 * GU}px;
+  align-items: center;
+  width: 200px;
 
-  return (
-    <ContextMenuItem onClick={handleEditLabel}>
-      <IconLabel />
-      {label ? 'Edit' : 'Add'} custom label
-    </ContextMenuItem>
-  )
-}
+  ${({ above }) =>
+    above('medium')
+      ? `
+          max-width: 250px;
+          width: 250px;
+        `
+      : ''}
+  ${({ above }) =>
+    above('large')
+      ? `
+          /* max possible length of custom label + from length + some spacing */
+          min-width: 342px;
+          max-width: unset;
+          width: unset;
+        `
+      : ''}
+`
+
+const AgentOrEntity = ({ entity }) => (
+  <LocalIdentityBadge entity={entity ? entity : 'Agent'} badgeOnly={!!entity} />
+)
 
 const ContextMenuViewTransaction = ({ transactionHash, network }) => {
   const handleViewTransaction = useCallback(() => {
