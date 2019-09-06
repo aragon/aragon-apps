9a13b620643b8c540a45870bfa0620031125eab1
Gorka <gorka@aquigorka.com>
2019-09-06 08:50:04 +0200

    LocalLabelAppBadge: add missing dep

--- apps/voting/app/src/components/LocalIdentityBadge/LocalLabelAppBadge.js
+++ apps/voting/app/src/components/LocalIdentityBadge/LocalLabelAppBadge.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { useNetwork } from '@aragon/api-react'
 import { AppBadge } from '@aragon/ui'
 import { useIdentity } from '../../identity-manager'
