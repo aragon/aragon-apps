70d65d416b754c6e1c695fc6eed43a35020e65bc
Brett Sun <qisheng.brett.sun@gmail.com>
2019-11-23 12:30:32 +0100

    Finance: only show token icons on test networks for known test tokens

--- apps/finance/app/src/lib/icon-utils.js
+++ apps/finance/app/src/lib/icon-utils.js
@@ -1,4 +1,5 @@
 import { tokenIconUrl as _tokenIconUrl } from '@aragon/ui'
+import { getTestTokenAddresses } from '../testnet'
 import { ETHER_TOKEN_VERIFIED_BY_SYMBOL } from './verified-tokens'
 
 // Small shim on top of @aragon/ui's tokenIconUrl, to handle our testnet tokens
@@ -7,7 +8,12 @@ export const tokenIconUrl = (tokenAddress, tokenSymbol, networkType) => {
     return _tokenIconUrl(tokenAddress)
   }
 
-  // On other networks, pretend any token with the same symbol is legit
-  const mainnetEquivalent = ETHER_TOKEN_VERIFIED_BY_SYMBOL.get(tokenSymbol)
-  return mainnetEquivalent ? _tokenIconUrl(mainnetEquivalent) : ''
+  // On other networks, only pretend known test tokens are legit
+  const testTokens = new Set(getTestTokenAddresses(networkType))
+  if (testTokens.has(tokenAddress.toLowerCase())) {
+    const mainnetEquivalent = ETHER_TOKEN_VERIFIED_BY_SYMBOL.get(tokenSymbol)
+    return mainnetEquivalent ? _tokenIconUrl(mainnetEquivalent) : ''
+  }
+
+  return ''
 }
