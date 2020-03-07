c4628f192102bdb4cb5aee15a58a72baf8800727
ajs <palebluedot@gmail.com>
2020-03-07 13:35:13 -0300

    Final changes to make agent work with Buidler

--- apps/agent/contracts/test/TestImports.sol
+++ apps/agent/contracts/test/TestImports.sol
@@ -14,12 +14,11 @@ import "@aragon/os/contracts/lib/ens/ENS.sol";
 import "@aragon/os/contracts/lib/ens/AbstractENS.sol";
 import "@aragon/os/contracts/lib/ens/PublicResolver.sol";
 
-import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
-import "@aragon/test-helpers/contracts/EtherTokenConstantMock.sol";
-import "@aragon/test-helpers/contracts/TokenMock.sol";
-import "@aragon/test-helpers/contracts/TokenReturnFalseMock.sol";
-import "@aragon/test-helpers/contracts/TokenReturnMissingMock.sol";
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/contract-test-helpers/contracts/EtherTokenConstantMock.sol";
+import "@aragon/contract-test-helpers/contracts/TokenMock.sol";
+import "@aragon/contract-test-helpers/contracts/TokenReturnFalseMock.sol";
+import "@aragon/contract-test-helpers/contracts/TokenReturnMissingMock.sol";
 import "@aragon/contract-test-helpers/contracts/TokenReturnMissingMock.sol";
 
 
