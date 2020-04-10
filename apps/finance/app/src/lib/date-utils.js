512330ce0d683ac039f76b619ae461d095c40117
Enrique Ortiz <hi@enriqueortiz.dev>
2020-04-10 15:29:19 -0400

    Remove / avoid exporting unneeded date formats

--- apps/finance/app/src/lib/date-utils.js
+++ apps/finance/app/src/lib/date-utils.js
@@ -1,7 +1,6 @@
 import { format } from 'date-fns'
 
-export const MMDDYY_FUNC_FORMAT = 'MM/dd/yy'
-export const YYMMDD_LONG_FORMAT = 'yyyy-MM-dd'
+const YYMMDD_LONG_FORMAT = 'yyyy-MM-dd'
 
 export function formatDate(date, dateFormat = YYMMDD_LONG_FORMAT) {
   return format(date, dateFormat)
