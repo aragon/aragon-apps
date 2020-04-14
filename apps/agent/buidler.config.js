68b42523dc8d1b87ec5ff1959d198b79cc2aa00f
Gabriel <gabrielpk.18@gmail.com>
2020-04-14 02:17:54 -0300

    Fix: use ganache-cli to fix coverage and gas report

--- apps/agent/buidler.config.js
+++ apps/agent/buidler.config.js
@@ -11,6 +11,9 @@ module.exports = {
   networks: {
     localhost: {
       url: 'http://localhost:8545',
+      accounts: {
+        mnemonic: "explain tackle mirror kit van hammer degree position ginger unfair soup bonus"
+      }
     },
     coverage: {
       url: 'http://localhost:8555',
