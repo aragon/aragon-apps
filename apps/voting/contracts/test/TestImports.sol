31d1c5a0ca03c543d703b801a49b0daf8e431cb1
Gabriel <gabrielpk.18@gmail.com>
2020-04-14 01:59:33 -0300

    Fix: use ganache-cli to fix coverage and gas report

---
+++ apps/voting/contracts/test/TestImports.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.24;
+
+import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/kernel/Kernel.sol";
+import "@aragon/os/contracts/factory/DAOFactory.sol";
+import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
+
+import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
+
+// You might think this file is a bit odd, but let me explain.
+// We only use some contracts in our tests, which means Truffle
+// will not compile it for us, because it is from an external
+// dependency.
+//
+// We are now left with three options:
+// - Copy/paste these contracts
+// - Run the tests with `truffle compile --all` on
+// - Or trick Truffle by claiming we use it in a Solidity test
+//
+// You know which one I went for.
+
+
+contract TestImports {
+    constructor() public {
+        // solium-disable-previous-line no-empty-blocks
+    }
+}
