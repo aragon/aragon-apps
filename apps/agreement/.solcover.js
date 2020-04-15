8c747ea3cd16e44bb236a14d748abee6b3bdc239
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-04-15 16:39:32 -0300

    agreement: init project

---
+++ apps/agreement/.solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+    norpc: true,
+    copyPackages: ['@aragon/os', '@aragon/apps-vault'],
+    skipFiles: [
+        'test',
+        '@aragon/os',
+        '@aragon/apps-vault',
+    ]
+}
