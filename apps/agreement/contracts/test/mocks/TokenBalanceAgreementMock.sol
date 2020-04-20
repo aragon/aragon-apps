2c38bde2a94bdb624911887c6db687398772a5ad
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-04-20 17:56:55 -0300

    agreement: model token-balance based permissions

---
+++ apps/agreement/contracts/test/mocks/TokenBalanceAgreementMock.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.4.24;
+
+import "../../TokenBalanceAgreement.sol";
+import "@aragon/test-helpers/contracts/TimeHelpersMock.sol";
+
+
+contract TokenBalanceAgreementMock is TokenBalanceAgreement, TimeHelpersMock {}
