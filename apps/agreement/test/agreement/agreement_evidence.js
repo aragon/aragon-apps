12019fbff3af9932d3a79695e7189d2f19e374ed
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-05-07 08:38:04 -0300

    agreements: remove old executor

--- apps/agreement/test/agreement/agreement_evidence.js
+++ apps/agreement/test/agreement/agreement_evidence.js
@@ -1,86 +1,40 @@
-const ERRORS = require('./helpers/utils/errors')
-const { RULINGS } = require('./helpers/utils/enums')
-const { assertBn } = require('./helpers/assert/assertBn')
-const { assertRevert } = require('./helpers/assert/assertThrow')
-const { decodeEventsOfType } = require('./helpers/lib/decodeEvent')
-const { assertEvent, assertAmountOfEvents } = require('./helpers/assert/assertEvent')
+const { assertBn } = require('../helpers/assert/assertBn')
+const { assertRevert } = require('../helpers/assert/assertThrow')
+const { decodeEventsOfType } = require('../helpers/lib/decodeEvent')
+const { assertEvent, assertAmountOfEvents } = require('../helpers/assert/assertEvent')
+const { RULINGS } = require('../helpers/utils/enums')
+const { AGREEMENT_ERRORS } = require('../helpers/utils/errors')
 
-const deployer = require('./helpers/utils/deployer')(web3, artifacts)
+const deployer = require('../helpers/utils/deployer')(web3, artifacts)
 
 contract('Agreement', ([_, someone, submitter, challenger]) => {
   let agreement, actionId
 
   beforeEach('deploy agreement instance', async () => {
-    agreement = await deployer.deployAndInitializeWrapper()
+    agreement = await deployer.deployAndInitializeWrapperWithExecutor()
   })
 
   describe('evidence', () => {
     context('when the given action exists', () => {
       beforeEach('create action', async () => {
-        ({ actionId } = await agreement.schedule({ submitter }))
+        ({ actionId } = await agreement.newAction({ submitter }))
       })
 
       const itCannotSubmitEvidence = () => {
         it('reverts', async () => {
-          await assertRevert(agreement.submitEvidence({ actionId, from: submitter }), ERRORS.ERROR_CANNOT_SUBMIT_EVIDENCE)
+          await assertRevert(agreement.submitEvidence({ actionId, from: submitter }), AGREEMENT_ERRORS.ERROR_CANNOT_SUBMIT_EVIDENCE)
         })
       }
 
       const itCannotSubmitEvidenceForNonExistingDispute = () => {
         it('reverts', async () => {
-          await assertRevert(agreement.submitEvidence({ actionId, from: submitter }), ERRORS.ERROR_DISPUTE_DOES_NOT_EXIST)
+          await assertRevert(agreement.submitEvidence({ actionId, from: submitter }), AGREEMENT_ERRORS.ERROR_DISPUTE_DOES_NOT_EXIST)
         })
       }
 
-      context('when the action was not cancelled', () => {
+      context('when the action was not closed', () => {
         context('when the action was not challenged', () => {
-          context('at the beginning of the delay period', () => {
-            itCannotSubmitEvidenceForNonExistingDispute()
-          })
-
-          context('in the middle of the challenge period', () => {
-            beforeEach('move before the delay period end date', async () => {
-              await agreement.moveBeforeEndOfDelayPeriod(actionId)
-            })
-
-            itCannotSubmitEvidenceForNonExistingDispute()
-          })
-
-          context('at the end of the delay period', () => {
-            beforeEach('move to the delay period end date', async () => {
-              await agreement.moveToEndOfDelayPeriod(actionId)
-            })
-
-            itCannotSubmitEvidenceForNonExistingDispute()
-          })
-
-          context('after the delay period', () => {
-            beforeEach('move after the delay period end date', async () => {
-              await agreement.moveAfterDelayPeriod(actionId)
-            })
-
-            context('when the action was not executed', () => {
-              context('when the action was not cancelled', () => {
-                itCannotSubmitEvidenceForNonExistingDispute()
-              })
-
-              context('when the action was cancelled', () => {
-                beforeEach('cancel action', async () => {
-                  await agreement.cancel({ actionId })
-                })
-
-                itCannotSubmitEvidenceForNonExistingDispute()
-              })
-            })
-
-            context('when the action was executed', () => {
-              beforeEach('execute action', async () => {
-                await agreement.execute({ actionId })
-              })
-
-              itCannotSubmitEvidenceForNonExistingDispute()
-            })
-          })
+          itCannotSubmitEvidenceForNonExistingDispute()
         })
 
         context('when the action was challenged', () => {
@@ -161,14 +115,13 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
                     it('can be ruled or submit evidence', async () => {
                       await agreement.submitEvidence({ actionId, evidence, from, finished })
 
-                      const { canCancel, canChallenge, canSettle, canDispute, canClaimSettlement, canRuleDispute, canExecute } = await agreement.getAllowedPaths(actionId)
+                      const { canProceed, canChallenge, canSettle, canDispute, canClaimSettlement, canRuleDispute } = await agreement.getAllowedPaths(actionId)
                       assert.isTrue(canRuleDispute, 'action dispute cannot be ruled')
-                      assert.isFalse(canCancel, 'action can be cancelled')
+                      assert.isFalse(canProceed, 'action can proceed')
                       assert.isFalse(canChallenge, 'action can be challenged')
                       assert.isFalse(canSettle, 'action can be settled')
                       assert.isFalse(canDispute, 'action can be disputed')
                       assert.isFalse(canClaimSettlement, 'action settlement can be claimed')
-                      assert.isFalse(canExecute, 'action can be executed')
                     })
                   }
 
@@ -194,7 +147,7 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
                     })
 
                     it('reverts', async () => {
-                      await assertRevert(agreement.submitEvidence({ actionId, from }), ERRORS.ERROR_SUBMITTER_FINISHED_EVIDENCE)
+                      await assertRevert(agreement.submitEvidence({ actionId, from }), AGREEMENT_ERRORS.ERROR_SUBMITTER_FINISHED_EVIDENCE)
                     })
                   })
                 })
@@ -212,7 +165,7 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
                     })
 
                     it('reverts', async () => {
-                      await assertRevert(agreement.submitEvidence({ actionId, from }), ERRORS.ERROR_SUBMITTER_FINISHED_EVIDENCE)
+                      await assertRevert(agreement.submitEvidence({ actionId, from }), AGREEMENT_ERRORS.ERROR_CHALLENGER_FINISHED_EVIDENCE)
                     })
                   })
                 })
@@ -221,7 +174,7 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
                   const from = someone
 
                   it('reverts', async () => {
-                    await assertRevert(agreement.submitEvidence({ actionId, from }), ERRORS.ERROR_SENDER_NOT_ALLOWED)
+                    await assertRevert(agreement.submitEvidence({ actionId, from }), AGREEMENT_ERRORS.ERROR_SENDER_NOT_ALLOWED)
                   })
                 })
               })
@@ -232,23 +185,13 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
                     await agreement.executeRuling({ actionId, ruling: RULINGS.IN_FAVOR_OF_SUBMITTER })
                   })
 
-                  context('when the action was not executed', () => {
-                    context('when the action was not cancelled', () => {
-                      itCannotSubmitEvidence()
-                    })
-
-                    context('when the action was cancelled', () => {
-                      beforeEach('cancel action', async () => {
-                        await agreement.cancel({ actionId })
-                      })
-
-                      itCannotSubmitEvidence()
-                    })
+                  context('when the action was not closed', () => {
+                    itCannotSubmitEvidence()
                   })
 
-                  context('when the action was executed', () => {
-                    beforeEach('execute action', async () => {
-                      await agreement.execute({ actionId })
+                  context('when the action was closed', () => {
+                    beforeEach('close action', async () => {
+                      await agreement.close({ actionId })
                     })
 
                     itCannotSubmitEvidence()
@@ -276,9 +219,9 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
         })
       })
 
-      context('when the action was cancelled', () => {
-        beforeEach('cancel action', async () => {
-          await agreement.cancel({ actionId })
+      context('when the action was closed', () => {
+        beforeEach('close action', async () => {
+          await agreement.close({ actionId })
         })
 
         itCannotSubmitEvidenceForNonExistingDispute()
@@ -287,7 +230,7 @@ contract('Agreement', ([_, someone, submitter, challenger]) => {
 
     context('when the given action does not exist', () => {
       it('reverts', async () => {
-        await assertRevert(agreement.submitEvidence({ actionId: 0, from: submitter }), ERRORS.ERROR_ACTION_DOES_NOT_EXIST)
+        await assertRevert(agreement.submitEvidence({ actionId: 0, from: submitter }), AGREEMENT_ERRORS.ERROR_ACTION_DOES_NOT_EXIST)
       })
     })
   })
