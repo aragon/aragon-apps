aea214c5f63afcdf0e2c394281415bcac2a6454d
Pierre Bertet <hello@pierre.world>
2020-06-09 17:56:21 +0100

    Balances: CSS improvements / scrollbar position

--- apps/finance/app/src/components/Balances.js
+++ apps/finance/app/src/components/Balances.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import BN from 'bn.js'
-import { Box, GU, textStyle, useTheme } from '@aragon/ui'
+import { Box, GU, textStyle, useTheme, useLayout } from '@aragon/ui'
 import BalanceToken from './BalanceToken'
 
 const CONVERT_API_RETRY_DELAY = 2000
@@ -80,88 +80,88 @@ function useBalanceItems(balances) {
 
 function Balances({ balances, compactMode }) {
   const theme = useTheme()
+  const { layoutName } = useLayout()
   const balanceItems = useBalanceItems(balances)
 
   return (
-    <Box heading="Token Balances">
+    <Box heading="Token Balances" padding={0}>
       <div
         css={`
-          /*
-            * translate3d() fixes an issue on recent Firefox versions where the
-            * scrollbar would briefly appear on top of everything (including the
-            * sidepanel overlay).
-            */
-          min-height: 112px;
-          transform: translate3d(0, 0, 0);
-          overflow-x: auto;
-          ${balanceItems.length === 0
-            ? `
-                display: flex;
-                justify-content: center;
-                align-items: center;
-              `
-            : ''}
+          padding: ${(layoutName === 'small' ? 1 : 2) * GU}px;
         `}
       >
-        {balanceItems.length === 0 ? (
-          <div
-            css={`
-              ${textStyle('body1')};
-              color: ${theme.content};
-            `}
-          >
-            No token balances yet.
-          </div>
-        ) : (
-          <ul
-            css={`
-              list-style: none;
-              display: flex;
-              ${compactMode
-                ? `
+        <div
+          css={`
+            display: flex;
+            align-items: center;
+            min-height: ${14 * GU}px;
+            overflow-x: auto;
+            padding: ${1 * GU}px;
+          `}
+        >
+          {balanceItems.length === 0 ? (
+            <div
+              css={`
+                display: flex;
+                width: 100%;
+                justify-content: center;
+                ${textStyle('body1')};
+                color: ${theme.content};
+              `}
+            >
+              No token balances yet.
+            </div>
+          ) : (
+            <ul
+              css={`
+                list-style: none;
+                display: flex;
+                ${compactMode
+                  ? `
                     flex-direction: column;
                     padding: ${1 * GU}px 0;
                   `
-                : ''}
-            `}
-          >
-            {balanceItems.map(
-              ({
-                address,
-                amount,
-                convertedAmount,
-                decimals,
-                symbol,
-                verified,
-              }) => (
-                <li
-                  key={address}
-                  css={`
-                    flex-shrink: 0;
-                    display: block;
-                    min-width: ${20 * GU}px;
-                    padding-right: ${3 * GU}px;
-                    ${compactMode ? `margin-bottom: ${3 * GU}px;` : ''}
-                    &:last-of-type {
-                      min-width: unset;
-                      margin-bottom: 0;
-                    }
-                  `}
-                >
-                  <BalanceToken
-                    address={address}
-                    amount={amount}
-                    compact={compactMode}
-                    convertedAmount={convertedAmount}
-                    decimals={decimals}
-                    symbol={symbol}
-                    verified={verified}
-                  />
-                </li>
-              )
-            )}
-          </ul>
-        )}
+                  : ''}
+              `}
+            >
+              {balanceItems.map(
+                ({
+                  address,
+                  amount,
+                  convertedAmount,
+                  decimals,
+                  symbol,
+                  verified,
+                }) => (
+                  <li
+                    key={address}
+                    css={`
+                      flex-shrink: 0;
+                      display: block;
+                      min-width: ${20 * GU}px;
+                      padding-right: ${3 * GU}px;
+                      ${compactMode ? `margin-bottom: ${3 * GU}px;` : ''}
+                      &:last-of-type {
+                        min-width: unset;
+                        margin-bottom: 0;
+                      }
+                    `}
+                  >
+                    <BalanceToken
+                      address={address}
+                      amount={amount}
+                      compact={compactMode}
+                      convertedAmount={convertedAmount}
+                      decimals={decimals}
+                      symbol={symbol}
+                      verified={verified}
+                    />
+                  </li>
+                )
+              )}
+            </ul>
+          )}
+        </div>
       </div>
     </Box>
   )
