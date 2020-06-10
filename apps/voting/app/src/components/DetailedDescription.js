505e0ab1b07ef81332b0aff0730b813728ee2aaa
Brett Sun <qisheng.brett.sun@gmail.com>
2020-06-10 14:02:04 +0200

    Voting: update DetailedDescription spacing to GU and adjust rendering

--- apps/voting/app/src/components/DetailedDescription.js
+++ apps/voting/app/src/components/DetailedDescription.js
@@ -1,17 +1,14 @@
 import React, { useMemo } from 'react'
-import styled from 'styled-components'
-import { GU, Link, useTheme } from '@aragon/ui'
+import { GU, useTheme } from '@aragon/ui'
 import LocalIdentityBadge from './LocalIdentityBadge/LocalIdentityBadge'
 
 function DetailedDescription({ path }) {
-  const theme = useTheme()
   const description = useMemo(() => {
     return path
-      ? path.map((step, index) => (
-          <DescriptionStep step={step} depth={index + 1} />
-        ))
+      ? path.map((step, index) => <DescriptionStep key={index} step={step} />)
       : ''
-  }, [path, theme])
+  }, [path])
+
   return (
     <div
       css={`
@@ -28,24 +25,20 @@ function DetailedDescription({ path }) {
   )
 }
 
-function DescriptionStep({ step, depth }) {
+function DescriptionStep({ step }) {
   const theme = useTheme()
-  const app =
-    step.name || step.identifier ? (
-      <React.Fragment key={0}>
-        {step.name && step.identifier
-          ? `${step.name} (${step.identifier})`
-          : step.name || step.identifier}
-        <span
-          css={`
-            padding-right: ${1 * GU}px;
-          `}
-        >
-          :
-        </span>
-      </React.Fragment>
-    ) : (
-      <React.Fragment key={0}>
+
+  const description = []
+
+  const appName =
+    step.name && step.identifier
+      ? `${step.name} (${step.identifier})`
+      : step.name || step.identifier
+
+  // Add app
+  description.push(
+    <React.Fragment key={0}>
+      {appName || (
         <LocalIdentityBadge
           compact
           entity={step.to}
@@ -53,75 +46,64 @@ function DescriptionStep({ step, depth }) {
             padding-right: 0;
           `}
         />
-        <span
-          css={`
-            padding-right: ${1 * GU}px;
-          `}
-        >
-          :
-        </span>
-      </React.Fragment>
-    )
-
-  let description = ''
+      )}
+      <span>:</span>
+    </React.Fragment>
+  )
 
   if (step.annotatedDescription) {
-    description = step.annotatedDescription.map(({ type, value }, index) => {
-      if (type === 'address' || type === 'any-account') {
-        return (
-          <span key={index + 1}>
-            <LocalIdentityBadge
-              compact
-              entity={type === 'any-account' ? 'Any account' : value}
-            />{' '}
-          </span>
-        )
-      }
+    description.push(
+      step.annotatedDescription.map(({ type, value }, index) => {
+        // The app has already been pushed as the first element, so we increment the index by 1 for
+        // these keys
+        const key = index + 1
 
-      if (type === 'role' || type === 'kernelNamespace' || type === 'app') {
-        return <span key={index + 1}>'{value.name}' </span>
-      }
+        if (type === 'address' || type === 'any-account') {
+          return (
+            <span key={key}>
+              {' '}
+              <LocalIdentityBadge
+                compact
+                entity={type === 'any-account' ? 'Any account' : value}
+              />
+            </span>
+          )
+        }
 
-      if (type === 'apmPackage') {
-        return (
-          <span key={index + 1}>
-            <LocalIdentityBadge entity={value.name} />{' '}
-          </span>
-        )
-      }
+        if (type === 'role' || type === 'kernelNamespace' || type === 'app') {
+          return <span key={key}> “{value.name}”</span>
+        }
 
-      return <span key={index + 1}>{value.description || value} </span>
-    })
+        if (type === 'apmPackage') {
+          return <span key={key}> “{value.appName}”</span>
+        }
 
-    description.unshift(app)
+        return <span key={key}> {value.description || value}</span>
+      })
+    )
   } else {
-    description = step.description ? (
-      <React.Fragment>
-        {app}
-        {step.description}
-      </React.Fragment>
-    ) : (
-      <span>No description</span>
+    description.push(
+      <span key={description.length + 1}>
+        step.description || 'No description'
+      </span>
     )
   }
+  description.push(<br key={description.lenth + 1} />)
 
-  let childrenDescriptions = ''
-  if (step.children) {
-    childrenDescriptions = step.children.map((child, index) => (
-      <DescriptionStep step={child} depth={`${depth}${index + 1}`} />
-    ))
-  }
+  const childrenDescriptions = (step.children || []).map((child, index) => {
+    return <DescriptionStep step={child} key={index} />
+  })
 
   return (
-    <React.Fragment key={depth}>
+    <React.Fragment>
       <span>{description}</span>
-      {childrenDescriptions && (
+      {childrenDescriptions.length > 0 && (
         <ul
           css={`
             list-style-type: none;
             margin-left: 0;
-            padding-left: 6px;
-            text-indent: -6px;
+            padding-left: ${0.5 * GU}px;
+            text-indent: -${0.5 * GU}px;
           `}
         >
           <li
@@ -129,10 +111,10 @@ function DescriptionStep({ step, depth }) {
               padding-left: ${2 * GU}px;
               &:before {
                 content: '';
-                width: 6px;
-                height: 6px;
+                width: ${0.5 * GU}px;
+                height: ${0.5 * GU}px;
                 background: ${theme.accent};
-                border-radius: 6px;
+                border-radius: 50%;
                 display: inline-block;
               }
               span {
