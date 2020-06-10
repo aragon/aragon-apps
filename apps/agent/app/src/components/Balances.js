9bcd8e191d760aac5a0a00f78972823d5a48813b
EC Wireless <40322776+ECWireless@users.noreply.github.com>
2020-06-10 09:08:55 -0600

    Agent: use `formatTokenAmount()` and add rounding tooltip (#1148)

--- apps/agent/app/src/components/Balances.js
+++ apps/agent/app/src/components/Balances.js
@@ -1,108 +1,172 @@
-import React, { useState, useEffect } from 'react'
-import { Box, GU, useLayout } from '@aragon/ui'
+import React, { useEffect, useMemo, useState } from 'react'
+import BN from 'bn.js'
+import { Box, GU, textStyle, useTheme, useLayout } from '@aragon/ui'
 import BalanceToken from './BalanceToken'
-import { round } from '../lib/math-utils'
 
-const CONVERT_API_BASE = 'https://min-api.cryptocompare.com/data'
+const CONVERT_API_RETRY_DELAY = 2000
 
-const convertApiUrl = symbols =>
-  `${CONVERT_API_BASE}/price?fsym=USD&tsyms=${symbols.join(',')}`
+function convertRatesUrl(symbolsQuery) {
+  return `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${symbolsQuery}`
+}
 
-const Balances = React.memo(function Balances({ balances }) {
-  const [convertRates, setConvertRates] = useState({})
-  const [balanceItems, setBalanceItems] = useState([])
-  const { layoutName } = useLayout
-  const compactMode = layoutName === 'small'
+function useConvertRates(symbols) {
+  const [rates, setRates] = useState({})
+
+  const symbolsQuery = symbols.join(',')
 
   useEffect(() => {
     let cancelled = false
+    let retryTimer = null
 
-    // Fetches the conversion rate for the verified tokens
-    const updateConvertedRates = async balances => {
-      const verifiedSymbols = balances
-        .filter(({ verified }) => verified)
-        .map(({ symbol }) => symbol)
-
-      if (!verifiedSymbols.length) {
+    const update = async () => {
+      if (!symbolsQuery) {
+        setRates({})
         return
       }
 
-      const res = await fetch(convertApiUrl(verifiedSymbols))
-      const convertRates = await res.json()
-      if (!cancelled) {
-        setConvertRates(convertRates)
-      }
-    }
-
-    updateConvertedRates(balances)
-    return () => {
-      cancelled = true
-    }
-  }, [balances])
-
-  useEffect(() => {
-    const balanceItems = balances.map(
-      ({ address, numData: { amount, decimals }, symbol, verified }) => {
-        const adjustedAmount = amount / Math.pow(10, decimals)
-        const convertedAmount =
-          verified && convertRates[symbol]
-            ? adjustedAmount / convertRates[symbol]
-            : -1
-
-        return {
-          address,
-          symbol,
-          verified,
-          amount: round(adjustedAmount, 5),
-          convertedAmount: round(convertedAmount, 5),
+      try {
+        const response = await fetch(convertRatesUrl(symbolsQuery))
+        const rates = await response.json()
+        if (!cancelled) {
+          setRates(rates)
+        }
+      } catch (err) {
+        // The !cancelled check is needed in case:
+        //  1. The fetch() request is ongoing.
+        //  2. The component gets unmounted.
+        //  3. An error gets thrown.
+        //
+        //  Assuming the fetch() request keeps throwing, it would create new
+        //  requests even though the useEffect() got cancelled.
+        if (!cancelled) {
+          retryTimer = setTimeout(update, CONVERT_API_RETRY_DELAY)
         }
       }
-    )
-    setBalanceItems(balanceItems)
+    }
+    update()
+
+    return () => {
+      cancelled = true
+      clearTimeout(retryTimer)
+    }
+  }, [symbolsQuery])
+
+  return rates
+}
+
+// Prepare the balances for the BalanceToken component
+function useBalanceItems(balances) {
+  const verifiedSymbols = balances
+    .filter(({ verified }) => verified)
+    .map(({ symbol }) => symbol)
+
+  const convertRates = useConvertRates(verifiedSymbols)
+
+  const balanceItems = useMemo(() => {
+    return balances.map(({ address, amount, decimals, symbol, verified }) => ({
+      address,
+      amount,
+      convertedAmount: convertRates[symbol]
+        ? amount.divn(convertRates[symbol])
+        : new BN(-1),
+      decimals,
+      symbol,
+      verified,
+    }))
   }, [balances, convertRates])
 
+  return balanceItems
+}
+
+function Balances({ balances }) {
+  const theme = useTheme()
+  const { layoutName } = useLayout()
+  const balanceItems = useBalanceItems(balances)
+
+  const compact = layoutName === 'small'
+
   return (
-    <Box heading="Token Balances">
+    <Box heading="Token Balances" padding={0}>
       <div
         css={`
-          overflow-x: auto;
+          padding: ${(layoutName === 'small' ? 1 : 2) * GU}px;
         `}
       >
-        <ul
+        <div
           css={`
-            min-height: ${16.5 * GU}px;
-            list-style: none;
-            padding: 0;
-            margin: 0;
             display: flex;
-            ${compactMode && `flex-direction: column;`}
+            align-items: center;
+            min-height: ${14 * GU}px;
+            overflow-x: auto;
+            padding: ${1 * GU}px;
           `}
         >
-          {balanceItems.map(
-            ({ address, amount, convertedAmount, symbol, verified }) => (
-              <li
-                key={address}
-                css={`
-                  display: block;
-                  padding: ${3 * GU}px;
-                `}
-              >
-                <div css="display:inline-block;">
-                  <BalanceToken
-                    address={address}
-                    amount={amount}
-                    convertedAmount={convertedAmount}
-                    symbol={symbol}
-                    verified={verified}
-                  />
-                </div>
-              </li>
-            )
+          {balanceItems.length === 0 ? (
+            <div
+              css={`
+                display: flex;
+                width: 100%;
+                justify-content: center;
+                ${textStyle('body1')};
+                color: ${theme.content};
+              `}
+            >
+              No token balances yet.
+            </div>
+          ) : (
+            <ul
+              css={`
+                list-style: none;
+                display: flex;
+                ${compact
+                  ? `
+                    flex-direction: column;
+                    padding: ${1 * GU}px 0;
+                  `
+                  : ''}
+              `}
+            >
+              {balanceItems.map(
+                ({
+                  address,
+                  amount,
+                  convertedAmount,
+                  decimals,
+                  symbol,
+                  verified,
+                }) => (
+                  <li
+                    key={address}
+                    css={`
+                      flex-shrink: 0;
+                      display: block;
+                      min-width: ${20 * GU}px;
+                      padding-right: ${4 * GU}px;
+                      ${compact ? `margin-bottom: ${3 * GU}px;` : ''}
+                      &:last-of-type {
+                        min-width: unset;
+                        margin-bottom: 0;
+                      }
+                    `}
+                  >
+                    <BalanceToken
+                      address={address}
+                      amount={amount}
+                      compact={compact}
+                      convertedAmount={convertedAmount}
+                      decimals={decimals}
+                      symbol={symbol}
+                      verified={verified}
+                    />
+                  </li>
+                )
+              )}
+            </ul>
           )}
-        </ul>
+        </div>
       </div>
     </Box>
   )
-})
+}
 
 export default Balances
