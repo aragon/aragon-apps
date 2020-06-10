88d7a489a32e128d49adacbc9f321ee42275745a
EC Wireless <40322776+ECWireless@users.noreply.github.com>
2020-06-10 09:08:55 -0600

    Agent: use `formatTokenAmount()` and add rounding tooltip (#1148)

--- apps/agent/app/src/components/BalanceToken.js
+++ apps/agent/app/src/components/BalanceToken.js
@@ -1,31 +1,37 @@
 import React from 'react'
-import { textStyle, useTheme } from '@aragon/ui'
+import PropTypes from 'prop-types'
+import BN from 'bn.js'
+import { GU, Help, formatTokenAmount, textStyle, useTheme } from '@aragon/ui'
 import { useNetwork } from '@aragon/api-react'
 import { tokenIconUrl } from '../lib/icon-utils'
-import { formatTokenAmount } from '../lib/utils'
-
 function BalanceToken({
-  address = '',
+  address,
   amount,
+  compact,
+  convertedAmount,
+  decimals,
   symbol,
   verified,
-  convertedAmount = -1,
 }) {
   const theme = useTheme()
   const network = useNetwork()
+  const amountFormatted = formatTokenAmount(amount, decimals, {
+    digits: decimals,
+  })
+  const amountFormattedRounded = formatTokenAmount(amount, decimals, {
+    digits: 3,
+  })
+  const amountWasRounded = amountFormatted !== amountFormattedRounded
   return (
-    <React.Fragment>
+    <div css="display: inline-block">
       <div
         title={symbol || 'Unknown symbol'}
         css={`
           display: flex;
           align-items: center;
-          text-transform: uppercase;
-          img {
-            margin-right: 10px;
-          }
           color: ${theme.surfaceContentSecondary};
           ${textStyle('body2')}
+          text-transform: uppercase;
         `}
       >
         {verified && address && (
@@ -34,6 +40,9 @@ function BalanceToken({
             width="20"
             height="20"
             src={tokenIconUrl(address, symbol, network && network.type)}
+            css={`
+              margin-right: ${0.75 * GU}px;
+            `}
           />
         )}
         {symbol || '?'}
@@ -42,9 +51,25 @@ function BalanceToken({
         <div
           css={`
             ${textStyle('title2')}
+            margin: ${(compact ? 1 : 1.5) * GU}px 0;
+            display: flex;
           `}
         >
-          <SplitAmount amount={amount.toFixed(3)} />
+          {amountWasRounded && '~'}
+          <SplitAmount amountFormatted={amountFormattedRounded} />
+          {amountWasRounded && (
+            <div
+              css={`
+                display: flex;
+                align-items: center;
+                margin-left: ${1 * GU}px;
+              `}
+            >
+              <Help hint="This is an approximation, see the complete amount">
+                Total: {amountFormatted} {symbol}
+              </Help>
+            </div>
+          )}
         </div>
         <div
           css={`
@@ -52,23 +77,43 @@ function BalanceToken({
             ${textStyle('body2')}
           `}
         >
-          {convertedAmount >= 0
-            ? `$${formatTokenAmount(convertedAmount.toFixed(2))}`
-            : '−'}
+          {convertedAmount.isNeg()
+            ? '−'
+            : `$${formatTokenAmount(convertedAmount, decimals)}`}
         </div>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
 
-function SplitAmount({ amount }) {
-  const [integer, fractional] = formatTokenAmount(amount).split('.')
+BalanceToken.defaultProps = {
+  convertedAmount: new BN(-1),
+}
+
+BalanceToken.propTypes = {
+  address: PropTypes.string.isRequired,
+  amount: PropTypes.instanceOf(BN).isRequired,
+  compact: PropTypes.bool.isRequired,
+  convertedAmount: PropTypes.instanceOf(BN),
+  decimals: PropTypes.instanceOf(BN).isRequired,
+  symbol: PropTypes.string.isRequired,
+  verified: PropTypes.bool.isRequired,
+}
+function SplitAmount({ amountFormatted }) {
+  const [integer, fractional] = amountFormatted.split('.')
   return (
     <span>
       <span>{integer}</span>
-      {fractional && <span css="font-size: 14px;">.{fractional}</span>}
+      {fractional && (
+        <span
+          css={`
+            ${textStyle('body3')}
+          `}
+        >
+          .{fractional}
+        </span>
+      )}
     </span>
   )
 }
-
 export default BalanceToken
