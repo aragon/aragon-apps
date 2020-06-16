3e8f9b8fcaf1fce16cd02af5a896386f85ff309b
ECWireless <Elliott@valtechcreative.com>
2020-06-16 15:17:38 -0600

    Agent: Update help text

---
+++ apps/agent/app/src/components/AgentHelp.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { GU, Info, Link, useTheme } from '@aragon/ui'
+import AgentSvg from './AgentSvg'
+
+function AgentHelp() {
+  const theme = useTheme()
+
+  return (
+    <React.Fragment>
+      <Info
+        css={`
+          margin-bottom: ${2 * GU}px;
+        `}
+      >
+        <div
+          css={`
+            display: grid;
+            grid-template-rows: auto auto;
+            grid-template-columns: auto 1fr;
+            grid-column-gap: ${2 * GU}px;
+            align-items: center;
+          `}
+        >
+          <div
+            css={`
+              width: 46px;
+              height: 46px;
+            `}
+          >
+            <AgentSvg />
+          </div>
+          <div>
+            <div
+              css={`
+                margin-bottom: ${0.5 * GU}px;
+              `}
+            >
+              The Agent app can be used to interact with external contracts.
+            </div>
+            <div>
+              <Link
+                css={`
+                  color: ${theme.indicatorContent};
+                `}
+                href="https://help.aragon.org/article/37-agent">Learn More</Link>
+            </div>
+          </div>
+        </div>
+      </Info>
+    </React.Fragment>
+  )
+}
+
+export default AgentHelp
