593549a1de80344df47155d9d4610bf65ba95caf
ßingen <bingen@aragon.one>
2020-06-17 22:20:57 +0200

    Fix CI (#1172)
    * Upgrade aragonOS dependency
    
    For Vault, Agent and Finance. To fix Istanbul related issues.
    See https://github.com/aragon/aragonOS/releases/tag/v4.3.0 for more context.
    
    * payroll: fix gas values in tests
    
    * agent: fix tests
    
    `isValidSignature` was calling the `bytes` version due to overloading
    issue with Truffle.
    
    * Fix CI, address PR #1172 comments

--- apps/agent/test/agent.js
+++ apps/agent/test/agent.js
@@ -1,5 +1,5 @@
 const runSharedAgentTests = require('./agent_shared.js')
 
 contract('Agent', (accounts) => {
-  runSharedAgentTests('Agent', { accounts, artifacts, web3 })
+  runSharedAgentTests('AgentMock', { accounts, artifacts, web3 })
 })
