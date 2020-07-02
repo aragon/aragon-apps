95f428fd61148750cfd9ec1fa890d5e8386af04d
delfipolito <delfinapolito@gmail.com>
2020-07-02 11:08:47 -0300

    Make a list on disputable action status

--- apps/voting/app/src/components/VoteDetail/DisputableActionStatus.js
+++ apps/voting/app/src/components/VoteDetail/DisputableActionStatus.js
@@ -11,9 +11,16 @@ import {
 } from '@aragon/ui'
 import { format } from 'date-fns'
 import { getAgreement } from '../../agreementsMockData'
-import { hasDispute } from '../../disputable-utils'
 
 const formatDate = date => `${format(date, 'yyyy-MM-dd, HH:mm')}`
+
+function hasDispute(vote) {
+  return (
+    vote.disputable &&
+    vote.disputable.action &&
+    vote.disputable.action.challenge
+  )
+}
 
 function DisputableActionStatus({ vote }) {
   //TODO: get agreement and vote real data
@@ -24,67 +31,70 @@ function DisputableActionStatus({ vote }) {
 
   return (
     <Box heading="Disputable Action Status">
-      <Item label="Status">
-        <div>DisputableStatusTag</div>
-      </Item>
-      <Item label="Action collateral locked">
-        <div
-          css={`
-            display: flex;
-            align-items: center;
-          `}
-        >
-          {challengeAmount} {collateralToken}
-          <span
-            css={`
-              display: inline-flex;
-              padding-left: ${1 * GU}px;
-            `}
-          >
-            <IconLock size="small" />
-          </span>
-        </div>
-      </Item>
-      <Item label="Challenge period">
-        {new Date().getTime() > vote.disputable.action.endDate ? (
-          formatDate(vote.disputable.action.endDate)
-        ) : (
+      <ul>
+        <Item label="Status">
+          <div>DisputableStatusTag</div>
+        </Item>
+        <Item label="Action collateral locked">
           <div
             css={`
-              display: inline-flex;
+              display: flex;
+              align-items: center;
             `}
           >
-            <Timer end={new Date(vote.disputable.action.endDate)} />{' '}
-            <div
+            {challengeAmount} {collateralToken}
+            <span
               css={`
+                display: inline-flex;
                 padding-left: ${1 * GU}px;
-                color: ${theme.contentSecondary};
               `}
             >
-              (48h)
-            </div>
+              <IconLock size="small" />
+            </span>
           </div>
-        )}
-      </Item>
-      <Item label="Agreement">
-        <Link>{agreement.agreementTitle}</Link>
-      </Item>
-      {hasDispute(vote) && (
-        <Item label="Dispute">
-          <Link
-            href={`https://court.aragon.org/disputes/${vote.disputable.action.challenge.disputeId}`}
-          >
-            Dispute #{vote.disputable.action.currentChallengeId}
-          </Link>
         </Item>
-      )}
-      <Item>
-        <Info>
-          Exceeding reaction chamber thermal limit. We have begun power-supply
-          calibration. Force fields have been established on all turbo lifts and
-          crawlways. Warp drive within normal parameters. I read an ion trail.
-        </Info>
-      </Item>
+        <Item label="Challenge period">
+          {new Date().getTime() > vote.disputable.action.endDate ? (
+            formatDate(vote.disputable.action.endDate)
+          ) : (
+            <div
+              css={`
+                display: inline-flex;
+              `}
+            >
+              <Timer end={new Date(vote.disputable.action.endDate)} />{' '}
+              <div
+                css={`
+                  padding-left: ${1 * GU}px;
+                  color: ${theme.contentSecondary};
+                `}
+              >
+                (48h)
+              </div>
+            </div>
+          )}
+        </Item>
+        <Item label="Agreement">
+          <Link>{agreement.agreementTitle}</Link>
+        </Item>
+        {hasDispute(vote) && (
+          <Item label="Dispute">
+            <Link
+              href={`https://court.aragon.org/disputes/${vote.disputable.action.challenge.disputeId}`}
+            >
+              Dispute #{vote.disputable.action.currentChallengeId}
+            </Link>
+          </Item>
+        )}
+        <Item>
+          <Info>
+            Exceeding reaction chamber thermal limit. We have begun power-supply
+            calibration. Force fields have been established on all turbo lifts
+            and crawlways. Warp drive within normal parameters. I read an ion
+            trail.
+          </Info>
+        </Item>
+      </ul>
     </Box>
   )
 }
@@ -92,9 +102,10 @@ function DisputableActionStatus({ vote }) {
 function Item({ label, children }) {
   const theme = useTheme()
   return (
-    <div
+    <li
       css={`
         margin-bottom: ${3 * GU}px;
+        list-style-type: none;
       `}
     >
       {label && (
@@ -110,7 +121,7 @@ function Item({ label, children }) {
         </label>
       )}
       {children}
-    </div>
+    </li>
   )
 }
 
--- apps/voting/app/src/disputable-utils.js
+++
@@ -1,7 +0,0 @@
-export function hasDispute(vote) {
-  return (
-    vote.disputable &&
-    vote.disputable.action &&
-    vote.disputable.action.challenge
-  )
-}
