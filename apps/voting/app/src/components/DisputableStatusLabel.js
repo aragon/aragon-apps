5ead60772e859e8aa14040d5b56acbee70349189
delfipolito <delfinapolito@gmail.com>
2020-07-03 09:34:04 -0300

    Fix on status filter

--- apps/voting/app/src/components/DisputableStatusLabel.js
+++ apps/voting/app/src/components/DisputableStatusLabel.js
@@ -10,55 +10,51 @@ import {
   textStyle,
   useTheme,
 } from '@aragon/ui'
-import { DISPUTABLE_VOTE_STATUSES } from '../disputable-vote-statuses'
+import {
+  VOTE_STATUS_ACTIVE,
+  VOTE_STATUS_CANCELLED,
+  VOTE_STATUS_CLOSED,
+  VOTE_STATUS_PAUSED,
+  DISPUTABLE_VOTE_STATUSES,
+} from '../disputable-vote-statuses'
+
+function getAttributes(status, theme) {
+  const attributes = {
+    [VOTE_STATUS_ACTIVE]: {
+      label: 'Scheduled',
+      Icon: IconClock,
+    },
+    [VOTE_STATUS_CANCELLED]: {
+      background: String(theme.surfaceUnder),
+      label: 'Cancelled',
+      Icon: IconClosed,
+      color: String(theme.disabledContent),
+    },
+    [VOTE_STATUS_CLOSED]: {
+      background: String(theme.surfaceUnder),
+      label: 'Closed',
+      Icon: IconInfo,
+      color: String(theme.disabledContent),
+    },
+    [VOTE_STATUS_PAUSED]: {
+      background: String(theme.warningSurface),
+      label: 'Challenged',
+      Icon: IconAttention,
+      color: String(theme.warningSurfaceContent),
+    },
+  }
+
+  return attributes[status]
+}
 
 function DisputableStatusLabel({ status }) {
   const theme = useTheme()
-  const statusAttributes = new Map([
-    [
-      DISPUTABLE_VOTE_STATUSES.get('Active'),
-      {
-        background: null,
-        label: 'Scheduled',
-        Icon: IconClock,
-        color: null,
-      },
-    ],
-    [
-      DISPUTABLE_VOTE_STATUSES.get('Cancelled'),
-      {
-        background: String(theme.surfaceUnder),
-        label: 'Cancelled',
-        Icon: IconClosed,
-        color: String(theme.disabledContent),
-      },
-    ],
-    [
-      DISPUTABLE_VOTE_STATUSES.get('Closed'),
-      {
-        background: String(theme.surfaceUnder),
-        label: 'Closed',
-        Icon: IconInfo,
-        color: String(theme.disabledContent),
-      },
-    ],
-    [
-      DISPUTABLE_VOTE_STATUSES.get('Paused'),
-      {
-        background: String(theme.warningSurface),
-        label: 'Challenged',
-        Icon: IconAttention,
-        color: String(theme.warningSurfaceContent),
-      },
-    ],
-  ])
-
-  const { Icon, background, color, label } = statusAttributes.get(status)
+  const { Icon, background, color, label } = getAttributes(status, theme)
 
   return (
     <Tag
-      background={background}
-      color={color}
+      background={background && `${background}`}
+      color={color && `${color}`}
       mode="indicator"
       label={label}
       icon={<Icon size="small" />}
@@ -68,10 +64,10 @@ function DisputableStatusLabel({ status }) {
 
 DisputableStatusLabel.propTypes = {
   status: PropTypes.oneOf([
-    DISPUTABLE_VOTE_STATUSES.get('Active'),
-    DISPUTABLE_VOTE_STATUSES.get('Cancelled'),
-    DISPUTABLE_VOTE_STATUSES.get('Closed'),
-    DISPUTABLE_VOTE_STATUSES.get('Paused'),
+    VOTE_STATUS_ACTIVE,
+    VOTE_STATUS_CANCELLED,
+    VOTE_STATUS_CLOSED,
+    VOTE_STATUS_PAUSED,
   ]),
 }
 
