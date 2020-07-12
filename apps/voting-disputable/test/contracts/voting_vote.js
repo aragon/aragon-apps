c9054d1216e9f067b9119082470d202bdf6f5911
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-07-12 17:01:37 -0300

    DisputableVoting: split create and vote tests

--- apps/voting-disputable/test/contracts/voting_vote.js
+++ apps/voting-disputable/test/contracts/voting_vote.js
@@ -1,18 +1,15 @@
 const VOTER_STATE = require('../helpers/state')
-const getBlockNumber = require('@aragon/contract-test-helpers/blockNumber')(web3)
 
 const { DAY } = require('@aragon/apps-agreement/test/helpers/lib/time')
 const { assertBn } = require('@aragon/apps-agreement/test/helpers/assert/assertBn')
 const { bn, bigExp } = require('@aragon/apps-agreement/test/helpers/lib/numbers')
 const { assertRevert } = require('@aragon/apps-agreement/test/helpers/assert/assertThrow')
-const { getEventArgument } = require('@aragon/contract-test-helpers/events')
-const { ARAGON_OS_ERRORS, VOTING_ERRORS } = require('../helpers/errors')
-const { assertEvent, assertAmountOfEvents } = require('@aragon/apps-agreement/test/helpers/assert/assertEvent')
+const { VOTING_ERRORS } = require('../helpers/errors')
 const { pct, createVote, voteScript, getVoteState } = require('../helpers/voting')(web3, artifacts)
 
 const deployer = require('../helpers/deployer')(web3, artifacts)
 
-contract('Voting', ([_, owner, holder1, holder2, holder20, holder29, holder51, nonHolder]) => {
+contract('Voting', ([_, owner, holder20, holder29, holder51, nonHolder]) => {
   let voting, token
 
   const CONTEXT = '0xabcdef'
@@ -22,228 +19,67 @@ contract('Voting', ([_, owner, holder1, holder2, holder20, holder29, holder51, n
   const REQUIRED_SUPPORT = pct(50)
   const MINIMUM_ACCEPTANCE_QUORUM = pct(20)
 
-  beforeEach('deploy voting', async () => {
-    voting = await deployer.deploy({ owner })
+  beforeEach('deploy and mint tokens', async () => {
+    token = await deployer.deployToken({})
+    await token.generateTokens(holder51, bigExp(51, 18))
+    await token.generateTokens(holder29, bigExp(29, 18))
+    await token.generateTokens(holder20, bigExp(20, 18))
   })
 
-  describe('newVote', () => {
-    it('it is a forwarder', async () => {
-      assert.isTrue(await voting.isForwarder())
+  beforeEach('deploy voting', async () => {
+    voting = await deployer.deployAndInitialize({ owner, minimumAcceptanceQuorum: MINIMUM_ACCEPTANCE_QUORUM, requiredSupport: REQUIRED_SUPPORT, voteDuration: VOTE_DURATION, overruleWindow: OVERRULE_WINDOW, executionDelay: EXECUTION_DELAY })
+  })
+
+  describe('vote', () => {
+    let voteId
+
+    beforeEach('create vote', async () => {
+      ({ voteId } = await createVote({ voting, voteContext: CONTEXT, from: holder51 }))
     })
 
-    context('when the app was not initialized', async () => {
-      it('fails creating a vote', async () => {
-        await assertRevert(createVote({ voting, script: false }), ARAGON_OS_ERRORS.APP_AUTH_FAILED)
-      })
+    it('holder can vote', async () => {
+      await voting.vote(voteId, false, { from: holder29 })
+      const { nays } = await getVoteState(voting, voteId)
+      const voterState = await voting.getVoterState(voteId, holder29)
 
-      it('fails to forward actions', async () => {
-        const { script } = await voteScript()
-        await assertRevert(voting.forward(script, { from: holder51 }), VOTING_ERRORS.VOTING_CANNOT_FORWARD)
-      })
+      assertBn(nays, bigExp(29, 18), 'nay vote should have been counted')
+      assert.equal(voterState, VOTER_STATE.NAY, 'holder29 should have nay voter status')
     })
 
-    context('when the app was initialized', () => {
-      beforeEach('initialize voting', async () => {
-        token = await deployer.deployToken({})
-        voting.initialize(token.address, REQUIRED_SUPPORT, MINIMUM_ACCEPTANCE_QUORUM, VOTE_DURATION, OVERRULE_WINDOW, EXECUTION_DELAY)
-      })
+    it('holder can not modify vote', async () => {
+      await voting.vote(voteId, true, { from: holder29 })
+      const firstTime = await getVoteState(voting, voteId)
+      assertBn(firstTime.nays, 0, 'nay vote should have been removed')
+      assertBn(firstTime.yeas, bigExp(29, 18), 'yea vote should have been counted')
 
-      context('when there is some supply', () => {
-        let voteId
+      await assertRevert(voting.vote(voteId, false, { from: holder29 }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
+    })
 
-        context('with normal token supply', () => {
-          let script, executionTarget
+    it('token transfers dont affect voting', async () => {
+      await token.transfer(nonHolder, bigExp(29, 18), { from: holder29 })
 
-          beforeEach('mint tokens', async () => {
-            await deployer.token.generateTokens(holder51, bigExp(51, 18))
-            await deployer.token.generateTokens(holder29, bigExp(29, 18))
-            await deployer.token.generateTokens(holder20, bigExp(20, 18))
-          })
+      await voting.vote(voteId, true, { from: holder29 })
+      const { yeas } = await getVoteState(voting, voteId)
 
-          beforeEach('build script', async () => {
-            ({ script, executionTarget } = await voteScript())
-          })
+      assertBn(yeas, bigExp(29, 18), 'yea vote should have been counted')
+      assert.equal(await token.balanceOf(holder29), 0, 'balance should be 0 at current block')
+    })
 
-          beforeEach('create vote', async () => {
-            ({ voteId, receipt } = await createVote({ voting, script, voteContext: CONTEXT, from: holder51 }))
-          })
+    it('throws when non-holder votes', async () => {
+      await assertRevert(voting.vote(voteId, true, { from: nonHolder }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
+    })
 
-          it('can be forwarded', async () => {
-            const receipt = await voting.forward(script, { from: holder51 })
-            voteId = getEventArgument(receipt, 'StartVote', 'voteId')
+    it('throws when voting after voting closes', async () => {
+      await voting.mockIncreaseTime(VOTE_DURATION)
+      await assertRevert(voting.vote(voteId, true, { from: holder29 }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
+    })
 
-            assertBn(voteId, 1, 'voting should have been created')
-          })
+    it('cannot vote on executed vote', async () => {
+      await voting.vote(voteId, true, { from: holder51 })
+      await voting.mockIncreaseTime(VOTE_DURATION)
+      await voting.executeVote(voteId)
 
-          it('emits an event', async () => {
-            assertAmountOfEvents(receipt, 'StartVote')
-            assertEvent(receipt, 'StartVote', { voteId, creator: holder51, context: CONTEXT })
-          })
-
-          it('has correct state', async () => {
-            const { isOpen, isExecuted, snapshotBlock, support, quorum, overruleWindow, yeas, nays, votingPower, script: execScript } = await getVoteState(voting, voteId)
-
-            assert.isTrue(isOpen, 'vote should be open')
-            assert.isFalse(isExecuted, 'vote should not be executed')
-            assertBn(snapshotBlock, await getBlockNumber() - 1, 'snapshot block should be correct')
-            assertBn(support, REQUIRED_SUPPORT, 'required support should be app required support')
-            assertBn(quorum, MINIMUM_ACCEPTANCE_QUORUM, 'min quorum should be app min quorum')
-            assertBn(overruleWindow, OVERRULE_WINDOW, 'default overrule window should be correct')
-            assertBn(yeas, 0, 'initial yea should be 0')
-            assertBn(nays, 0, 'initial nay should be 0')
-            assertBn(votingPower, bigExp(100, 18), 'voting power should be 100')
-            assert.equal(execScript, script, 'script should be correct')
-            assertBn(await voting.getVoterState(voteId, nonHolder), VOTER_STATE.ABSENT, 'nonHolder should not have voted')
-          })
-
-          it('fails getting a vote out of bounds', async () => {
-            await assertRevert(voting.getVote(voteId + 1), VOTING_ERRORS.VOTING_NO_VOTE)
-          })
-
-          it('holder can vote', async () => {
-            await voting.vote(voteId, false, { from: holder29 })
-            const { nays } = await getVoteState(voting, voteId)
-            const voterState = await voting.getVoterState(voteId, holder29)
-
-            assertBn(nays, bigExp(29, 18), 'nay vote should have been counted')
-            assert.equal(voterState, VOTER_STATE.NAY, 'holder29 should have nay voter status')
-          })
-
-          it('holder can not modify vote', async () => {
-            await voting.vote(voteId, true, { from: holder29 })
-            const firstTime = await getVoteState(voting, voteId)
-            assertBn(firstTime.nays, 0, 'nay vote should have been removed')
-            assertBn(firstTime.yeas, bigExp(29, 18), 'yea vote should have been counted')
-
-            await assertRevert(voting.vote(voteId, false, { from: holder29 }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
-          })
-
-          it('token transfers dont affect voting', async () => {
-            await token.transfer(nonHolder, bigExp(29, 18), { from: holder29 })
-
-            await voting.vote(voteId, true, { from: holder29 })
-            const { yeas } = await getVoteState(voting, voteId)
-
-            assertBn(yeas, bigExp(29, 18), 'yea vote should have been counted')
-            assert.equal(await token.balanceOf(holder29), 0, 'balance should be 0 at current block')
-          })
-
-          it('throws when non-holder votes', async () => {
-            await assertRevert(voting.vote(voteId, true, { from: nonHolder }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
-          })
-
-          it('throws when voting after voting closes', async () => {
-            await voting.mockIncreaseTime(VOTE_DURATION)
-            await assertRevert(voting.vote(voteId, true, { from: holder29 }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
-          })
-
-          it('cannot vote on executed vote', async () => {
-            await voting.vote(voteId, true, { from: holder51 })
-            await voting.mockIncreaseTime(VOTE_DURATION)
-            await voting.executeVote(voteId)
-
-            await assertRevert(voting.vote(voteId, true, { from: holder20 }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
-          })
-        })
-
-        context('token supply = 1', () => {
-          beforeEach('mint tokens', async () => {
-            await token.generateTokens(holder1, 1)
-          })
-
-          it('new vote cannot be executed before voting', async () => {
-            // Account creating vote does not have any tokens and therefore doesn't vote
-            ({ voteId } = await createVote({ voting, script: false }))
-
-            assert.isFalse(await voting.canExecute(voteId), 'vote cannot be executed')
-
-            await voting.vote(voteId, true, { from: holder1 })
-            await voting.mockIncreaseTime(VOTE_DURATION)
-            await voting.executeVote(voteId)
-
-            const { isOpen, isExecuted } = await getVoteState(voting, voteId)
-
-            assert.isFalse(isOpen, 'vote should be closed')
-            assert.isTrue(isExecuted, 'vote should have been executed')
-          })
-
-          it('creates a vote without executing', async () => {
-            ({ voteId } = await createVote({ voting, script: false, from: holder1 }))
-
-            const { isOpen, isExecuted } = await getVoteState(voting, voteId)
-            assert.isTrue(isOpen, 'vote should be open')
-            assert.isFalse(isExecuted, 'vote should not have been executed')
-          })
-        })
-
-        context('token supply = 3', () => {
-          beforeEach('mint tokens', async () => {
-            await token.generateTokens(holder1, 1)
-            await token.generateTokens(holder2, 2)
-          })
-
-          it('new vote cannot be executed even after holder2 voting', async () => {
-            ({ voteId } = await createVote({ voting, script: false }))
-
-            await voting.vote(voteId, true, { from: holder1 })
-            await voting.vote(voteId, true, { from: holder2 })
-
-            const { isOpen, isExecuted } = await getVoteState(voting, voteId)
-            assert.isTrue(isOpen, 'vote should still be open')
-            assert.isFalse(isExecuted, 'vote should have not been executed')
-            assert.isFalse(await voting.canExecute(voteId), 'vote cannot be executed')
-          })
-
-          it('creating vote as holder2 does not execute vote', async () => {
-            ({ voteId } = await createVote({ voting, script: false, from: holder2 }))
-
-            const { isOpen, isExecuted } = await getVoteState(voting, voteId)
-            assert.isTrue(isOpen, 'vote should still be open')
-            assert.isFalse(isExecuted, 'vote should have not been executed')
-            assert.isFalse(await voting.canExecute(voteId), 'vote cannot be executed')
-          })
-        })
-
-        context('with changing token supply', () => {
-          beforeEach('mint tokens', async () => {
-            await token.generateTokens(holder1, 1)
-            await token.generateTokens(holder2, 1)
-          })
-
-          it('uses the correct snapshot value if tokens are minted afterwards', async () => {
-            // Create vote and afterwards generate some tokens
-            ({ voteId } = await createVote({ voting, script: false }))
-            await token.generateTokens(holder2, 1)
-
-            const { snapshotBlock, votingPower } = await getVoteState(voting, voteId)
-
-            // Generating tokens advanced the block by one
-            assertBn(snapshotBlock, await getBlockNumber() - 2, 'snapshot block should be correct')
-            assertBn(votingPower, await token.totalSupplyAt(snapshotBlock), 'voting power should match snapshot supply')
-            assertBn(votingPower, 2, 'voting power should be correct')
-          })
-
-          it('uses the correct snapshot value if tokens are minted in the same block', async () => {
-            // Create vote and generate some tokens in the same transaction
-            // Requires the voting mock to be the token's owner
-            await token.changeController(voting.address)
-            const receipt = await voting.newTokenAndVote(holder2, 1, CONTEXT)
-            voteId = getEventArgument(receipt, 'StartVote', 'voteId')
-
-            const { snapshotBlock, votingPower } = await getVoteState(voting, voteId)
-
-            assertBn(snapshotBlock, await getBlockNumber() - 1, 'snapshot block should be correct')
-            assertBn(votingPower, await token.totalSupplyAt(snapshotBlock), 'voting power should match snapshot supply')
-            assertBn(votingPower, 2, 'voting power should be correct')
-          })
-        })
-      })
-
-      context('when there is no supply', () => {
-        it('reverts', async () => {
-          await assertRevert(createVote({ voting, script: false }), VOTING_ERRORS.VOTING_NO_VOTING_POWER)
-        })
-      })
+      await assertRevert(voting.vote(voteId, true, { from: holder20 }), VOTING_ERRORS.VOTING_CANNOT_VOTE)
     })
   })
 
