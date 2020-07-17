4c05d72461263831cc8c10a29aa4be663916740c
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-07-17 10:25:30 -0300

    Vault: rollback changed test
    Co-authored-by: Brett Sun <qisheng.brett.sun@gmail.com>

--- apps/vault/test/vault_shared.js
+++ apps/vault/test/vault_shared.js
@@ -296,7 +296,7 @@ module.exports = (
 
         // MIGRATION: update to aOS 5.x, `transferToVault` is using `.transfer`, not enough gas being forwarded
         it.skip('set up the default vault correctly to recover ETH from the kernel', async () => {
-          await kernel.sendTransaction({ value: 1, gas: 310000 })
+          await kernel.sendTransaction({ value: 1, gas: 31000 })
           assertBn(await web3.eth.getBalance(kernel.address), 1, 'kernel should have 1 balance')
 
           await kernel.transferToVault(ETH)
