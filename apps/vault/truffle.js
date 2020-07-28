e8ce45c8b9cf2acdd27359ed6dd78114c81b6dc2
ßingen <bingen@aragon.one>
2020-07-28 10:08:41 +0200

    agreements: Rollback payable actions (#1232)
    * agreements: Rollback payable actions
    
    * agreements: Upgrade aragonOS dependency
    
    * agent: Upgrade to solidity-coverage 0.7
    
    * Upgrade to solidity-coverage 0.7
    
    * agreements: Remove unused stuff
    
    Address PR #1232 comments.

--- apps/vault/truffle.js
+++ apps/vault/truffle.js
@@ -1,5 +1,6 @@
 const TruffleConfig = require('@aragon/truffle-config-v5')
 
 TruffleConfig.compilers.solc.version = '0.4.24'
+TruffleConfig.plugins = ["solidity-coverage"]
 
 module.exports = TruffleConfig
