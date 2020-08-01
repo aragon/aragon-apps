647db647d89a8271a36c7b78af93d1ff8b4dd11d
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-01 09:23:33 -0300

    DisputableVoting: Polish roles tests

--- apps/voting-disputable/test/contracts/voting_roles.js
+++ apps/voting-disputable/test/contracts/voting_roles.js
@@ -1,38 +1,32 @@
-const { keccak_256 } = require('js-sha3')
+const { sha3 } = require('web3-utils')
 const deployer = require('../helpers/deployer')(web3, artifacts)
 
 contract('Voting', ([_, owner]) => {
+  let voting
+
   before('deploy voting', async () => {
-    await deployer.deploy({ owner })
+    voting = await deployer.deploy({ owner })
   })
 
   describe('roles', () => {
-    const COMPUTED_CREATE_VOTES_ROLE = '0x' + keccak_256("CREATE_VOTES_ROLE");
-    const COMPUTED_MODIFY_SUPPORT_ROLE = '0x' + keccak_256("MODIFY_SUPPORT_ROLE");
-    const COMPUTED_MODIFY_QUORUM_ROLE = '0x' + keccak_256("MODIFY_QUORUM_ROLE");
-    const COMPUTED_MODIFY_OVERRULE_WINDOW_ROLE = '0x' + keccak_256("MODIFY_OVERRULE_WINDOW_ROLE");
-    const COMPUTED_MODIFY_EXECUTION_DELAY_ROLE = '0x' + keccak_256("MODIFY_EXECUTION_DELAY_ROLE");
-    const COMPUTED_MODIFY_QUIET_ENDING_CONFIGURATION = '0x' + keccak_256("MODIFY_QUIET_ENDING_CONFIGURATION");
+    it('computes roles properly', async () => {
+      const EXPECTED_CREATE_VOTES_ROLE = sha3('CREATE_VOTES_ROLE')
+      assert.equal(await voting.CREATE_VOTES_ROLE(), EXPECTED_CREATE_VOTES_ROLE, 'CREATE_VOTES_ROLE doesn’t match')
 
-    let CREATE_VOTES_ROLE, MODIFY_SUPPORT_ROLE, MODIFY_QUORUM_ROLE,
-        MODIFY_OVERRULE_WINDOW_ROLE, MODIFY_EXECUTION_DELAY_ROLE, MODIFY_QUIET_ENDING_CONFIGURATION
+      const EXPECTED_MODIFY_SUPPORT_ROLE = sha3('MODIFY_SUPPORT_ROLE')
+      assert.equal(await voting.MODIFY_SUPPORT_ROLE(), EXPECTED_MODIFY_SUPPORT_ROLE, 'MODIFY_SUPPORT_ROLE doesn’t match')
 
-    before('load role', async () => {
-      CREATE_VOTES_ROLE = await deployer.base.CREATE_VOTES_ROLE()
-      MODIFY_SUPPORT_ROLE = await deployer.base.MODIFY_SUPPORT_ROLE()
-      MODIFY_QUORUM_ROLE = await deployer.base.MODIFY_QUORUM_ROLE()
-      MODIFY_OVERRULE_WINDOW_ROLE = await deployer.base.MODIFY_OVERRULE_WINDOW_ROLE()
-      MODIFY_EXECUTION_DELAY_ROLE = await deployer.base.MODIFY_EXECUTION_DELAY_ROLE()
-      MODIFY_QUIET_ENDING_CONFIGURATION = await deployer.base.MODIFY_QUIET_ENDING_CONFIGURATION()
-    })
+      const EXPECTED_MODIFY_QUORUM_ROLE = sha3('MODIFY_QUORUM_ROLE')
+      assert.equal(await voting.MODIFY_QUORUM_ROLE(), EXPECTED_MODIFY_QUORUM_ROLE, 'MODIFY_QUORUM_ROLE doesn’t match')
 
-    it('roles match', async () => {
-      assert.equal(CREATE_VOTES_ROLE, COMPUTED_CREATE_VOTES_ROLE, 'CREATE_VOTES_ROLE doesn’t match')
-      assert.equal(MODIFY_SUPPORT_ROLE, COMPUTED_MODIFY_SUPPORT_ROLE, 'MODIFY_SUPPORT_ROLE doesn’t match')
-      assert.equal(MODIFY_QUORUM_ROLE, COMPUTED_MODIFY_QUORUM_ROLE, 'MODIFY_QUORUM_ROLE doesn’t match')
-      assert.equal(MODIFY_OVERRULE_WINDOW_ROLE, COMPUTED_MODIFY_OVERRULE_WINDOW_ROLE, 'MODIFY_OVERRULE_WINDOW_ROLE doesn’t match')
-      assert.equal(MODIFY_EXECUTION_DELAY_ROLE, COMPUTED_MODIFY_EXECUTION_DELAY_ROLE, 'MODIFY_EXECUTION_DELAY_ROLE doesn’t match')
-      assert.equal(MODIFY_QUIET_ENDING_CONFIGURATION, COMPUTED_MODIFY_QUIET_ENDING_CONFIGURATION, 'MODIFY_QUIET_ENDING_CONFIGURATION doesn’t match')
+      const EXPECTED_MODIFY_OVERRULE_WINDOW_ROLE = sha3('MODIFY_OVERRULE_WINDOW_ROLE')
+      assert.equal(await voting.MODIFY_OVERRULE_WINDOW_ROLE(), EXPECTED_MODIFY_OVERRULE_WINDOW_ROLE, 'MODIFY_OVERRULE_WINDOW_ROLE doesn’t match')
+
+      const EXPECTED_MODIFY_EXECUTION_DELAY_ROLE = sha3('MODIFY_EXECUTION_DELAY_ROLE')
+      assert.equal(await voting.MODIFY_EXECUTION_DELAY_ROLE(), EXPECTED_MODIFY_EXECUTION_DELAY_ROLE, 'MODIFY_EXECUTION_DELAY_ROLE doesn’t match')
+
+      const EXPECTED_MODIFY_QUIET_ENDING_CONFIGURATION = sha3('MODIFY_QUIET_ENDING_CONFIGURATION')
+      assert.equal(await voting.MODIFY_QUIET_ENDING_CONFIGURATION(), EXPECTED_MODIFY_QUIET_ENDING_CONFIGURATION, 'MODIFY_QUIET_ENDING_CONFIGURATION doesn’t match')
     })
   })
 })
