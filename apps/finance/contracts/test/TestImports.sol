3e31401c8cab89327982a9fe1b9a18252cfc6059
0xGabi <gabrielpk.18@gmail.com>
2020-08-06 19:47:45 -0300

    Finance: move to buidler

--- apps/finance/contracts/test/TestImports.sol
+++ apps/finance/contracts/test/TestImports.sol
@@ -1,11 +1,14 @@
 pragma solidity 0.4.24;
 
 import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/apm/APMRegistry.sol";
+import "@aragon/os/contracts/factory/APMRegistryFactory.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
+import "@aragon/os/contracts/factory/ENSFactory.sol";
 import "@aragon/os/contracts/factory/DAOFactory.sol";
+import "@aragon/os/contracts/lib/ens/ENS.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 
-import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
 import "@aragon/contract-helpers-test/contracts/0.4/misc/ExecutionTarget.sol";
 import "@aragon/contract-helpers-test/contracts/0.4/token/TokenMock.sol";
 import "@aragon/contract-helpers-test/contracts/0.4/token/TokenReturnFalseMock.sol";
