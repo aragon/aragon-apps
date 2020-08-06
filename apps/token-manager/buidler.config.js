e1874a9fdb59b27e8625c6e0e12babd84515e37e
0xGabi <gabrielpk.18@gmail.com>
2020-08-06 19:48:05 -0300

    Tokens: move to buidler

---
+++ apps/token-manager/buidler.config.js
@@ -0,0 +1,42 @@
+const { usePlugin } = require('@nomiclabs/buidler/config')
+const hooks = require('./scripts/buidler-hooks')
+
+usePlugin('@aragon/buidler-aragon')
+usePlugin("@nomiclabs/buidler-etherscan");
+usePlugin('buidler-gas-reporter')
+usePlugin('solidity-coverage')
+
+module.exports = {
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545'
+    },
+    coverage: {
+      url: 'http://localhost:8555',
+    },
+  },
+  solc: {
+    version: '0.4.24',
+    optimizer: {
+      enabled: true,
+      runs: 10000,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORTER ? true : false,
+  },
+  etherscan: {
+    // The url for the Etherscan API.
+    url: "https://api.etherscan.io/api",
+    // API key for Etherscan
+    apiKey: process.env.ETHERSCAN_KEY
+  },
+  aragon: {
+    appServePort: 3003,
+    clientServePort: 3000,
+    appSrcPath: 'app/',
+    appBuildOutputPath: 'dist/',
+    hooks,
+  },
+}
