ee2d7eeb55cb9c8332144ea4f0aa7e8014eb082f
0xGabi <gabrielpk.18@gmail.com>
2020-08-06 19:47:18 -0300

    Agreements: move to buidler

--- apps/agreement/contracts/test/TestImports.sol
+++ apps/agreement/contracts/test/TestImports.sol
@@ -3,8 +3,8 @@ pragma solidity 0.4.24;
 import "@aragon/os/contracts/acl/ACL.sol";
 import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
+
 import "@aragon/minime/contracts/MiniMeToken.sol";
-import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
 import "@aragon/contract-helpers-test/contracts/0.4/misc/ExecutionTarget.sol";
 
 
