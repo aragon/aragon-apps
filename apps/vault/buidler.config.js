20b37d6a8f823591ca7b65397f81c73a6ab595fa
0xGabi <gabrielpk.18@gmail.com>
2020-08-11 14:18:37 -0300

    Update scripts and use buidlerevm as default network

--- apps/vault/buidler.config.js
+++ apps/vault/buidler.config.js
@@ -1,15 +1,11 @@
 const { usePlugin } = require('@nomiclabs/buidler/config')
 
-usePlugin("@nomiclabs/buidler-truffle5")
+usePlugin('@nomiclabs/buidler-truffle5')
 usePlugin('buidler-gas-reporter')
 usePlugin('solidity-coverage')
 
 module.exports = {
-  defaultNetwork: 'localhost',
   networks: {
-    localhost: {
-      url: 'http://localhost:8545'
-    },
     coverage: {
       url: 'http://localhost:8555',
     },
@@ -22,6 +18,6 @@ module.exports = {
     },
   },
   gasReporter: {
-    enabled: process.env.GAS_REPORTER ? true : false,
-  }
+    enabled: process.env.REPORT_GAS ? true : false,
+  },
 }
