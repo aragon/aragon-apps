11146312c843878283e9168efafcb4c3f6b85e75
0xGabi <gabrielpk.18@gmail.com>
2020-08-12 14:32:08 -0300

    Chore: Add network configurations

--- apps/agreement/buidler.config.js
+++ apps/agreement/buidler.config.js
@@ -9,6 +9,24 @@ module.exports = {
     coverage: {
       url: 'http://localhost:8555',
     },
+    mainnet: {
+      url: 'https://mainnet.eth.aragon.network',
+      accounts: [
+        process.env.ETH_KEY ||
+          '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
+      ],
+    },
+    rinkeby: {
+      url: 'https://rinkeby.eth.aragon.network',
+      accounts: [
+        process.env.ETH_KEY ||
+          '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
+      ],
+    },
+    frame: {
+      httpHeaders: { origin: 'buidler' },
+      url: 'http://localhost:1248',
+    },
   },
   solc: {
     version: '0.4.24',
@@ -17,6 +35,11 @@ module.exports = {
       runs: 1000,
     },
   },
+  // The gas reporter plugin do not properly handle the buidlerevm
+  // chain yet. In the mean time we should 'npx buidler node' and
+  // then attach to running process using '--network localhost' as
+  // explained in: https://buidler.dev/buidler-evm/#connecting-to-buidler-evm-from-wallets-and-other-software.
+  // You can also ran 'yarn devchain' and on a separate window 'yarn test:gas'
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
   },
