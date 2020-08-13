c0bee8ce5eff39eec3b7e778edb124b616d0601f
0xGabi <gabrielpk.18@gmail.com>
2020-08-13 12:19:00 -0300

    Chore: Network configurations
    Add ganache as default network and include a few comments on each network

--- apps/vault/buidler.config.js
+++ apps/vault/buidler.config.js
@@ -6,9 +6,23 @@ usePlugin('solidity-coverage')
 
 module.exports = {
   networks: {
+    defaultNetwork: "ganache",
+    // Local development network using ganache. You can set any of the 
+    // Ganache's options. All of them are supported, with the exception
+    // of accounts.
+    // https://github.com/trufflesuite/ganache-core#options
+    ganache: {
+      url: 'http://localhost:8545',
+      gasLimit: 6000000000,
+      defaultBalanceEther: 100
+    },
+    // Local development network to test coverage. Solidity coverage 
+    // pluging launches its own in-process ganache server.
+    // and expose it at port 8555.
     coverage: {
       url: 'http://localhost:8555',
     },
+    // Mainnet network configured with Aragon node.
     mainnet: {
       url: 'https://mainnet.eth.aragon.network',
       accounts: [
@@ -16,6 +30,7 @@ module.exports = {
           '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
       ],
     },
+    // Rinkeby network configured with Aragon node.
     rinkeby: {
       url: 'https://rinkeby.eth.aragon.network',
       accounts: [
@@ -23,14 +38,12 @@ module.exports = {
           '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
       ],
     },
+    // Network configured to interact with Frame wallet. Requires 
+    // to have Frame running on your machine. Download it from:
+    // https://frame.sh
     frame: {
       httpHeaders: { origin: 'buidler' },
       url: 'http://localhost:1248',
-    },
-    ganache: {
-      url: 'http://localhost:8545',
-      gasLimit: 6000000000,
-      defaultBalanceEther: 100
     }
   },
   solc: {
