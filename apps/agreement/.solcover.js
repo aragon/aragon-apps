32e4e2849e189b9f6a4966f497db2b94242625f5
0xGabi <gabrielpk.18@gmail.com>
2020-08-13 15:27:31 -0300

    Test: Skip tests with gas diffrence on coverage

--- apps/agreement/.solcover.js
+++ apps/agreement/.solcover.js
@@ -1,3 +1,7 @@
 module.exports = {
   skipFiles: ['test', '@aragon/os'],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true               // Run the grep's inverse set.
+  }
 }
