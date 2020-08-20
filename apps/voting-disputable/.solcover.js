7b1c403f8c39e72520e059eb1c2764fcb24eb7fc
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-20 11:03:17 -0300

    DisputableVoting: Add skip tag for coverage

--- apps/voting-disputable/.solcover.js
+++ apps/voting-disputable/.solcover.js
@@ -4,5 +4,9 @@ module.exports = {
     '@aragon/os',
     '@aragon/minime',
     '@aragon/contract-helpers-test',
-  ]
+  ],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true               // Run the grep's inverse set.
+  }
 }
