be3e618a392933818fdeff5502efc1446dee5ae5
Brett Sun <qisheng.brett.sun@gmail.com>
2020-08-26 01:26:26 +0200

    Agreement: polish interface test

--- apps/agreement/test/agreement/agreement_initialize.js
+++ apps/agreement/test/agreement/agreement_initialize.js
@@ -8,6 +8,9 @@ const { assertBn, assertEvent, assertRevert } = require('@aragon/contract-helper
 injectWeb3(web3)
 injectArtifacts(artifacts)
 
+const ARBITRABLE_INTERFACE = '0x88f3ee69'
+const ARAGON_APP_INTERFACE = '0x54053e6c'
+
 contract('Agreement', ([_, EOA, owner]) => {
   let arbitrator, stakingFactory, agreement
 
@@ -86,11 +89,11 @@ contract('Agreement', ([_, EOA, owner]) => {
 
   describe('ERC165', () => {
     it('supports IArbitrable interface', async () => {
-      assert.isTrue(await agreement.supportsInterface('0x88f3ee69'), 'agreement does not support IArbitrable')
+      assert.isTrue(await agreement.supportsInterface(ARBITRABLE_INTERFACE), 'agreement does not support IArbitrable')
     })
 
     it('supports IAragonApp interface', async () => {
-      assert.isTrue(await agreement.supportsInterface('0x54053e6c'), 'agreement does not support IAragonAPP')
+      assert.isTrue(await agreement.supportsInterface(ARAGON_APP_INTERFACE), 'agreement does not support IAragonApp')
     })
   })
 })
