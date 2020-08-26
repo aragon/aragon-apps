2b697d947f99daec289732bdbd2e0771032170db
Brett Sun <qisheng.brett.sun@gmail.com>
2020-08-26 11:56:17 +0200

    DisputableVoting: migrate to staking 0.2.9 (#1259)

--- apps/voting-disputable/contracts/test/TestImports.sol
+++ apps/voting-disputable/contracts/test/TestImports.sol
@@ -6,12 +6,12 @@ import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 
 import "@aragon/minime/contracts/MiniMeToken.sol";
-import "@aragon/contract-helpers-test/contracts/0.4/misc/ExecutionTarget.sol";
 
-import "@aragon/staking/contracts/StakingFactory.sol";
 import "@aragon/apps-agreement/contracts/test/mocks/AgreementMock.sol";
 import "@aragon/apps-agreement/contracts/test/mocks/disputable/ArbitratorMock.sol";
 import "@aragon/apps-agreement/contracts/test/mocks/disputable/AragonAppFeesCashierMock.sol";
+
+import "@aragon/contract-helpers-test/contracts/0.4/misc/ExecutionTarget.sol";
 
 // You might think this file is a bit odd, but let me explain.
 // We only use some contracts in our tests, which means Truffle
---
+++ apps/voting-disputable/mocks/staking/StakingImports.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.17;
+
+import "@aragon/staking/contracts/Staking.sol";
+import "@aragon/staking/contracts/StakingFactory.sol";
+
+// Importing staking-related contracts to allow compiling them with a different version
+
+
+contract StakingImports {
+    constructor() public {
+        // solium-disable-previous-line no-empty-blocks
+    }
+}
