eb4bf8517b38fca80c384073f73cd1a9e57b6f49
Brett Sun <qisheng.brett.sun@gmail.com>
2020-08-26 02:05:30 +0200

    Agreement: bump back up to 1k optimization

--- apps/agreement/buidler.config.js
+++ apps/agreement/buidler.config.js
@@ -50,7 +50,8 @@ module.exports = {
     version: '0.4.24',
     optimizer: {
       enabled: true,
-      runs: 100,
+      // Any more and the contract goes over the size limit
+      runs: 1000,
     },
   },
   // The gas reporter plugin do not properly handle the buidlerevm
