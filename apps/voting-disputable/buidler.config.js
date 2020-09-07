c0b2f61b4e6858e73eee3b04db0a3265fe7eef2a
Brett Sun <qisheng.brett.sun@gmail.com>
2020-09-07 10:21:43 +0200

    Apps: update buidler config and remove keys (#1266)

--- apps/voting-disputable/buidler.config.js
+++ apps/voting-disputable/buidler.config.js
@@ -5,6 +5,9 @@ usePlugin('@nomiclabs/buidler-truffle5')
 usePlugin('buidler-gas-reporter')
 usePlugin('solidity-coverage')
 
+const ACCOUNTS = (process.env.ETH_KEYS ? process.env.ETH_KEYS.split(',') : [])
+  .map(key => key.trim())
+
 module.exports = {
   networks: {
     // Local development network using ganache. You can set any of the
@@ -25,18 +28,12 @@ module.exports = {
     // Mainnet network configured with Aragon node.
     mainnet: {
       url: 'https://mainnet.eth.aragon.network',
-      accounts: [
-        process.env.ETH_KEY ||
-          '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
-      ],
+      accounts: ACCOUNTS,
     },
     // Rinkeby network configured with Aragon node.
     rinkeby: {
       url: 'https://rinkeby.eth.aragon.network',
-      accounts: [
-        process.env.ETH_KEY ||
-          '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
-      ],
+      accounts: ACCOUNTS,
     },
     // Network configured to interact with Frame wallet. Requires
     // to have Frame running on your machine. Download it from:
